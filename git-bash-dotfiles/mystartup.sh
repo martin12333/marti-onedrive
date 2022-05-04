877d0dfd83210eb565f3d0cc98e002e496713b60
Martin Novy <martin12333@users.noreply.github.com>
2022-05-04 18:07:52 +0200

    bku

---
+++ git-bash-dotfiles/mystartup.sh
@@ -0,0 +1,32 @@
+#set -x
+
+echo '.sh'
+
+set
+
+
+#       --backup=numbered \
+# wincmd.ini
+
+while  true
+do
+
+   cp \
+      \
+      --archive \
+      --parents --strip-trailing-slashes --target-directory=$HOME/OneDrive/tar-exe/ --update --verbose \
+       `cygpath.exe -f ~/OneDrive/tar-exe/utf8-LF-tc-clip-vsc.txt  `       \
+      \
+      \
+      \
+   #--help   \
+
+   #   date
+   sleep 60s  #25s
+
+done
+
+
+echo press enter
+read x
+
