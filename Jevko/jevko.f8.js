dc06005cb6d5b2ef6c874adadc95c212d749589b
Martin Milan <martin12333@users.noreply.github.com>
2022-09-12 17:23:21 +0200

    bk

--- Jevko/jevko.f8.js
+++ Jevko/jevko.f8.js
@@ -1,10 +1,7 @@
 (mm_f8_doc file pseudo-format)
 
-// (selected lines from this file can be run in a terminal ... e.g. by F8 runSelectedText  )
+// (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
 
-
-
-(do-not run this file as a whole)
 
 // How to try parseJevko in node repl
 
