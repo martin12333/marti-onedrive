7786267cb1128d8c893a9b8b6ea96adfc2ef57c5
Martin Milan <martin12333@users.noreply.github.com>
2022-09-20 22:26:55 +0200

    bk

---
+++ f8-notebooks/clojurescript.edn.f8.cljs
@@ -0,0 +1,37 @@
+https://observablehq.com/@martin12333/hello-clojure-small-interpreter
+
+Hello, Clojure small interpreter
+
+
+sci = require("@borkdude/sci")
+
+cljs`   '[1 2 dddd\\;\\, ee "r" 'x :y '[a b]]   `
+
+
+
+my=98
+
+q=`   '[1  2 r ${my} 'x :y '[a b]]   `
+
+cljs(q)
+
+
+
+
+cljs`
+   '((all the files (in this repo)) (are shared (in the hope (that they will be useful to you)) (but WITHOUT ANY WARRANTY) (without even the implied warranty (of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE))))
+`
+
+reddit error
+
+
+
+
+
+
+
+
+
+
+
+
