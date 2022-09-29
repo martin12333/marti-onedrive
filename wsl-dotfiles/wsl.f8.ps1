3ee74b3877e5242546446d89c709d9a04203318e
Martin Milan <martin12333@users.noreply.github.com>
2022-09-29 21:26:29 +0200

    bk

--- wsl-dotfiles/wsl.f8.ps1
+++ wsl-dotfiles/wsl.f8.ps1
@@ -7,6 +7,28 @@ wsl --list --all --verbose
 
 w:
 ls
+c:
+
+wsl -d Ubuntu-22.04
+uname -a
+cat /etc/lsb_rele
+cat /etc/lsb-release
+
+
+history1324564=1 ; cat  D:\umarti\powershell_eternal_history.txt  | find   '"wsl -d "'
+
+wsl -d Ubuntu-20.04 -e ps -ef
+wsl -d Ubuntu-20.04 -e ps -e
+wsl -d Ubuntu-20.04 -e ps -f
+wsl -d Ubuntu-20.04 -e ps
+wsl -d Ubuntu-20.04 -- ps -ef
+wsl -d Ubuntu-20.04 --exec ps -ef
+
+
+
+
+
+
 
 history1324564=1 ; cat  D:\umarti\powershell_eternal_history.txt  | findstr  -i drive | findstr -vi one
 
@@ -16,3 +38,8 @@ history1324564=1 ; cat  D:\umarti\powershell_eternal_history.txt  | findstr  -i 
 # get disk free space
 ms store ..... open
 
+
+
+sudo apt update
+
+sudo apt install mc zsh zsh-doc
