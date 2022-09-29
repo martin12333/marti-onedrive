ee99f1dbcace368c5c501ba1433b4905123f8141
Martin Milan <martin12333@users.noreply.github.com>
2022-09-29 17:42:04 +0200

    bk

--- wsl-dotfiles/wsl.f8.ps1
+++ wsl-dotfiles/wsl.f8.ps1
@@ -1,3 +1,18 @@
 
+## f1 backspace
+# .wslconfig
+# wsl.conf
 
 wsl --list --all --verbose
+
+w:
+ls
+
+history1324564=1 ; cat  D:\umarti\powershell_eternal_history.txt  | findstr  -i drive | findstr -vi one
+
+
+## u20 to  u22
+# res pnt
+# get disk free space
+ms store ..... open
+
