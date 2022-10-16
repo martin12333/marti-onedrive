a7a32da4ebbe958ad5adb5e6faba37c844666eff
Martin Milan <martin12333@users.noreply.github.com>
2022-10-16 22:09:00 +0200

    bk

--- ohayo-22a/ohayo--install.f8.sh
+++ ohayo-22a/ohayo--install.f8.sh
@@ -5,4 +5,85 @@ exit
 
 (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
 
-ohayo--install.f8.sh
+# ohayo--install.f8.sh
+
+=========
+
+exit
+
+wsl
+zsh
+
+cd
+npm i ohayo
+
+
+
+
+================
+
+cd C:\Users\marti\ohayo\node_modules\.bin
+.\jtree.ps1 help
+.\jtree.ps1 version
+
+faaail
+(node:736) UnhandledPromiseRejectionWarning: Error: No definition found for nodeType id "DidYouMeanTileNode"
+.\jtree.ps1 "D:\umarti\dowNLOADS--SYMLINKED\ohayo-main (1)\ohayo-main\ohayo\packages\print\test.ohayo"
+.\jtree.ps1 run  "D:\umarti\dowNLOADS--SYMLINKED\ohayo-main (1)\ohayo-main\ohayo\packages\print\test.ohayo"
+
+C:\pf\Git\bin\bash.exe
+env
+env node
+.exit
+#! /usr/bin/env node /usr/local/bin/jtree
+exit
+
+
+===============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=================
+=========
+
+➜  ~ git:(wsl) ✗ npm i ohayo
+npm WARN deprecated superagent@5.3.1: Please upgrade to v7.0.2+ of superagent.  We have fixed numerous issues with streams, form-data, attach(), filesystem errors not bubbling up (ENOENT on attach()), and all tests are now passing.  See the releases tab for more information at <https://github.com/visionmedia/superagent/releases>.
+npm WARN deprecated formidable@1.2.6: Please upgrade to latest, formidable@v2 or formidable@v3! Check these notes: https://bit.ly/2ZEqIau
+npm WARN saveError ENOENT: no such file or directory, open '/home/martin/package.json'
+npm notice created a lockfile as package-lock.json. You should commit this file.
+npm WARN enoent ENOENT: no such file or directory, open '/home/martin/package.json'
+npm WARN martin No description
+npm WARN martin No repository field.
+npm WARN martin No README data
+npm WARN martin No license field.
+
++ ohayo@19.2.0
+added 129 packages from 170 contributors and audited 129 packages in 22.428s
+
+12 packages are looking for funding
+  run `npm fund` for details
+
+found 4 vulnerabilities (1 low, 3 high)
+  run `npm audit fix` to fix them, or `npm audit` for details
+
+========
+
+
