5449f427fe870656013464838076930e008179a9
Martin Milan <martin12333@users.noreply.github.com>
2022-10-16 22:11:12 +0200

    bk

---
+++ f8-notebooks/sudo.f8.sh
@@ -0,0 +1,13 @@
+65860387:0;ll bin
+: 1665860446:0;which -a sudo
+: 1665860463:0;ll `which -a sudo`
+: 1665860512:0;echo `which -a sudo`
+: 1665860517:0;find `which -a sudo`
+: 1665860523:0;find `which -a sudo` -ls
+: 1665860590:0;find .p* -ls
+: 1665860603:0;find .z* -ls
+: 1665860651:0;/bin/sudo -i
+: 1665860670:0;/bin/sudo -i ls
+: 1665860688:0;/bin/sudo -i ls -la
+: 1665860803:0;/bin/sudo -i env
+: 1665860815:0;/bin/sudo  env
