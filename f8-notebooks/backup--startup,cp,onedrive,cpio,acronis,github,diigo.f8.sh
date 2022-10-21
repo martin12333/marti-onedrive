4027eac621dc6d7a88fcb7ac7acd254ed6845bb2
Martin Milan <martin12333@users.noreply.github.com>
2022-10-21 23:09:44 +0200

    bk

--- f8-notebooks/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
+++ f8-notebooks/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
@@ -101,8 +101,10 @@ wsl
 
 
 	cd ~/OneDrive
+	find . -type f -size -2000k -size +100c -mmin -360   |cpio --verbose  --create --format=crc >/mnt/c/u/cpio/22al2306-2000k-mmin--360.cpio
 
-	find . -type f -size -2000k -size +100c -mmin -360   |cpio --verbose  --create --format=crc >/mnt/d/umarti/cpio/22aj0114-2000k-mmin--360.cpio
+##FAIL MOUNT D:/ 
+	#d/umarti/cpio/22al2306-2000k-mmin--360.cpio
 
 
 
