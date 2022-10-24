364857dff9f7ccf886558ee25ef402c7ea4812e4
Martin Milan <martin12333@users.noreply.github.com>
2022-10-25 00:47:46 +0200

    bk

---
+++ stackblitz-WebContainer/WebContainer.f8.js
@@ -0,0 +1,19 @@
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+(do-not run this file as a whole)
+)
+exit
+
+
+WebContainer.f8.sh
+WebContainer.f8.js
+
+WebContainer™ working group
+
+
+
+ignore-dep-scripts=true
+prefer-symlinked-executables=true
+verify-store-integrity=false
+global-pnpmfile=~/.pnpm/.pnpmfile.cjs
+update-notifier=false
---
+++ stackblitz-WebContainer/WebContainer.f8.sh
@@ -0,0 +1,70 @@
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+(do-not run this file as a whole)
+)
+exit
+
+
+WebContainer.f8.sh
+WebContainer.f8.js
+
+WebContainer™ working group
+
+
+
+.jsh_history 30!
+
+env
+cp /home/.pnpm/.pnpmfile.cjs .
+cp ~/.jsh_history  ./2.jsh_history
+#ln -iv ~/.jsh
+ln -siv
+mv -i .jsh_history 2
+mv -iv
+cp ~/.jsh_history  .
+cp ~/.npmrc  .
+cd projects/node-3ymeag/
+cd --help
+cd -
+cd
+cat .npmrc
+ls -la
+ls
+cd ..
+ls
+cd ..
+echo sdfjf >/dev/ttys001
+ps
+ls -FRal / >1
+ls
+ll
+ls -FRal /
+ls -Ral /
+ls --help
+cat
+tar
+find
+
+du
+df
+set
+
+ls -l /
+node
+npx
+npm
+man
+
+some lost
+
+
+
+
+
+
+
+ignore-dep-scripts=true
+prefer-symlinked-executables=true
+verify-store-integrity=false
+global-pnpmfile=~/.pnpm/.pnpmfile.cjs
+update-notifier=false
