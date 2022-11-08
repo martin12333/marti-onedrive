43d4aa5fe3f2d0ec77a579d01f2aa5c8d5eb2a17
Martin Milan <martin12333@users.noreply.github.com>
2022-11-08 22:22:16 +0100

    before-refactor

---
+++ f8-notebooks/get-unix-info--id,env,ls.f8.sh
@@ -0,0 +1,20 @@
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+
+id
+env
+ls -la
+ls -la /
+ls -la /mnt/wsl
+du -ak|sort -n
+
+df -m
+
+
+cat /etc/wsl.conf
