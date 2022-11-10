9dfe0311d24fea5b8aae8d23d29e9f1d725f04b9
Martin Milan <martin12333@users.noreply.github.com>
2022-11-10 20:35:13 +0100

    refactor

--- wiki-f8/github-wiki.f8.sh
+++ wiki-f8/github-wiki.f8.sh
@@ -14,3 +14,16 @@ C:\Users\marti\OneDrive\git\git init--separate-git-dir.f8.sh
 git remote -vvv add   origin https://github.com/martin12333/marti-onedrive.wiki.git
 
 
+cd AAAA_GITHUB_WIKI
+git pull -vv origin master
+ls -la
+cd -
+
+
+
+
+
+
+https://github.com/foambubble/foam/issues/16
+
+
