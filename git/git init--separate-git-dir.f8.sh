6d59a5525c42bd222b2699662d2320a2338f4f1f
Martin Milan <martin12333@users.noreply.github.com>
2022-11-10 18:58:51 +0100

    bk

--- git/git init--separate-git-dir.f8.sh
+++ git/git init--separate-git-dir.f8.sh
@@ -1,4 +1,13 @@
-git sep
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
+
 
 
 
--- wiki/github-wiki.f8.sh
+++ wiki/github-wiki.f8.sh
@@ -1,8 +1,15 @@
-wiki
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
 
 cat .git
-
-
+C:\Users\marti\OneDrive\git\git init--separate-git-dir.f8.sh
 $ git clone https://github.com/YOUR_USERNAME/YOUR_REPOSITORY.wiki.git
 
 
