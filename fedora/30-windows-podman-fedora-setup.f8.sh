9bf0fb03a8e44b0a5b230297239eeddad910c2f5
Martin Milan <martin12333@users.noreply.github.com>
2022-11-11 18:54:50 +0100

    before-refactor

---
+++ fedora/30-windows-podman-fedora-setup.f8.sh
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+use_mouse
+	wexplorer
+		\\wsl$\
+		rclick 	map P:
+		rclick create shortcut
+			f5 to see it
+			move to C:\Users\marti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\jako-zaremovane
+
+
+
+
