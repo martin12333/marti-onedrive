b5350343b60d1c576841a7d4115c4dda60afa371
Martin Milan <martin12333@users.noreply.github.com>
2022-11-12 17:54:29 +0100

    bk

---
+++ backup-f8/windows-icacls.f8.ps1
@@ -0,0 +1,33 @@
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
+
+where.exe powershell.exe
+powershell.exe
+
+echo see this in win-x-admin history
+
+mkdir "d:\appendbyUSER"
+mkdir "d:\appendbyUSER\admin"
+mkdir "d:\appendbyUSER\win-x-admin-powershell-mkdir"
+
+use_mouse
+	win-e
+		"D:\appendbyUSER"
+			properties
+				check that
+					it is not form about prop of d:\
+					it is form about prop of "D:\appendbyUSER"
+				advanced
+					grant appenddata to mar80 for "d:\appendbyUSER"
+
+
+
+
+
