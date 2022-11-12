2875b00afd6e58d85a3aeb0b04f1055a70717c72
Martin Milan <martin12333@users.noreply.github.com>
2022-11-12 16:08:23 +0100

    bk

---
+++ backup-f8/grant-append-not-write.f8.ps1
@@ -0,0 +1,23 @@
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
+
+mkdir "backup-f8"
+
+$newname=".\backup-f8\grant-append-not-write.f8.ps1"
+newname=".\backup-f8\grant-append-not-write.f8.ps1"
+
+cp -iv ".\f8-notebooks\00-TEMPLATE.f8.ps1" $newname
+
+git add -vv  '*.f8.*'
+git add -vv --dry-run '*.f8.*'
+
+code $newname
+
+
---
+++ f8-notebooks/00-TEMPLATE.f8.ps1
@@ -0,0 +1,27 @@
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
+
+mkdir "backup-f8"
+
+$newname=".\backup-f8\grant-append-not-write.f8.ps1"
+newname=".\backup-f8\grant-append-not-write.f8.ps1"
+
+mv -iv ".\f8-notebooks\00-TEMPLATE.f8.sh" .\OLD
+ls -latr ".\f8-notebooks"
+
+cp -iv ".\f8-notebooks\00-TEMPLATE.f8.ps1" $newname
+
+git add -vv --dry-run '*.f8.*'
+git add -vv  '*.f8.*'
+git commit -v -a -m bk
+
+code $newname
+
+
