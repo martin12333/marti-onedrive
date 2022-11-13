020c3b1c6d0618e664937e2250603f612d1a89b5
Martin Milan <martin12333@users.noreply.github.com>
2022-11-13 17:30:07 +0100

    fix

--- f8-notebooks/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
+++ f8-notebooks/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
@@ -72,11 +72,14 @@ git add -v '/c/Users/marti/OneDrive/tar-exe/u/home/martin/.zshrc'    '/c/Users/m
 git add -v  "C:\Users\marti\OneDrive\wiki-f8\zzzexperim--rdoc.rdoc"   "C:\Users\marti\OneDrive\wiki-f8\zzza-textile-file.textile"  "C:\Users\marti\OneDrive\wiki-f8\zzza-textile-file - Copy.txt"  "C:\Users\marti\OneDrive\tar-exe\c\Users\marti\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 
 
-	git add -vv --dry-run '*sprea*iss*' #    '*f8.*'
+	git add -vv       --dry-run       '*sprea*iss*'
+	##    '*f8.*'
+
 	git add -vv  '*.f8.*'
-	git add -vv --dry-run '*.f8.*'
+	#git add -vv         --dry-run       '*.f8.*'
+
 	git add -vv    'g*o.sh'
-	git add -vv  --dry-run   'g*o.sh'
+	git add -vv          --dry-run            'g*o.sh'
 
 echo '-------- section --------'
 
---
+++ git-f8/git-init--separate-git-dir.f8.sh
@@ -0,0 +1,43 @@
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
+
+
+
+
+https://github.com/search?q=user%3Aactes+GIT-bk04&type=code
+https://github.com/martin12333/marti-onedrive/blob/c7c23beb7c727bfce53923146cafce7b70f6e24a/git-bash-dotfiles/.bash_eternal_history
+
+
+##mkdir --parents $HOME/GIT-bk04/pymartinca-beta.git
+
+mkdir /d/GIT-bk04/marti-onedrive.wiki.git
+
+cd OneDrive
+mkdir AAAA_GITHUB_WIKI
+cd AAAA_GITHUB_WIKI
+
+git init --separate-git-dir=/d/GIT-bk04/marti-onedrive.wiki.git
+git remote -vvv add   origin https://github.com/martin12333/marti-onedrive.wiki.git
+git pull -vv origin
+git pull -vv origin master
+
+git checkout master
+
+#master
+
+ls -la
+git ls-files
+du -a |sort -n
+
+#$ git clone https://github.com/YOUR_USERNAME/YOUR_REPOSITORY.wiki.git
+
+
+echo '-------- section --------'
+
