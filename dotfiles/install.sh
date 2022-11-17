1ee13a1a30b194280291dad687f96d466b380872
Martin Milan <martin12333@users.noreply.github.com>
2022-11-17 20:07:35 +0100

    fix

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+set -e
+set -u
+set -x
 
-create_symlinks() {
+
+#create_symlinks() {
     # Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
 
     # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
+#    files=$(find -maxdepth 1 -type f -name ".*")
 
     # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
-}
+#    for file in $files; do
+#        name=$(basename $file)
+#        echo "Creating symlink to $name in home directory."
+        #####rm -rf ~/$name
+        #ln -s $script_dir/$name ~/$name
+    #done
+#}
 
-create_symlinks
+#create_symlinks
 
-echo "Initializing conda for zsh."
-conda init zsh
+#echo "Initializing conda for zsh."
+#conda init zsh
 
 echo "Installing fonts."
 FONT_DIR="$HOME/.fonts"
