2542e07f87e0a31fa707c1dd6fdca7c70136999a
Martin Milan <martin12333@users.noreply.github.com>
2022-11-19 23:22:03 +0100

    refactor

--- fedora/30-windows-podman-fedora-setup.f8.sh
+++ fedora/30-windows-podman-fedora-setup.f8.sh
@@ -115,9 +115,7 @@ cp -R -v -i /mnt/c/Users/marti/OneDrive/dotfiles  .
 #cp -R -v -i /mnt/c/Users/marti/OneDrive/dotfiles/.    dotfiles
 #cp -R -v --backup=numbered  /mnt/c/Users/marti/OneDrive/dotfiles/.    dotfiles
 
-ls
-ll
-###lah
+
 ls -la
 ls -latr
 ls -1aR dotfiles
@@ -125,12 +123,13 @@ ls -1aR dotfiles
 
 
 
-
 echo '-------- section --------'
 
 cd dotfiles
 
-# todo install.sh
+
+
+./install.sh
 
 
 
