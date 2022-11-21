8477ea6985f91ad74b4b07ca1296720f4a5b31aa
Martin Milan <martin12333@users.noreply.github.com>
2022-11-21 20:55:53 +0100

    bk

--- podman-docker/toolbox.f8.sh
+++ podman-docker/toolbox.f8.sh
@@ -7,9 +7,29 @@ exit
 
 #```shell
 
-sudo du -x -m /
+sudo du -x -m / | sort -n
+
+44      /usr/lib64/python3.10
+51      /usr/share
+76      /usr/bin
+81      /var/cache/dnf/fedora-e58e3c9673d413e5
+81      /var/cache/dnf/fedora-e58e3c9673d413e5/repodata
+81      /var/tmp/dnf-user-3ps9xppu/fedora-e58e3c9673d413e5
+81      /var/tmp/dnf-user-3ps9xppu/fedora-e58e3c9673d413e5/repodata
+136     /usr/lib64
+252     /var/cache
+252     /var/cache/dnf
+252     /var/tmp
+252     /var/tmp/dnf-user-3ps9xppu
+333     /usr
+525     /var
+877     /
+
+df -m
+/dev/sdc         1031019    877    977698   1% /
 
 
 
-
-
+dnf search toolbox
+sudo dnf install toolbox bash-completion man-db man-pages
+y
