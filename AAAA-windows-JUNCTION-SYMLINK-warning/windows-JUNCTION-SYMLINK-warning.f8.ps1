de76d6fb4c628844f5e073fc2a9376088e01002a
Martin Milan <martin12333@users.noreply.github.com>
2022-11-23 22:10:39 +0100

    fix

--- AAAA-windows-JUNCTION-SYMLINK-warning/windows-JUNCTION-SYMLINK-warning.f8.ps1
+++ AAAA-windows-JUNCTION-SYMLINK-warning/windows-JUNCTION-SYMLINK-warning.f8.ps1
@@ -1,3 +1,29 @@
 AAAA-windows-JUNCTION-SYMLINK-warning
 
 some tools bug when MOVE-JUNCTION-SYMLIN--RADEJI-OPRAVDU-LNK
+
+therefore deleted using wexplorer
+
+
+
+
+
+cd .\dotfiles
+
+. "C:\Program Files\WindowsApps\Microsoft.SysinternalsSuite_2022.11.1.0_x64__8wekyb3d8bbwe\Tools\junction.exe"
+C:\Pf\WindowsApps\Microsoft.SysinternalsSuite_2022.11.1.0_x64__8wekyb3d8bbwe\Tools\junction.exe ".\dotfiles"
+
+
+
+C:\Users\marti\OneDrive\dotfiles\dotfiles: UNKNOWN MICROSOFT REPARSE POINT
+
+
+
+Junction v1.07 - Creates and lists directory links
+Copyright (C) 2005-2016 Mark Russinovich
+Sysinternals - www.sysinternals.com
+
+The first usage is for displaying reparse point information, the
+second usage is for creating
+
+
