36c555b08c0048256eabb03c17fb096966b2c897
Martin Milan <martin12333@users.noreply.github.com>
2022-11-23 19:14:29 +0100

    before-refactor

--- linux-f8/30-linux-setup.f8.sh
+++ linux-f8/30-linux-setup.f8.sh
@@ -1,6 +1,3 @@
-fedora of podman ... setup.f8.sh
-
-
 
 (mm_f8_doc file pseudo-format)
 # (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
@@ -8,6 +5,12 @@ fedora of podman ... setup.f8.sh
 do-not run this file as a whole
 )
 exit
+
+echo '-------- section --------'
+
+
+
+MOVING FROM THIS FILE TO dotfiles/*-*.f8.sh
 
 
 
