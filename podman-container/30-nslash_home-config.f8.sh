22a73de478b3a7b97a1faaf775153b0d2186d2e7
Martin Milan <martin12333@users.noreply.github.com>
2022-11-26 22:49:40 +0100

    it-works

--- podman-container/30-nslash_home-config.f8.sh
+++ podman-container/30-nslash_home-config.f8.sh
@@ -7,28 +7,33 @@ do-not run this file as a whole
 )
 exit
 
+explorer "P:\home\user\.local\share\containers\storage\volumes\nslash_home"
 
 podman machine start
 podman start   -ai    cmy22b
+
 podman ps --all
 podman container list --all
-
 
 id
 
 touch /homedockw/touch
 cat /etc/os-release
-cat /etc/lsb-release
 env
-
-exit
-
-explorer "P:\home\user\.local\share\containers\storage\volumes\nslash_home"
 
 echo '-------- section --------'
 
+#cd /
+cd /home
+#tar czf /homedockw/home.tarz home
+tar czf /homedockw/home.tarz .
 
+cd /homedockw
+tar tzf /homedockw/home.tarz
 
+tar xzf /homedockw/home.tarz
+
+ll
 
 echo '-------- section --------'
 
