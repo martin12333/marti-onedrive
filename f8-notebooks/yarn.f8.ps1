aea2dbace2ca124f224444c0233be62ae4251321
Martin Milan <martin12333@users.noreply.github.com>
2022-11-27 17:50:36 +0100

    bug-found

---
+++ f8-notebooks/yarn.f8.ps1
@@ -0,0 +1,26 @@
+
+yarn deletes all files and directories under node_modules that don't belong to the currently installed packages. This is part of the design. It does work differently than npm's pruning/cleaning of extraneous files.
+
+For comparison, npm will only delete extraneous directories in node_modules if they have a package.json file. It won't remove other non-package directories. It also just never cleans anything out of node_modules/.bin. Even npm prune doesn't remove them.
+
+
+
+cd
+mkdir tmp
+cd tmp
+where.exe yarn
+
+history1324564=1 ; cat  D:\umarti\powershell_eternal_history.txt  | findstr   yar
+
+cmd /c dir /b /s /a '@*'
+cd OneDrive
+
+explorer.exe C:\Users\marti\OneDrive\js-shell-22a
+
+
+
+
+
+
+"C:\Users\marti\ohayo-npm\node_modules\jtree\treeComponentFramework\node_modules\formidable\yarn.lock"
+
