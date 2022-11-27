e06965d477d1ab039378de0bde36f580e547b7ef
Martin Milan <martin12333@users.noreply.github.com>
2022-11-27 22:53:04 +0100

    fix

--- node-f8/10-curl-nvm.f8.sh
+++ node-f8/10-curl-nvm.f8.sh
@@ -1,4 +1,15 @@
 
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+
+podman machine start
+
 podman start   -ai    cmy22b
 
 
