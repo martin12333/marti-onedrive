bbbdf1fbfdae0d5238a4796e658d8781ca91cbbd
Martin Milan <martin12333@users.noreply.github.com>
2022-12-10 20:59:27 +0100

    fix

--- cowasm/bsd.f8.sh
+++ cowasm/bsd.f8.sh
@@ -1,5 +1,14 @@
 
+(mm_f8_doc file pseudo-format)
+do-not run this file as a whole
+)
+exit
 
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
 
 
 
--- wasmer-os/wasmer-sh.f8.sh
+++ wasmer-os/wasmer-sh.f8.sh
@@ -1,4 +1,13 @@
+(mm_f8_doc file pseudo-format)
+do-not run this file as a whole
+)
+exit
 
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
 strace
 strace echo
 strace true
