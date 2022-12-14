bb2aaf4d0d2382040ab063ff4a447fadb17c70f7
Martin Milan <martin12333@users.noreply.github.com>
2022-12-14 03:52:06 +0100

    fix

--- cowasm/15-cowasm-build.f8.sh
+++ cowasm/15-cowasm-build.f8.sh
@@ -51,119 +51,65 @@ cd -
 
 
 
-export PATH=/python-wasm/bin/:$PATH
+##export PATH=/python-wasm/bin/:$PATH
 
 echo '-------- section --------'
 
-Setting up automake (1:1.16.5-1.3) ...
-update-alternatives: using /usr/bin/automake-1.16 to provide /usr/bin/automake (automake) in auto mode
-update-alternatives: warning: skip creation of /usr/share/man/man1/automake.1.gz because associated file /usr/share/man/man1/automake-1.16.1.gz (of link group automake) doesn't exist
+#Setting up automake (1:1.16.5-1.3) ...
+#update-alternatives: using /usr/bin/automake-1.16 to provide /usr/bin/automake (automake) in auto mode
+#update-alternatives: warning: skip creation of /usr/share/man/man1/automake.1.gz because associated file /usr/share/man/man1/automake-1.16.1.gz (of link group automake) doesn't exist'
 
-echo '-------- section: install --------'
-
-
+echo '-------- section --------'
 
 
 
-cd
-mkdir cowasm
-cd cowasm
-npm i dash-wasm
+cd ~/cowasm/packages
 
-find . -name "*.wasm" -ls
+#find . -name "*.wasm" -ls
 
-cd node_modules
-cd dash-wasm
-
-npm i
 
 du -m
 
-# added later:
-cat package.json
-npx dash-wasm
+
+
+
+
+## added later:
+#cat package.json
+#npx dash-wasm
 # ctrl-d
-cat `which dash-wasm`
+#cat `which dash-wasm`
 
 echo '-------- section: run --------'
 
-cd ~/cowasm/node_modules/dash-wasm
+#cd ~/cowasm/node_modules/dash-wasm
 
-npx /usr/bin/zsh
+#npx /usr/bin/zsh
 
-dash-wasm
+#dash-wasm
 
 echo '-------- section: experiments --------'
 
-#(cowasm)$
- ls -l
-
-total 561
-----------    1 0  0    4246 Dec  6 17:26 README.md
-d---------    2 0  0    4096 Dec  6 17:26 bin
-d---------    3 0  0    4096 Dec  6 17:26 dist
-d---------  222 0  0   12288 Dec  6 17:32 node_modules
-----------    1 0  0  261599 Dec  6 17:32 package-lock.json
-----------    1 0  0    1673 Dec  6 17:26 package.json
-----------    1 0  0     207 Dec  6 17:26 tsconfig.json
 
 
-(cowasm)$
- ls -l /
-
-total 99
-d---------    1 0  0   512 Nov  2 21:16 acloudSUBDIR
-l---------    1 0  0     7 Oct 20 11:49 bin -> usr/bin
-d---------    1 0  0  4096 Apr 18  2022 boot
-d---------    1 0  0   512 Nov  2 20:30 cdrivemy
-
-d---------    1 0  0     0 Dec  7 00:16 cowasm
-
-d---------    5 0  0   360 Dec  6 17:25 dev
-d---------    1 0  0  4096 Nov 27 20:07 etc
-d---------    5 0  0  4096 Nov 28 22:10 home
-l---------    1 0  0     7 Oct 20 11:49 lib -> usr/lib
-l---------    1 0  0     9 Oct 20 11:49 lib32 -> usr/lib32
-l---------    1 0  0     9 Oct 20 11:49 lib64 -> usr/lib64
-l---------    1 0  0    10 Oct 20 11:49 libx32 -> usr/libx32
-d---------    1 0  0  4096 Oct 20 11:49 media
-d---------    1 0  0  4096 Oct 20 11:49 mnt
-d---------    1 0  0   512 Dec  6 17:05 onedri
-d---------    1 0  0  4096 Oct 20 11:49 opt
-d---------  195 0  0     0 Dec  6 17:25 proc
-d---------    1 0  0  4096 Oct 27 17:48 root
-d---------    1 0  0  4096 Nov 26 22:15 run
-l---------    1 0  0     8 Oct 20 11:49 sbin -> usr/sbin
-d---------    1 0  0  4096 Oct 20 11:49 srv
-d---------   11 0  0     0 Dec  6 17:25 sys
-d---------    1 0  0  4096 Dec  6 17:25 tmp
-d---------    1 0  0  4096 Oct 20 11:49 usr
-d---------    1 0  0  4096 Oct 20 11:53 var
 
 
-(cowasm)$ ^D
 
 
-vscode ➜ ~/cowasm/node_modules/dash-wasm $ ls -l /
-total 48
-drwxrwxrwx   1 vscode vscode   512 Nov  2 21:16 acloudSUBDIR
-lrwxrwxrwx   1 root   root       7 Oct 20 11:49 bin -> usr/bin
-drwxr-xr-x   1 root   root    4096 Apr 18  2022 boot
-drwxrwxrwx   1 vscode nogroup  512 Nov  2 20:30 cdrivemy
-drwxr-xr-x   5 root   root     360 Dec  6 17:25 dev
-drwxr-xr-x   1 root   root    4096 Nov 27 20:07 etc
+
+echo '-------- sections copied from 10 --------'
 
 
-echo '-------- section --------'
+
+
+
+
 
 
 dash-wasm
 
 
-ls -la /boot
 ls -la /cowasm/usr/*
-ls -la /dev
-ls -la /etc
 
 help
 exit
