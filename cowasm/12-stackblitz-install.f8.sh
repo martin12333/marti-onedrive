80a8823569be70857604d5c5abbf6bdbeb862687
Martin Milan <martin12333@users.noreply.github.com>
2022-12-21 04:10:20 +0100

    add

---
+++ cowasm/12-stackblitz-install.f8.sh
@@ -0,0 +1,14 @@
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
+https://stackblitz.com/edit/node-fvjqji?file=index.js
+
