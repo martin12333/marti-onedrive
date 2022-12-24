09cd3d83b513b05148230be9f7bfe61155c54229
Martin Milan <martin12333@users.noreply.github.com>
2022-12-24 11:33:16 +0100

    bk

--- python230wasm/python-wasm.f8.py
+++ python230wasm/python-wasm.f8.py
@@ -10,7 +10,11 @@ breakpoint()
 
 
 
+ipython
 python
+
+
+python -m sysconfig
 
 zsh
 python3
@@ -21,7 +25,67 @@ echo '-------- section --------'
 
 
 
-#%%
+https://docs.python.org/3/library/sysconfig.html
+
+import sysconfig
+sysconfig.get_config_vars()
+
+
+In [2]: sysconfig.get_config_vars()
+Out[2]:
+{'prefix': 'D:\\conda',
+ 'exec_prefix': 'D:\\conda',
+ 'py_version': '3.9.12',
+ 'py_version_short': '3.9',
+ 'py_version_nodot': '39',
+ 'installed_base': 'D:\\conda',
+ 'base': 'D:\\conda',
+ 'installed_platbase': 'D:\\conda',
+ 'platbase': 'D:\\conda',
+ 'projectbase': 'D:\\conda',
+ 'platlibdir': 'lib',
+ 'abiflags': '',
+ 'LIBDEST': 'D:\\conda\\Lib',
+ 'BINLIBDEST': 'D:\\conda\\Lib',
+ 'INCLUDEPY': 'D:\\conda\\Include',
+ 'EXT_SUFFIX': '.cp39-win_amd64.pyd',
+ 'EXE': '.exe',
+ 'VERSION': '39',
+ 'BINDIR': 'D:\\conda',
+ 'TZPATH': 'D:\\conda\\share\\zoneinfo',
+ 'SO': '.cp39-win_amd64.pyd',
+ 'userbase': 'C:\\Users\\marti\\AppData\\Roaming\\Python',
+ 'srcdir': 'D:\\conda'}
+
+
+
+
+sysconfig.get_path_names()
+
+sysconfig.get_paths()
+
+
+
+
+import platform
+
+#platform.libc_ver()
+
+import sys
+
+sys.builtin_module_names
+sys.stdlib_module_names
+
+#sys.stderr
+#sys.__stderr__
+
+
+os.environ¶
+
 
 #%%
 
+#%%
+
+
+quit()
