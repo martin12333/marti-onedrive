8096c62230961e0ee707a52349d8af3c3a2f4a66
Martin Milan <martin12333@users.noreply.github.com>
2022-12-27 12:54:24 +0100

    fix

--- wasmer-os/40-run.f8.sh
+++ wasmer-os/40-run.f8.sh
@@ -28,30 +28,56 @@ cd
 #mkdir wasmer-coreutils1
 cd wasmer-coreutils1
 
+echo '-------- section --------'
 
-
-
+https://github.com/wasmerio/wasmer-js/issues/282
+#### issues
 wapm run env
 wapm run pwd
 pwd: error: failed to get current directory environment variable not found
+
+
+
+
+https://github.com/wasmerio/wasmer-js/issues/282
+#### issues
 wapm run mkdir newdir
 mkdir: newdir: failed to find a preopened file descriptor through which "newdir" could be opened
 
-##wapm run mkdir  --dir .  -- newdir12
 
+
+
+##wapm run mkdir  --dir .  -- newdir12
 error: failed to run `/home/vscode/wapm_packages/syrusakbary/coreutils@0.0.1/target/wasm32-wasi/release/uutils.wasm`
 │   1: failed to instantiate WASI module
 ╰─▶ 2: wasi filesystem creation error: `Could not get metadata for file "--": entity not found`
-vscode ➜ ~ $
+
+
+
+wapm run --dir=.   ls -la
+##wapm run --dir=.   pwd
 
 
 
 
 
+wapm run --dir=. mkdir  -v  newdir123465
+##wapm run mkdir  --dir .   newdir12
+wapm run mkdir  --dir=.    newdir1234
+wapm run mkdir  --dir=.    newdir12
+##wapm --dir=. run mkdir    newdir12
 
+
+wasmer  --dir=.  wapm_packages/syrusakbary/coreutils@0.0.1/target/wasm32-wasi/release/uutils.wasm --  mkdir  -v newdir1257
+wasmer  --dir .  wapm_packages/syrusakbary/coreutils@0.0.1/target/wasm32-wasi/release/uutils.wasm --  mkdir  -v newdir12
+
+
+
+
+wasmer
 wasmer list
 wapm list
-wasmer  --dir .  wapm_packages/syrusakbary/coreutils@0.0.1/target/wasm32-wasi/release/uutils.wasm -- ls -l
+wasmer  --dir=.  wapm_packages/syrusakbary/coreutils@0.0.1/target/wasm32-wasi/release/uutils.wasm -- ls -l
 ls -l
 
 akbary/coreutils@0.0.1/target/wasm32-wasi/release/uutils.wasm -- ls -l
@@ -67,6 +93,8 @@ vscode ➜ ~/wasmer-coreutils1 $
 
 
 
+https://github.com/wasmerio/wasmer-js/issues/282
+#### issues
 wapm run cat /dev
 cat: /dev: Invalid argument (os error 28)
 wapm run cat /
@@ -79,3 +107,19 @@ wapm run ls /
 wapm run ls /dev
 wapm run ls /this_is_not_exist
 ls /this_is_not_exist
+
+
+
+
+
+
+
+echo '-------- section --------'
+
+
+
+wapm run --dir=.   ls -la ..
+wapm run --dir=.   ls -l ../..
+wapm run --dir=.   ls  ../..
+wapm run --dir=.   ls   /
+wapm run --dir=.   ls -lad
