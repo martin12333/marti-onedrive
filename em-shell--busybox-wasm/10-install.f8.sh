bf571d340a26216d1875d1edf2e99add2381d41c
Martin Milan <martin12333@users.noreply.github.com>
2022-12-28 08:13:59 +0100

    add

---
+++ em-shell--busybox-wasm/10-install.f8.sh
@@ -0,0 +1,28 @@
+
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+podman start   -ai    cmy22b
+
+echo '-------- section: install --- first attempt --------'
+
+
+cd
+mkdir 10-cowasm
+
+cd 10-cowasm
+
+
+npm i dash-wasm
+
