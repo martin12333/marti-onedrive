ae6a8fbb25c8cefc3ff53d5f364993dfb0d8a786
Martin Milan <martin12333@users.noreply.github.com>
2022-12-31 12:06:19 +0100

    bug-found

--- em-shell--busybox-wasm/10-install.f8.sh
+++ em-shell--busybox-wasm/10-install.f8.sh
@@ -31,31 +31,44 @@ echo /c dir
 cmd /c dir dist
 cmd /c dir example
 
+
+cd
 cd busytex
 
 cmd /c mklink /j dist  D:\umarti\dowNLOADS--SYMLINKED
-
-
-
 
 https://github.com/busytex/busytex/issues/1
 
 # wasm version, download latest compiled assets, launch server.py and then go to http://localhost:8080/example/example.html
 
-mkdir -p dist
+#mkdir -p dist
 
-wget -P dist --backups=1 $(printf "https://github.com/busytex/busytex/releases/latest/download/%s " busytex_pipeline.js busytex_worker.js    busytex.wasm busytex.js texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
+#wget -P dist --backups=1 $(printf "https://github.com/busytex/busytex/releases/latest/download/%s " busytex_pipeline.js busytex_worker.js    busytex.wasm busytex.js texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
 
 #python example/serve.py
 python example/example.py
 
 
 
+cd
+cd busyide
+cmd /c mklink /j dist  D:\umarti\dowNLOADS--SYMLINKED
+
+#python ../busytex/example/example.py
+python serve.py
+
+A preload for 'http://localhost:8080/README.tex' is found, but is not used because the request credentials mode does not match. Consider taking a look at crossorigin attribute.
+(anonymous) @ (index):982
 
 
+cd
+mkdir remote-debug-profile1
+mkdir  chr-cors-profile1
+#edge.exe --remote-debugging-port=9222 --user-data-dir=remote-debug-profile1
+& 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'   --disable-web-security  --disable-gpu --disable-features=IsolateOrigins,site-per-process  --user-data-dir=C:\Users\marti\remote-debug-profile1
 
 
-
+& 'C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe' --disable-web-security  --disable-gpu --disable-features=IsolateOrigins,site-per-process  --user-data-dir=C:\Users\marti\chr-cors-profile1
 
 
 THIS BUSYBOX MAYBE IS ONLY FOR BROWSER, NOT FOR NODE
