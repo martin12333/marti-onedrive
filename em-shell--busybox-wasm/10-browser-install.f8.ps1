4f4b51d16437b279a1937c0e127aad5c2ac997b1
Martin Milan <martin12333@users.noreply.github.com>
2023-01-01 04:15:37 +0100

    refactor

--- em-shell--busybox-wasm/10-browser-install.f8.ps1
+++ em-shell--busybox-wasm/10-browser-install.f8.ps1
@@ -9,13 +9,7 @@ exit
 
 
 
-echo '-------- section --------'
-
-# i use  a container
-podman machine start
-podman start   -ai    cmy22b
-
-echo '-------- section: install --- first attempt --------'
+echo '-------- section: install --- first attempts --------'
 
 cd -
 exit
@@ -31,6 +25,7 @@ echo /c dir
 cmd /c dir dist
 cmd /c dir example
 
+echo '-------- section --------'
 
 cd
 cd busytex
@@ -49,6 +44,8 @@ https://github.com/busytex/busytex/issues/1
 python example/example.py
 
 
+echo '-------- section --------'
+
 
 cd
 cd busyide
@@ -62,7 +59,9 @@ cmd /c mklink /j dist  D:\umarti\dowNLOADS--SYMLINKED
 ##python ../busytex/example/example.py
 python serve.py
 
-# maybe ok #A preload for 'http://localhost:8080/README.tex' is found, but is not used because the request credentials mode does not match. Consider taking a look at crossorigin attribute.
+# maybe false alarm
+github.io has this too
+#A preload for 'http://localhost:8080/README.tex' is found, but is not used because the request credentials mode does not match. Consider taking a look at crossorigin attribute.
 (anonymous) @ (index):982
 cd
 mkdir remote-debug-profile1
@@ -73,6 +72,7 @@ mkdir  chr-cors-profile1
 
 
 
+echo '-------- section --------'
 
 
 cd
@@ -87,59 +87,4 @@ python example.py
 
 
 
-
-
-THIS BUSYBOX MAYBE IS ONLY FOR BROWSER, NOT FOR NODE
-
-#mkdir 10-cowasm
-cd busyide
-ll *.js *.wasm
-
-###node *.js *.wasm
-FAIL
-
-
-
-
-cd
-mkdir busytex
-cd busytex
-
-#e updated example/example.html, so it should work:
-# wasm version, download latest compiled assets, launch server.py and then go to http://localhost:8080/example/example.html
-mkdir -p dist
-
-a=https://github.com/busytex/busytex/releases/tag/build_16d437a3d6f662d7fcbc799775c9db3b1b5957d0
-
-ll dist
-
-WRONG URLs
-
-wget -P dist --backups=1 $(printf "$a/download/%s " busytex_pipeline.js  busytex_worker.js    busytex.wasm busytex.js texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
-
-
-wget -P dist --backups=1 $(printf "https://github.com/busytex/busytex/releases/latest/download/%s " busytex_pipeline.js busytex_worker.js    busytex.wasm busytex.js
-
-# texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
-
-
-
-#python3 example/serve.py
-
-
-
-https://objects.githubusercontent.com/github-production-release-asset-2e65be/301110516/e15b9391-1912-431a-b77d-1f12a574034e?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20221228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221228T072954Z&X-Amz-Expires=300&X-Amz-Signature=47e3465fa9b0193ca030df850c30c06d524c64d403c56c661e79b0658a7ac7aa&X-Amz-SignedHeaders=host&actor_id=870886&key_id=0&repo_id=301110516&response-content-disposition=attachment%3B%20filename%3Dbusytex_pipeline.js&response-content-type=application%2Foctet-stream
-
-
-
-
-
-ll *.js *.wasm
-echo node *x.js *x.wasm
-node --debug *x.js *x.wasm
-node --inspect *x.js *x.wasm
-node --inspect-brk *x.js *x.wasm
-ps -ef
-
-node debug *x.js *x.wasm
-node inspect *x.js *x.wasm
+echo '-------- section --------'
--- em-shell--busybox-wasm/10-node-install.f8.sh
+++ em-shell--busybox-wasm/10-node-install.f8.sh
@@ -8,3 +8,74 @@ exit
 (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
 
 
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+podman start   -ai    cmy22b
+
+echo '-------- section: install --- first attempts --------'
+
+
+
+
+
+THIS BUSYBOX MAYBE IS ONLY FOR BROWSER, NOT FOR NODE
+
+#mkdir 10-cowasm
+cd busyide
+ll *.js *.wasm
+
+###node *.js *.wasm
+FAIL
+
+
+
+
+cd
+mkdir busytex
+cd busytex
+
+#e updated example/example.html, so it should work:
+# wasm version, download latest compiled assets, launch server.py and then go to http://localhost:8080/example/example.html
+mkdir -p dist
+
+a=https://github.com/busytex/busytex/releases/tag/build_16d437a3d6f662d7fcbc799775c9db3b1b5957d0
+
+ll dist
+
+WRONG URLs
+
+wget -P dist --backups=1 $(printf "$a/download/%s " busytex_pipeline.js  busytex_worker.js    busytex.wasm busytex.js texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
+
+
+wget -P dist --backups=1 $(printf "https://github.com/busytex/busytex/releases/latest/download/%s " busytex_pipeline.js busytex_worker.js    busytex.wasm busytex.js
+
+# texlive-basic.js texlive-basic.data    ubuntu-texlive-latex-extra.data ubuntu-texlive-latex-extra.js    ubuntu-texlive-latex-recommended.data ubuntu-texlive-latex-recommended.js    ubuntu-texlive-science.data ubuntu-texlive-science.js)
+
+
+
+#python3 example/serve.py
+
+
+
+https://objects.githubusercontent.com/github-production-release-asset-2e65be/301110516/e15b9391-1912-431a-b77d-1f12a574034e?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20221228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221228T072954Z&X-Amz-Expires=300&X-Amz-Signature=47e3465fa9b0193ca030df850c30c06d524c64d403c56c661e79b0658a7ac7aa&X-Amz-SignedHeaders=host&actor_id=870886&key_id=0&repo_id=301110516&response-content-disposition=attachment%3B%20filename%3Dbusytex_pipeline.js&response-content-type=application%2Foctet-stream
+
+
+
+
+
+ll *.js *.wasm
+echo node *x.js *x.wasm
+node --debug *x.js *x.wasm
+node --inspect *x.js *x.wasm
+node --inspect-brk *x.js *x.wasm
+ps -ef
+
+node debug *x.js *x.wasm
+node inspect *x.js *x.wasm
+
+
+
+echo '-------- section --------'
