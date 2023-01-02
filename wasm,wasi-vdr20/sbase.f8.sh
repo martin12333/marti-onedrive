163e63844d786ebdfc52726b51c87fc68deac2d6
Martin Milan <martin12333@users.noreply.github.com>
2023-01-02 06:48:11 +0100

    add

---
+++ wasm,wasi-vdr20/sbase.f8.sh
@@ -0,0 +1,35 @@
+
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+podman start   -ai    cmy22b
+
+echo '-------- section --------'
+
+
+
+file OneDrive/emsdk*/*log
+file emsdk*/*.wasm
+
+
+
+
+
+
+echo '-------- section --------'
+
+
+echo '-------- section --------'
+
+
