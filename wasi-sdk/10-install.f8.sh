026aeb1a3a7dd796c78ae1f5528719c7fd65bc4f
Martin Milan <martin12333@users.noreply.github.com>
2023-01-05 07:02:35 +0100

    add

---
+++ wasi-sdk/10-install.f8.sh
@@ -0,0 +1,32 @@
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+
+#podman start   -ai    cmy22b
+
+
+
+C:\Users\marti\OneDrive\podman-docker\podman-docker-commit.f8.sh
+
+
+
+echo '-------- section --------'
+
+
+
+
+echo '-------- section --------'
+
+#sudo apt update
