256c9455f20a7fd4853473cc0694b89ccee77630
Martin Milan <martin12333@users.noreply.github.com>
2023-01-06 08:18:30 +0100

    bk

--- toybox.wasm/toybox.f8.sh
+++ toybox.wasm/toybox.f8.sh
@@ -35,7 +35,7 @@ echo '-------- section --------'
 #source ./emsdk_env.sh
 source "/home/vscode/emsdk/emsdk_env.sh"
 
-
+#emdump
 
 
 
@@ -46,11 +46,42 @@ C:\Users\marti\OneDrive\wasm,wasi-vdr20\sbase.f8.sh
 
 echo '-------- section --------'
 
+cd toybox.wasm
 
+make help
 
+  config          - Update current config utilising a line-oriented program
+  menuconfig      - Update current config utilising a menu based program
+  oldconfig       - Update current config utilising a provided .config as base
+
+defconfig       - New config with default answer to all options
+                    This is the maximum sane configuration.
+  allyesconfig    - New config where all options are accepted with yes
+                    This may not actually compile, it's a starting point
+                    for further configuration (probably with menuconfig)
+  allnoconfig     - New config where all options are answered with no
+                    (NOP binary, starting point for further configuration)'
+
+  android_defconfig - Select commands available on android
+
+  COMMANDNAME     - Build individual toybox command as a standalone binary.
+
+ list            - List COMMANDNAMEs you can build standalone.
+  list_example    - List example commands (often used by the test suite)
+  list_pending    - List unfinished COMMANDNAMEs out of toys/pending.
 
 
 echo '-------- section --------'
 
 
+make menuconfig
+
+Arrow keys navigate the menu.  <Enter> selects submenus --->.  Highlighted letters are hotkeys.  Pressing <Y> includes,  │
+  │  <N> excludes, <M> modularizes features.  Press <Esc><Esc> to exit, <?> for Help, </> for Search.  Legend: [*] built-in   │
+  │  [ ] excluded  <M> module  < > module capable                                                                             │
+  │    Toybox global settings
+
+Save an Alternate Configuration File 
+
+echo '-------- section --------'
 
