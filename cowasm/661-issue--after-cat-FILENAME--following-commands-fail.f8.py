73a162d454ed72712e5e0104c9d86d8e1e52ecaa
Martin Milan <martin12333@users.noreply.github.com>
2023-01-18 10:23:51 +0100

    add

---
+++ cowasm/661-issue--after-cat-FILENAME--following-commands-fail.f8.py
@@ -0,0 +1,23 @@
+#661-issue--after-cat-FILENAME--following-commands-fail.f8.py
+
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+podman start   -ai    cmy22b
+
+echo '-------- section: 40-run --------'
+
+#```sh-session
+
+cd ~/10-cowasm/node_modules/dash-wasm
