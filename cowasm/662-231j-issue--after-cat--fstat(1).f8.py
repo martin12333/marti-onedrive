2c8905ee5b3c0dc8dcafd2785ba1ccc5e74ccaa7
Martin Milan <martin12333@users.noreply.github.com>
2023-01-19 13:48:51 +0100

    add

---
+++ cowasm/662-231j-issue--after-cat--fstat(1).f8.py
@@ -0,0 +1,35 @@
+#662-231j-issue--after-cat--fstat(1).f8.py
+#
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+podman start   -ai    cmy22b
+
+echo '-------- section: 40-run --------'
+
+#```sh-session
+
+cd ~/10-cowasm/node_modules/dash-wasm
+
+
+
+
+
+npx dash-wasm   -x
+
+cat
+
+^d
+
+python
+
