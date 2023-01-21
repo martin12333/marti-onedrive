dfde0455a0c209ad0253e2ff3c97917ffe00bd2f
Martin Milan <martin12333@users.noreply.github.com>
2023-01-21 17:31:56 +0100

    add

---
+++ node-f8/node_inspect--inspect---debugger.f8.sh
@@ -0,0 +1,43 @@
+
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+
+
+podman machine start
+podman start   -ai    cmy22b
+
+echo '-------- section --------'
+
+
+echo '-------- section --------'
+
+
+
+echo '-------- section --------'
+
+
+echo '-------- section --------'
+
+.exit
+echo '-------- section --------'
+
+ps -ef
+
+echo '-------- section --------'
+
+
+nvm which 18
+
+echo $path
+
+nvm ls-remote
+
+echo '-------- section --------'
+
+
