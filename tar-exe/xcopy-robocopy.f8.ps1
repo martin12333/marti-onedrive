07f08ce079f5e1e6d129d896ee549d23c78e991b
Martin Milan <martin12333@users.noreply.github.com>
2023-01-25 13:39:03 +0100

    ...

--- tar-exe/xcopy-robocopy.f8.ps1
+++ tar-exe/xcopy-robocopy.f8.ps1
@@ -5,9 +5,115 @@
 
 
 xcopy  /?|more
+
+/D:m-d-y     Copies files changed on or after the specified date.
+
+
+               If no date is given, copies only those files whose
+               source time is newer than the destination time.
+
+
+
+
+
+			   /EXCLUDE:file1[+file2][+file3]...
+               Specifies a list of files containing strings.  Each string
+               should be in a separate line in the files.  When any of the
+               strings match any part of the absolute path of the file to be               copied, that file will be excluded from being copied.
+
+
+
+			   /I           If destination does not exist and copying more than one file,               assumes that destination must be a directory.
+
+
+
+
+
+
+
+
+/U           Copies only files that already exist in destination.
+
+
+
+
+/B           Copies the Symbolic Link itself versus the target of the link.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 robocopy  /?|more
 robocopy  /?|findstr -i new
 robocopy  /?|findstr -i xn
 robocopy  /?|findstr -i upda
+
+/S :: copy Subdirectories, but not empty ones.
+
+
+/MON:n :: MONitor source; run again when more than n changes seen.-             /MOT:m :: MOnitor source; run again in m minutes Time, if changed.-
+
+
+/SJ :: copy Junctions as junctions instead of as the junction targets.
+/SL :: copy Symbolic Links as links instead of as the link targets.
+
+/XJ :: eXclude symbolic links (for both files and directories) and Junction points.
+
+
+
+
+/XD dirs [dirs]... :: eXclude Directories matching given names/paths.
+
+
+
+
+/XO :: eXclude Older files.
+/XX :: eXclude eXtra files and directories.
+
+/IS :: Include Same files.
+/IT :: Include Tweaked files.
+
+
+/IM :: Include Modified files (differing change times).
+
+
+
+/L :: List only - don't copy, timestamp or delete any files'
+
+
+
+
+
+
+
+
+
+
+
+Robocopy.exe    $env:HOME\sbase.wasm   .\sbase.wasm--robocopy      /L   /s   /XJ   /XD .git
+
+echo    $env:HOME
+ls    ~/sbase.wasm
+
+
+
+
+
+
 
 
