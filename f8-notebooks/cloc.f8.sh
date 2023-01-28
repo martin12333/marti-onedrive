7e951dc5795e842be4cf34133636ac34dccddcda
Martin Milan <martin12333@users.noreply.github.com>
2023-01-28 13:54:45 +0100

    add

---
+++ f8-notebooks/cloc.f8.sh
@@ -0,0 +1,39 @@
+
+
+
+ sudo apt install cloc
+
+cd cloc*
+
+cloc .
+
+
+
+
+
+
+ ~/cloc-cowasm-main $ cloc .
+    1271 text files.
+     604 unique files.
+     746 files ignored.
+
+                            127           4504           7973          28103
+TypeScript                       95           1170           1868          11332
+C/C++ Header                     52            879           1591           3971
+Zig                              36            487            524           3174
+make                             39            504            327           1572
+Markdown                         48            405              0           1111
+yacc                              1             96             87            762
+JavaScript                       15            100            135            739
+Bourne Shell                     15             36             44            225
+diff                             18             26            180            184
+Python                            4             46             35            126
+Bourne Again Shell                2              4              0             28
+YAML                              2              6              2             21
+Dockerfile                        1              8              7             13
+CMake                             1              4              0             10
+Lua                               1              2              1             10
+Fortran 77                        1              3              0              5
+
+
+
