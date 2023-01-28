7f606c3eb9c6730c55d451cfe5502005e3632799
Martin Milan <martin12333@users.noreply.github.com>
2023-01-28 14:32:41 +0100

    add

---
+++ ps-mylen-mar/mystartup--.f8.ps1
@@ -0,0 +1,22 @@
+
+
+
+ls -l ~/log/mymyrsync.*
+
+
+
+
+
+mkdir ~/log
+
+echo >~/log/mymyrsync.sh.log
+
+# file:///C:/Users/marti/log/mymyrsync.sh.log
+
+ls -l ~/log/mymyrsync.sh.log
+echo >~/log/mymyrsync.sh.err.log
+
+echo  'file:///C:/Users/marti/log/mymyrsync.sh.log'
+echo  'file:///C:/Users/marti/log/mymyrsync.sh.err.log'
+
+
