e39b3982e8940d86555bda480f8c2c882895cf5b
Martin Milan <martin12333@users.noreply.github.com>
2023-01-28 14:02:33 +0100

    add

---
+++ backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
@@ -0,0 +1,287 @@
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+(do-not run this file as a whole)
+)
+exit
+
+echo '-------- section --------'
+
+# startup -> C:\Users\marti\OneDrive\git-bash-dotfiles\mymyrsync.sh
+
+## dotfi=" .bash_history .bash_eternal_history  .guile_history .lsc_history .node_repl_history .python_history .sbcl_history .sqlite_history .zsh_history "
+
+# C:\Users\marti\OneDrive\tar-exe\utf8-LF-tc-clip-vsc.txt
+
+
+ (eepitch-shell)
+pwsh
+# THIS PWSH CAN ALSO BE RUN FROM E.G. VSCODE
+ls
+
+C:\pf\Git\bin\bash.exe
+
+	cd ~/OneDrive
+
+
+	ls -latr c:/Users/marti/AppData/Local/Temp/
+
+	git ls-files
+	####git ls-files settings.json
+	git ls-files|grep settings[.]json
+	git ls-files|grep [.]el
+	git ls-files|grep [.]ipynb
+	git ls-files|grep f8[.]
+	git ls-files|grep zsh
+
+	git add --help
+
+	cd hyperpolyglot----P*
+	cd ..
+	cd tar-exe
+	cd jupyter-notebooks
+	#git status
+
+	### interactive . not seeing pwd
+	### ^c enter enter
+	git add -vv --dry-run --interactive *.css
+	git add -vv --interactive  '*sprea*iss*' # '*.ipynb' # * # '*.ini' #'*.json' #'*zsh_history*'
+	4
+	1
+	2
+	3
+
+	7
+	git add -vv --dry-run --interactive *.css
+
+  '/c/Users/marti/OneDrive/tar-exe/u/home/martin/.zsh_history'
+
+ /u/home/martin/eternal.zsh_history  /w/home/martin/eternal.zsh_history
+
+	git add -vv '/c/Users/marti/OneDrive/tar-exe/u/home/martin/.bash_history'    '/c/Users/marti/OneDrive/tar-exe/u/home/martin/eternal.zsh_history'    '/c/Users/marti/OneDrive/tar-exe/w/home/martin/eternal.zsh_history'
+
+	git add -vv  '/c/Users/marti/OneDrive/tar-exe/u/home/dockw/node/.zsh_history'
+
+git add -v '/c/Users/marti/OneDrive/tar-exe/u/home/martin/.zshrc'    '/c/Users/marti/OneDrive/tar-exe/w/home/martin/.zshrc'
+
+
+
+
+	# because diigo
+	git add -vv   '/c/Users/marti/OneDrive/tar-exe/w/home/martin/.zsh_history'
+
+	git add -vv 'C:\Users\marti\OneDrive\conn\writing22.md' "C:\Users\marti\OneDrive\task-scheduler--win\po-pulnoci23.xml"   "C:\Users\marti\OneDrive\wiki-f8\a-mediawiki-file.mediawiki"  "C:\Users\marti\OneDrive\wiki-f8\a-markdown-file.md"
+
+git add -v  "C:\Users\marti\OneDrive\wiki-f8\zzzexperim--rdoc.rdoc"   "C:\Users\marti\OneDrive\wiki-f8\zzza-textile-file.textile"  "C:\Users\marti\OneDrive\wiki-f8\zzza-textile-file - Copy.txt"  "C:\Users\marti\OneDrive\tar-exe\c\Users\marti\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" '/c/Users/marti/OneDrive/tar-exe/c/Users/marti/.wslconfig'   '/c/Users/marti/OneDrive/tar-exe/p/home/user/.local/share/containers/storage/volumes/nslash_home/_data/vscode/sbase.wasm/Makefile'
+
+	git add -vv       --dry-run       '*sprea*iss*'
+	git add -vv       --dry-run       '*/*.md'
+	git add -vv       --dry-run       '*.md'
+
+
+	git add -vv       --dry-run       .gitignore    'install.sh*'
+	git add -vv       --dry-run      '*.gitignore'  '*install.sh'
+	git add -vv       --dry-run       '*/.gitignore'  '*/install.sh'
+	git add -vv       --dry-run       '*s/.gitignore'  '*s/install.sh'
+	git add -vv       --dry-run       '*s/.gitignore'  '*s/*install*.sh'   '*r/.zshrc'
+	git add -vv       --dry-run       'w*/Sea*'
+	git add -vv           'w*m/Sea*'
+	git add -vv        '*s/.gitignore'  '*s/*install*.sh'   '*r/.zshrc'
+
+	##    '*f8.*'
+	git add -vv  '*.f8.*'
+	#git add -vv         --dry-run       '*.f8.*'
+
+	git add -vv    'g*o.sh'
+	git add -vv          --dry-run            'g*o.sh'
+
+echo '-------- section --------'
+
+git ls-files|grep ' '|grep f8   # spaces in names .... not ~ ~ tar cygpath
+
+echo '-------- section --------'
+
+exit
+C:\pf\Git\bin\bash.exe
+
+# "f8_doc" in *.tib
+
+code ./dotfiles
+#
+
+	git add -vv  '*.f8.*'
+	git add -vv  'podman-container/*create*.sh'
+	git add -vv --dry-run '*/*docker-create*.sh'
+	git add -vv --dry-run '*/*ockerfile*'
+	git add -vv --dry-run '*/*zsh_history*'
+	git add -vv --dry-run '*/*ish_history*'
+	git add -vv --dry-run 'wasm*/**/*sh_history*'
+	git add -vv    'wasm*/*/*sh_history*'
+	git add -vv    'sb*/*/*sh_history*'
+	git add -vv    '*/*zsh_history*'
+	git add -vv  '*/*ockerfile*'
+	git add -vv  '*/*docker-create*.sh'
+	git add -vv  'dotfiles/*.sh'
+	git add -vv --dry-run  'wa*/*akefile*'
+	git add -vv  'wa*/*akefile*'
+	git add -vv  'sb*/*akefile*'
+	git add -vv  'sb*/conf*'
+	git add -vv --dry-run  '*/*config'
+	git add -vv   '*/*config.mk'
+
+	git add -vv  '*.f8.*'
+
+	git push
+
+	/c/Users/marti/OneDrive/git-bash-dotfiles/mymyrsync.sh
+
+	git commit -v -a -m  before-manip-dirs
+	git commit -v -a -m ...
+	git commit -v -a -m done
+	git commit -v -a -m it-works
+	git commit -v -a -m add
+	git commit -v -a -m win-x-admin
+#	acro-sett,22bb-turn-off-auto-cleanup.zip
+	git commit -v -a -m acro-sett,22bb-turn-off-auto-cleanup
+	git commit -v -a -m bk-scripts--edited---
+	git commit -v -a -m gitignore
+
+	git commit -v -a -m bug-found
+	git commit -v -a -m  false-alarm-found
+	git commit -v -a -m discovery
+
+	git commit -v -a -m fix
+	git commit -v -a -m fix-comments
+	git commit -v -a -m patch-simplify-code
+	git commit -v -a -m false-alarm
+	git commit -v -a -m maybe-enhance
+	git commit -v -a -m important-test
+
+	git commit -v -a -m  rename
+	git commit -v -a -m  rewrite
+
+	git commit -v -a -m  before-refactor
+	git commit -v -a -m  partial-refactor
+	git commit -v -a -m  refactor
+	git commit -v -a -m  partial
+
+	git commit -v -a -m vulnera #
+	git commit -v -a -m zshrc
+
+
+	git push
+
+	git show --name-only
+
+	git push
+
+	git commit -v -a -m ...
+	git commit -v -a -m add
+
+	git add -vv  '*.f8.*'
+	git add -vv  '*.code-wo*'
+
+C:\pf\Git\bin\bash.exe
+
+
+cd ~/OneDrive
+
+cd ~/sbase.wasm
+git pull -vv
+
+cd
+git clone 'P:\home\user\.local\share\containers\storage\volumes\nslash_home\_data\vscode\sbase.wasm'
+cd sbase.wasm
+ls -latr
+cd ~/OneDrive
+
+
+
+tail tar-exe/w/home/martin/eternal.zsh_history
+tail tar-exe/u/home/martin/eternal.zsh_history
+tail tar-exe/u/home/dockw/node/.zsh_history
+
+	git show
+	git status | head -n 15
+	git log
+git whatchanged -p --abbrev-commit --pretty=medium
+
+	git gui &
+	# menu Visualize
+	git gui
+
+
+
+	# https://github.com/martin12333/marti-onedrive
+	# w: failed
+	# forgot to git add
+	#
+
+
+		#didnot help /d/dow too-long to read PROBABLY, too many files  ... prb in u2204 ...
+todo pseudosh
+	mv -iv dow OLDdow22
+ 	mkdir /d//dowNLOADS--SYMLINKED
+
+	wsl --list --all --verbose
+	wsl --shutdown
+	pseudosh	wsl 2204
+		cd /mnt/d/umarti/
+		dmesg|grep sdc
+
+
+
+
+
+
+cd /d/GIT-b*
+	git clone /c/Users/marti/OneDrive
+	ll
+	du -k|sort -n
+	cd OneDrive
+cd -
+cd
+cd OneDrive
+
+	exit
+C:\pf\Git\bin\bash.exe
+
+grep -REo '.{0,10}grep.{0,20}015.{0,10}'
+######grep -REo 'grep.*{0,20}015'
+
+#
+ls
+exit
+# END OF PWSH
+
+open another terminal
+wsl
+	zsh
+	cd ~/OneDrive
+
+	find . -type f -size -2000k -size +100c -mtime -1
+
+	find . -type f -size -2000k -size +1000k  -mtime -1 #-ls
+
+	find . -type f -size -100k -size +100c -mtime -3   |cpio --verbose --warning=all --create --format=crc >/mnt/d/umarti/cpio/228
+
+
+prepis 22ar
+	cd ~/OneDrive
+	find . -type f -size -2000k -size +100c -mmin -360   |cpio --verbose  --create --format=crc >/mnt/c/u/cpio/22bb-2000k-mmin--360.cpio
+
+##FAIL MOUNT D:/
+	#d/umarti/cpio/22al2306-2000k-mmin--360.cpio
+
+
+
+	find . -type f -size -2000k -size +100c -mtime -1   |cpio --verbose  --create --format=crc >/mnt/d/umarti/cpio/22af-2000k.cpio
+# scr shot 200k..800k
+
+
+#
+
+
+
+ (eepitch-shell)
+ (eepitch-kill)
+
