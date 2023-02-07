6a97b5fc1d306b57470fd63041e669e3012eaaae
Martin Milan <martin12333@users.noreply.github.com>
2023-02-07 02:31:22 +0100

    ...

--- f8-notebooks/win-disk-c-full.f8.ps1
+++ f8-notebooks/win-disk-c-full.f8.ps1
@@ -1,25 +1,49 @@
 win-disk-c-full.f8.ps1
 
 
+df -m
 
-du64.exe
+
 cd "C:\Users\milan_rxtipwr\OneDrive\Plocha\du64-mujlen-podpisy\"
+
+./du64
 
 ./du64.exe -l 1 ../..  | sort
 ./du64.exe -l 2   c:\  >D:\umarti\unsort-du64-l-2--c--.txt
+
+./du64.exe -l 2   c:\  >D:\umarti\adm-unsort-du64-l-2--c--.txt
+
+exit
+
+
+cat  D:\umarti\unsort-du64-l-2--c--.txt
+cat  D:\umarti\adm-unsort-du64-l-2--c--.txt
+
+cmd
+
+sort.exe  < D:\umarti\unsort-du64-l-2--c--.txt   >D:\umarti\du64-l-2--c--.txt
+
+sort.exe  < D:\umarti\adm-unsort-du64-l-2--c--.txt   >D:\umarti\adm-du64-l-2--c--.txt
+
+exit
+
+~
+| The '<' operator is reserved for future
+| use.
+
+
+
+
 
 | sort
 
 cat D:\umarti\du64-l-2--c--.txt
 
 ls D:\umarti\unsort-du64-l-2--c--.txt
-cat  D:\umarti\unsort-du64-l-2--c--.txt
 
 
-../..  | sort
 
 
-./du64 
 
 
 
