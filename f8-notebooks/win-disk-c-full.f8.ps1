db6e02f3d0f1a4619a3c87efc0b54998fa6268a3
Martin Milan <martin12333@users.noreply.github.com>
2023-02-07 02:02:18 +0100

    ...

--- f8-notebooks/win-disk-c-full.f8.ps1
+++ f8-notebooks/win-disk-c-full.f8.ps1
@@ -2,6 +2,26 @@ win-disk-c-full.f8.ps1
 
 
 
+du64.exe
+cd "C:\Users\milan_rxtipwr\OneDrive\Plocha\du64-mujlen-podpisy\"
+
+./du64.exe -l 1 ../..  | sort
+./du64.exe -l 2   c:\  >D:\umarti\unsort-du64-l-2--c--.txt
+
+| sort
+
+cat D:\umarti\du64-l-2--c--.txt
+
+ls D:\umarti\unsort-du64-l-2--c--.txt
+cat  D:\umarti\unsort-du64-l-2--c--.txt
+
+
+../..  | sort
+
+
+./du64 
+
+
 
 
 .\fedora
