d26b3e0a2faf36acb070d22ec6aebbdc84e809ac
Martin Milan <martin12333@users.noreply.github.com>
2023-02-09 00:19:11 +0100

    add

---
+++ browserPOSIX/telehack.f8.sh
@@ -0,0 +1,150 @@
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+
+podman start   -ai    cmy22b
+
+echo '-------- section --------'
+
+
+  Type HELP for a detailed command list.
+  Type NEWUSER to create an account.
+
+May the command line live forever.
+
+Command, one of the following:
+  2048         a2           ac           basic        bf           c8
+  cal          calc         cat          ching        clear        clock
+  cowsay       date         ddate        delta        diff         dir
+  echo         eliza        factor       figlet       file         geoip
+  head         help         ipaddr       joke         liff         minesweeper
+  more         morse        netstat      newuser      notes        phoon
+  pig          ping         pong         qr           rand         rfc
+  rig          roll         run          salvo        sleep        starwars
+  tail         traceroute   typespeed    usenet       users        uumap
+  uupath       uuplot       weather      when         zc           zork
+
+Press control-C to interrupt any command.
+More commands become available after login.
+.
+
+
+
+Press control-C to interrupt any command.
+More commands become available after login.
+.help
+2048                           sliding tile puzzle game
+?                              show command list
+a2 <disk|file>                 apple ][ emulator
+advent                         play adventure
+aquarium                       an aquarium/sea animation in ASCII art
+areacode <areacode|location>   look up an areacode or location
+basic                          start the basic interpreter
+bf [code|file]                 brainf*ck interpreter
+c8 [rom]                       launch CHIP-8 emulator
+cal [year]                     print a calendar
+calc [expr]                    calculator
+callsign <text>                lookup information for an amateur radio callsign
+cat <file>                     dump contents of file
+ching                          The Book of Changes
+clear                          clear screen
+clock [/font=font]             figlet time display. type figlet for a font list
+cowsay [/cow] <message>        configurable speaking cow
+date                           print day and time
+ddate                          convert Gregorian dates to Discordian dates
+delta                          convert timestamp to delta time string
+diff <filea> <fileb>           show the difference between two files
+dir                            list files
+echo [/lc|uc|title] <text>     display or transform a line of text
+eliza                          converse with an AI psychotherapist
+exit                           terminate and return to previous shell or logout
+factor <number>                print the prime factors of a number
+figlet [/font] <message>       make large letters out of ordinary text
+file <filename>                determine file type
+finger [@host]                 show users on current system or <host>
+fnord                          generate a fnord
+geoip [ipaddr]                 show location for an ip address
+gif [search]                   search for a gif
+head <file>                    display first lines of a file
+help                           print a list of commands and descriptions
+ipaddr
+          show current ip address
+joke [search]                  show random joke from unified joke database
+liff [search]                  look up a definition from The Meaning of Liff
+
+login [user]                   login or create a new telehack account
+
+mac <mac address>              look up the vendor of a mac address
+
+md5 [/base64] <string>         convert a string to md5
+
+minesweeper                    play a game of minesweeper
+
+more <file>                    print contents of file
+
+morse <message>                encode/decode morse code
+
+netstat
+            show connected hosts
+
+newuser                        create a new telehack account
+
+notes                          usenet newsreader
+
+octopus                        octal/hex/binary/ascii key dumper
+
+phoon                          show phase of the moon
+pig <text>                     convert text into pig latin
+
+ping <host>                    test a host for availablility
+
+pong                           start a local pong game
+pong /host                     host a pong game
+pong [user|port]               join a hosted pong game
+
+primes                         find prime numbers
+privacy                        read the privacy policy
+qr <text>                      generate a qr code
+rain                           animated raindrops display
+rand [low high]                random number generator
+rfc [num]                      Read Internet Request for Comment (RFC)
+rig                            random identity generator
+roll                           roll animated dice
+rot13 <message>                encode/decode rot13 text
+run <program>                  run the specified program (.bas .gam .exe .a2)
+
+salvo [/host]                  play salvo
+sleep <seconds>                delay for a specified amount of time
+starwars                       ascii movie
+sudoku                         play a game of sudoku
+tail <file>                    display the last part of a file
+traceroute <host>              show path to <host>
+typespeed                      measures your typing speed
+units                          unit conversion
+uptime                         time telehack has been up since last boot
+usenet                         distributed Internet discussion system
+
+users                          show user table
+
+uumap <host>                   show uumap entry for a host
+
+uupath <host> [avoid-hosts]    show path to host
+
+uuplot <host> [avoid-hosts]    plot uupath to a host
+
+weather [user|port]            show weather
+when [unixtime]                convert a unix time
+zipcode <zipcode|location>     lookup a zipcode or location
+zork                           play zork
+
+Press control-C to interrupt any command.
+More commands become available after login.
+.
