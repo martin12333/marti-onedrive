b44dc9e86b3097f17c9a0fda978dec6a01c979a4
Martin Milan <martin12333@users.noreply.github.com>
2023-02-09 04:24:09 +0100

    add

---
+++ browserPOSIX/sdf.org.f8.sh
@@ -0,0 +1,350 @@
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+
+podman start   -ai    cmy22b
+
+echo '-------- section --------'
+
+
+
+(I am the admin of the browserPOSIX subreddit, and I love Unix ). Today I learned about Telehack.com, but they are temporarily closed to new users. So I decided to try to be accepted at SDF .  I plan to write a  post to the browserPOSIX subreddit  about SDF ...
+
+
+
+
+
+
+
+
+
+#ssh new@sdf.org
+ssh  martinmilan@sdf.org
+#######ssh  martinmilan@sdfeu.sdf.org
+#ssh  martinmilan@tty.sdf.org
+yes
+
+
+
+Did you know you can validate your account and gain weekend IRC access
+by making a donation of $1 to $3?  Type '
+validate
+
+
+If you were referred to SDF by a friend or a current SDF member, they may
+be able to validate your new account for you.  You can usually find SDF
+regulars in either 'com' or 'irc'.  Be sure to ask them to help you.
+
+(contin
+
+Alternatively you may 'validate' your account via PAYPAL by clicking on the
+'DONATE' button at the bottom of the http://sdf.org website.  The paypal
+minimum is $3.  Please include 'Validate martinmilan' in the Payment For field.
+The SDF paypal address is 'paypal@freeshell.org'
+
+We also accept BitCoin and other crypto - Please type 'crypto' for details.
+
+You may also credit the validation fee towards 'arpa' membership should
+you decide to join 'arpa' within 30 days of validating your account
+
+
+
+
+To see what you get as a validated member, type '
+user
+
+200mb
+
+'
+For Lifetime ARPA membership to SDF via paypal, type 'arpa'
+To see a list of UNIX commands you can use *NOW*, type '
+unix
+
+
+
+          - Change your password
+ disk
+          - show current disk usage
+ uptime            - show system status
+
+ df          -m
+       - print system storage
+ freeirc
+
+
+exit
+
+
+'
+To view user contributed tutorials, visit http://sdf.org/?tutorials
+
+
+
+
+' for more info!
+
+
+
+sdf:/sdf/udd/m/martinmilan> mail
+
+=================================================
+You are entering restricted mail.  Please read...
+=================================================
+When saving a message while sending, the editor
+will prompt 'File Name to write' give it '.' then
+'y' to OVERWRITE so you can send your message.
+
+(continue)
+
+
+=======================================================================
+SDF host uptime report for Seattle WA, Dallas TX (USA) and Germany (EU)
+             Please use 'tty.sdf.org' for general access
+=======================================================================
+
+vscode ➜ ~ $ ssh new@sdf.org
+The authenticity of host 'sdf.org (205.166.94.16)' can't be established.
+ED25519 key fingerprint is SHA256:ZjwbO7AU8rHJExYrmZS2LqGZ7WfdoELfMrF54W92PYA.
+This key is not known by any other names
+Are you sure you want to continue connecting (yes/no/[fingerprint])?
+
+yes
+
+
+
+Warning: Permanently added 'sdf.org' (ED25519) to the list of known hosts.
+
+You will now be connected to NEWUSER mkacct server.
+Please login as '
+new
+' password 'new' when prompted.
+
+
+[RETURN]
+THIS MAY TAKE A MOMENT .. Trying 10.0.0.22...
+Connected to ol1.
+Escape character is 'off'.
+
+NetBSD/amd64 (mx) (pts/16)
+
+
+
+PLEASE READ THIS CAREFULLY:
+
+You are about to create a UNIX shell account.  This account may be unlike
+anything you've used before.  We urge you to carefully read all the text
+displayed on your terminal, as it will aide you in your learning.
+We also encourage you to try all the commands available with your new
+account.  There are many types of games, applications and utilities
+you will be able to instantly run in just a few moments.  If you are
+looking for a particular command or version of a command that we do not
+have, there are ways to request that it be installed.  We also offer DIALUP
+and DSL in the USA and Canada which you will be able to learn about
+shortly.  Be patient, read what is displayed - Explore and Enjoy!
+
+[RETURN]
+
+
+
+
+
+
+
+martinmilan
+
+
+
+
+
+
+
+PLEASE READ THIS CAREFULLY:
+   **limitations and policy**
+     ___________________________________________________________________________
+
+   The SDF Public Access UNIX System makes no guarantees in the reliability
+   or preservation of user accounts, email sent or received and files uploaded
+   or created by online editing or compilation.  That being said, SDF makes a
+   good effort to perform regular back ups and will restore from those back
+   ups as quickly as possible in the event of a catastrophic hardware failure.
+
+
+ Lifetime ARPA membership is based on the lifetime of SDF, not of the
+   user and is non-transferable.  The SDF has existed since 1987 and with
+   your support it will continue to be here.
+
+   Members of the SDF Public Access UNIX System are expected to conduct
+   themselves in an appropriate and reasonable manner when using our facilities.
+
+   Illegal activities which include, but are not limited to, e-mail spamming,
+   portflooding, unauthorized portscanning, unauthorized encryption cracking,
+   unauthorized storing and distribution of copyrighted data and unauthorized
+   connections to remote hosts are not allowed.  Your cooperation and care in
+   the use of all aspects of the SDF is greatly appreciated.
+
+
+Please answer the following questions as you see fit.
+
+CTRL-U will clear input ..
+
+
+
+
+Your Name:     Martin Milan
+Year of Birth:      1975
+Postal Zip Code:
+
+Are you a K-12, University faculty member or student? (y/n) NO
+Are you an Amateur Radio Operator? (y/n) NO
+Are you an Amateur or Professional DJ? (y/n) NO
+% Check out https://aNONradio.net where SDFers stream their own DJ music.
+Do you play Minecraft, MUDs, MOOs or MMORPG? (y/n) YES
+
+% SDF has its own home grown MOO!  You will soon be able to
+  access it from your new shell account.  There are many
+  areas where all players can build SECTORS and OBJECTS.
+  Just type 'thxmoo' when you get to the shell prompt.
+
+  SDF has also hosted John Gonnerman's 'Multi-User Dungeon'
+  since 1991.  Check 'man mdg' for full details on the game.
+
+  SDFmud is accessible via 'mud' (based on SMAUG code) and has
+  a very long history going back to the 1990s.  We're always
+  looking for creative types to develop it.
+
+  New on the scene is MINECRAFT.  We offer a multi-verse
+  semi-private server which you can invite your own friends to.
+  There are games, creative, tekkit, survival and so on.
+
+
+
+
+
+
+
+
+
+
+at Hacker News ... a discussion about Telehack.com
+
+
+
+multiuser Unix
+MMO
+
+
+
+Your SDF Service addresses - PLEASE SAVE THESE!
+========================================================
+ssh/telnet/ftp      : tty.sdf.org
+IRC                 : irc.sdf.org
+Jabber XMPP         : jabber.sdf.org
+Matrix Homeserver   : matrix.sdf.org
+WWW URL             : http://martinmilan.sdf.org
+WWW Gopher          : gopher://sdf.org/users/martinmilan
+WWW Gemini          : gemini://sdf.org/users/martinmilan
+Webmail             : https://mx.sdf.org
+Deskshots           : https://deskshots.org
+Image Gallery       : https://sdf.org/g
+SDF Peertube        : https://toobnix.org
+SDF PixelFed        : https://pixelfed.sdf.org
+SDF Mastodon        : https://mastodon.sdf.org
+SDF Jitsi-Meet      : https://jitsi.sdf.org
+SDF Gitea           : https://git.sdf.org
+SDF TT-RSS          : https://ttrss.sdf.org
+MineCraft           : mc.sdf.org
+BZFlag              : sdf.org:5154
+SDFMud (SMAUG)      : http://sdf.org/mud
+Community Radio     : http://aNONradio.net
+========================================================
+
+Login          : martinmilan Martin Milan
+E-mail         : martinmilan@sdf.org
+
+Create the 'martinmilan' account? (y/n
+
+
+lease set an initial password of up to 8 charaters.  Once you have
+logged in via SSH, you will be able to set a longer password using
+the 'passwd' command.  If you try to set an empty or blank password
+now, you will not be able to login securely via SSH.
+
+
+
+
+New password:
+Re-type new password:
+Adding password for user martinmilan
+
+Creating login, please wait [NegotiatingSearching for a unique numeric userid.
+] Established!
+Sending update, please wait [Negotiating] Established!
+Creating your directories /sdf/udd/m/martinmilan, /www/m/martinmilan
+Welcome to the SDF, martinmilan!
+
+If you'd like, please consider plotting a point on the SDF world user map:
+http://sdf.org/map
+
+If you use Twitter, Instagram or Twitch, please follow sdf_pubnix / sdfpubnix
+We always follow those who retweet, favorite and comment.
+
+We also run our own Mastodon service at https://mastodon.sdf.org'
+
+
+
+(continue)
+Picking a host to login to .. using faeroes.sdf.org
+Attempting a secure connection to faeroes.sdf.org ..
+(If this doesn't work, connect back using 'tty.sdf.org')
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@    WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!     @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+IT IS POSSIBLE THAT SOMEONE IS DOING SOMETHING NASTY!
+Someone could be eavesdropping on you right now (man-in-the-middle attack)!
+It is also possible that a host key has just been changed.
+The fingerprint for the ED25519 key sent by the remote host is
+SHA256:ZjwbO7AU8rHJExYrmZS2LqGZ7WfdoELfMrF54W92PYA.
+Please contact your system administrator.
+Add correct host key in /sys/new/.ssh/known_hosts to get rid of this message.
+Offending RSA key in /sys/new/.ssh/known_hosts:1
+ED25519 host key for faeroes has changed and you have requested strict checking.
+Host key verification failed.
+Connection closed by foreign host.
+Connection to sdf.org closed.
+vscode ➜ ~ $'
+
+
+
+7410 guestbook entries.
+
+Type 'help' for Commands.
+Type 'com' to chat with other users.
+Type 'ttytter' to listen to Twitter Tweets anonymously.
+Type 'mud' to play the SDFmud.
+Type 'mkhomepg' to set up your personal website.
+
+
+sdf:/sdf/udd/m/martinmilan>
+
+
+
+echo *
+
+ls --help
+man ls
+
+pwd
+ls  /
+ls -l /
+ls  ../../..
+ls -l  ../../..
+set
