015679bc5b2fd4a640bdd00a5eeb28b1db4e9d33
Martin Milan <martin12333@users.noreply.github.com>
2023-02-12 00:04:56 +0100

    add

--- f8-notebooks/win-disk-c-full.f8.ps1
+++ f8-notebooks/win-disk-c-full.f8.ps1
@@ -2,6 +2,23 @@ win-disk-c-full.f8.ps1
 
 
 
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+
+podman start   -ai    cmy22b
+
+echo '-------- section --------'
+
+
 
 cd "C:\Users\marti\AppData\Local\Microsoft\Edge\User Data\Profile 1"
 
