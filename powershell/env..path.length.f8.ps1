56a4350e93a16ae3895c9ae802ff9a629c505828
Martin Milan <martin12333@users.noreply.github.com>
2023-02-12 01:37:18 +0100

    ...

--- powershell/env..path.length.f8.ps1
+++ powershell/env..path.length.f8.ps1
@@ -22,7 +22,11 @@ $env:Path.Length
 $env:Path
 
 
-
+$env:Path.Length
+1018
+PS C:\Users\marti\OneDrive> $env:Path
+C:\Program Files\PowerShell\7;D:\conda;D:\conda\Library\mingw-w64\bin;D:\conda\Library\usr\bin;D:\conda\Library\bin;D:\conda\Scripts;D:\conda\bin;D:\conda\condabin;C:\pf\PowerShell\7;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\WINDOWS\System32\OpenSSH;C:\Program Files (x86)\Common Files\Acronis\VirtualFile;C:\Program Files (x86)\Common Files\Acronis\VirtualFile64;C:\Program Files (x86)\Common Files\Acronis\FileProtector;C:\Program Files (x86)\Common Files\Acronis\FileProtector64;C:\Program Files (x86)\Common Files\Acronis\SnapAPI;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\nodejs;C:\Program Files\dotnet;C:\Program Files (x86)\Windows Kits\10\Microsoft Application Virtualization\Sequencer;C:\Program Files\PowerShell\7;C:\Program Files\RedHat\Podman;C:\Program Files\Tailscale;C:\Users\marti\AppData\Local\Microsoft\WindowsApps;C:\Users\marti\AppData\Local\GitHubDesktop\bin;C:\Users\marti\AppData\Roaming\npm;C:\Users\marti\.dotnet\tools;.
+PS C:\Users\marti\OneDrive> 
 
 
 
