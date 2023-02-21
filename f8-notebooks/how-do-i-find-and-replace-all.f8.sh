81e06f3fe24b8668a8b82c19aec88962b6f89b29
Martin Milan <martin12333@users.noreply.github.com>
2023-02-21 05:13:23 +0100

    before-find-and-replace

--- f8-notebooks/how-do-i-find-and-replace-all.f8.sh
+++ f8-notebooks/how-do-i-find-and-replace-all.f8.sh
@@ -27,10 +27,15 @@ grep  --no-filename  f8_document `  find -name '*.f8.*' -type f     `   |sort|un
 
 
 
-C:\Users\marti\OneDrive\f8-notebooks\how-do-i-find-and-replace-all-occurrences-in-all-files-in-visual-studio-code.txt
 
 
 
+old phrases can be restored ffrom clipboard-history and git---history
+#mm_f8_doc
+f8_document
+#(f8_document file pseudo-format)#
+
+# C:\Users\marti\OneDrive\f8-notebooks\how-do-i-find-and-replace-all-occurrences-in-all-files-in-visual-studio-code.txt
 
 	git add -vv  '*.f8.*'
 
@@ -50,6 +55,28 @@ Invoke Replace in Files (under the Edit menu, or with shortcut Ctrl+Shift+H)
 mm_f8_doc
 f8_document
 *.f8.*
+
+
+
+Ctrl+enter
+1
+(f8_document file pseudo-format)
+Ctrl+enter
+
+2
+
+Ctrl+enter
+f8_document #
+Ctrl+enter
+
+3
+*.f8.*
+
+54
+https://github.com/martin12333/marti-onedrive/blob/830c0151ebfffa4cfa49f75f8178df812499f13b/backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh#L3
+
+https://github.com/martin12333/marti-onedrive/blob/main2/backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh#L3
+
 
 
 
