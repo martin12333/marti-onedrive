4db205f2447a5e6ee6895c82aad706ac17d82644
Martin Milan <martin12333@users.noreply.github.com>
2023-02-21 03:21:04 +0100

    before-refactor

--- f8-notebooks/how-do-i-find-and-replace-all.f8.sh
+++ f8-notebooks/how-do-i-find-and-replace-all.f8.sh
@@ -7,11 +7,15 @@ grep  --no-filename  mm_f8_doc `  find -name '*.f8.*' -type f     `   |sort|uniq
 
 
 
-git commit
+	git add -vv  '*.f8.*'
+
+	git commit -v -a -m  before-refactor
 
 git push
 
 acronis backup
+
+
 
 Invoke Replace in Files (under the Edit menu, or with shortcut Ctrl+Shift+H)
 
