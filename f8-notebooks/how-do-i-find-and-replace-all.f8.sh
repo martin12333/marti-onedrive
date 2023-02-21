7582f9ea582e079f5da9f3b525b827bfaac8025d
Martin Milan <martin12333@users.noreply.github.com>
2023-02-21 03:32:12 +0100

    before-refactor

--- f8-notebooks/how-do-i-find-and-replace-all.f8.sh
+++ f8-notebooks/how-do-i-find-and-replace-all.f8.sh
@@ -1,9 +1,28 @@
 
+
+# (a file in the mm_f8_doc pseudo-format)
+do-not run this file as a whole
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+
+mm_f8_doc
+"f8_document"
+
+google search
+github
+
+
+
+
 find -name '*.f8.*' -type f | grep ' '
 
-
---recursive
 grep  --no-filename  mm_f8_doc `  find -name '*.f8.*' -type f     `   |sort|uniq -c
+
 
 
 
@@ -16,10 +35,15 @@ git push
 acronis backup
 
 
+https://stackoverflow.com/questions/37346481/how-do-i-find-and-replace-all-occurrences-in-all-files-in-visual-studio-code
 
 Invoke Replace in Files (under the Edit menu, or with shortcut Ctrl+Shift+H)
 
 
+
+mm_f8_doc
+f8_document
+*.f8.*
 
 
 
