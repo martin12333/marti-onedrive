188d4b2ab7506b0fb70d7bbe31488107544a88ac
Martin Milan <martin12333@users.noreply.github.com>
2023-02-25 06:18:53 +0100

    ...

--- json-jq/google-takeout.jq.f8.sh
+++ json-jq/google-takeout.jq.f8.sh
@@ -1,10 +1,32 @@
 google-takeout.jq.f8.sh
 
 
+# f8_text_file
+# (an f8_text_file is-not meant to be run as a whole)
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+# i use  a container
+podman machine start
+
+podman start   -ai    cmy22b
+
+wsl -d Ubuntu-22.04
+
+echo '-------- section --------'
 
 
 
-apt
+
+
+apt show jq
+1.6
 sudo apt-get install jq
+
+
 
 
