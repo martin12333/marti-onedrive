5e02c7c9e513e19b2ce78b2bc97e953745dfd772
Martin Milan <martin12333@users.noreply.github.com>
2023-03-02 11:45:21 +0100

    bug-found

--- f8-notebooks--older/powershell-pwsh-vscode.f8.ps1
+++ f8-notebooks--older/powershell-pwsh-vscode.f8.ps1
@@ -17,10 +17,23 @@ PowerShell: Show Integrated Console
 	"key": "",
 	"command": "PowerShell.ShowSessionConsole"
   }
+
+
+
+
+is-open
+  Feature Request: Call VSCode commands from $psEditor variable · Issue #804 · PowerShell/vscode-powershell
+
+
 =====> PowerShell Integrated Console v2022.6.3 <=====
 
 
+
+
+
 {"text":"$psEditor.GetEditorContext().CurrentFile.InsertText($outputString)","dateAdded":1652191388619,"length":66,"sourceUrl":"https://www.google.com/search?q=%24psEditor.GetEditorContext().CurrentFile.InsertText(%24outputString)&oq=%24psEditor.GetEditorContext().CurrentFile.InsertText(%24outputString)&aqs=edge..69i57.1369j0j4&sourceid=chrome&ie=UTF-8"},{"text":"Visual Studio Code $psise equivalent ... $psEditor ","dateAdded":1652191346095,"length":51,"sourceUrl":"https://www.reddit.com/r/Windows_diaries/submit"},{"text":"psEditor ","dateAdded":1652191320575,"length":9,"sourceUrl":"https://stackoverflow.com/questions/44265842/visual-studio-code-psise-equivalent"},{"text":"https://stackoverflow.com/questions/44265842/visual-studio-code-psise-equivalent","dateAdded":1652191234197,"dateLastCopied":1652191300701,"length":80,"sourceUrl":"https://stackoverflow.com/questions/44265842/visual-studio-code-psise-equivalent"},{"text":"Windows_diaries","dateAdded":1652191123829,"length":15,"sourceUrl":"https://www.reddit.com/search/?q=windows_diaries&type=link"},{"text":"windows_diaries","dateAdded":1652190942117,"dateLastCopied":1652195481040,"length":15,"sourceUrl":"https://www.reddit.com/search/?q=windows_diaries&type=sr"},{"text":"        $psEditor.GetEditorContext().CurrentFile.InsertText($outputString)\n","dateAdded":1652190695742,"length":75,"sourceUrl":"http://www.stayfocusd.com/?background&customMsg="},{"text":"cat  C:\\Users\\marti\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadLine\\ConsoleHost_history.txt | tee   C:\\Users\\marti\\OneDrive\\ps-mylen-mar\\vscode-Host_history.txt\n","dateAdded":1652132830887,"length":169},{"text":"cat  C:\\Users\\marti\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadLine\\ConsoleHost_history.txt | tee   C:\\Users\\marti\\OneDrive\\ps-mylen-mar\\ConsoleHost_history.txt   \n","dateAdded":1652132758193,"length":172},{"text":"C:\\Users\\marti\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadLine\\Visual Studio Code Host_history.txt\n","dateAdded":1652132702420,"length":107},{"text":"C:\\Users\\marti\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadLine\\Visual Studio Code Host_history.txt","dateAdded":1652132282298,"dateLastCopied":1652216841971,"length":106},
+
+
 
 
 
@@ -29,9 +42,16 @@ $psEditor
 $psEditor|select *
 
 $psEditor|  Get-Member
+
+$y=$PSEditor.GetCommands()
+$y.GetType()
+
 $psEditor|  Format-List -Property *
 $psEditor|  Format-List *
 $psEditor|  Format-List
+
+
+
 
 
 
@@ -53,16 +73,27 @@ fail
 $r=$PSEditor.GetEditorContext().SelectedRange
 #$r = $PSEditor.GetEditorContext().CurrentFile.FileRange
 ##$PSEditor.GetEditorContext().CurrentFile.GetTextLines($r)
-$PSEditor.GetEditorContext().CurrentFile.GetTextLines()
-$PSEditor.GetEditorContext().CurrentFile.GetText()
 $r.Start
 $r.End
 $PSEditor.GetEditorContext().
 
+
+
+$PSEditor.GetEditorContext().CurrentFile.GetTextLines()
+$PSEditor.GetEditorContext().CurrentFile.GetText()
+
+
+
+
 $x=$PSEditor.GetEditorContext().CurrentFile|Get-Member
+$x=$PSEditor.GetEditorContext()|Get-Member
+$x=$PSEditor|Get-Member
+
 $x
 $x[3].Definition
+$x[3].Definition
 $x[2].Definition
+
 
 
 $PSEditor.Window|Get-Member
@@ -96,11 +127,10 @@ $PSEditor.GetEditorContext().GetType()
 
 fail
 . "c:\Program Files\Microsoft VS Code\resources\app\out\vs\workbench\contrib\terminal\browser\media\shellIntegration.ps1"
-
 show-ps
 
 history
-history[0]
+#history[0]
 (history)
 (history)[0]
 (history)[0].CommandLine
@@ -126,8 +156,8 @@ code "c:\Program Files\Microsoft VS Code\resources\app\out\vs\workbench\contrib\
 
 
 
-
-
+##########################
+OLD
 
 C:\Users\marti\OneDrive\Dokumenty\PowerShell\profile.ps1
 32767
@@ -137,20 +167,10 @@ PS C:\Users\marti\OneDrive> . "c:\Users\marti\OneDrive\vscode-zsh-bash-gitbash-p
 True
 2
 3
-2
-$
-PS C:\Users\marti\OneDrive>
-PS C:\Users\marti\OneDrive> . "c:\Users\marti\OneDrive\vscode-zsh-bash-gitbash-pwsh\a.ps1"
-1
-True
-2
-c:\Users\marti\OneDrive\vscode-zsh-bash-gitbash-pwsh\a.ps1
-3
-2
-$
+###########################
 
-EditorServicesVersion Workspace
---------------------- ---------
-3.4.5.0               Microsoft.PowerShell.EditorService…
 
-PS C:\Users\marti\OneDrive>
+
+
+
+
