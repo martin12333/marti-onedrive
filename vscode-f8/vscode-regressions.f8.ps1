76c517e7d38ab1fdc26ff0bbf1fb4f1ac379b435
Martin Milan <martin12333@users.noreply.github.com>
2023-03-03 20:14:20 +0100

    bug-found

---
+++ vscode-f8/vscode-regressions.f8.ps1
@@ -0,0 +1,79 @@
+
+
+https://github.com/microsoft/vscode/issues/174822
+
+Yes, it seems to be fixed in 1.76.0-insider
+
+
+
+Version: 1.72.2 (system setup)
+Commit: d045a5eda657f4d7b676dedbfa7aab8207f8a075
+Date: 2022-10-12T22:15:18.074Z
+Electron: 19.0.17
+Chromium: 102.0.5005.167
+Node.js: 16.14.2
+V8: 10.2.154.15-electron.0
+OS: Windows_NT x64 10.0.22621
+Sandboxed: No
+
+
+respn
+pre uninst vsc 1 72 2
+
+
+win+x
+ise
+C:\Users\marti\OneDrive\vscode-f8\vscode-regressions.f8.ps1
+
+acronis
+
+git commit ; push
+
+
+
+
+
+D:\instu\vscode
+
+
+
+
+Destination location:
+      D:\instu\vscode
+
+Start Menu folder:
+      Visual Studio Code
+
+Additional tasks:
+      Additional icons:
+         Create a desktop icon
+      Other:
+         Add "Open with Code" action to Windows Explorer file context menu
+         Add "Open with Code" action to Windows Explorer directory context menu
+         Register Code as an editor for supported file types
+         Add to PATH (requires shell restart)
+
+
+
+
+relogin
+
+
+
+acronis
+
+git commit ; push
+
+multiline gitbash
+C:\Users\marti\OneDrive\vscode-f8\bug2333.ini
+
+
+$Env:Path
+
+
+
+
+respnt
+
+
+
