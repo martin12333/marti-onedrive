0d4adad015f3eb5ee584dc2f4e31a882a6b16394
Martin Milan <martin12333@users.noreply.github.com>
2023-03-08 22:45:42 +0100

    add

---
+++ redhat.vscode-didact/didact.f8.sh
@@ -0,0 +1,12 @@
+
+# f8_text_file
+# (an f8_text_file is-not meant to be run as a whole)
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
