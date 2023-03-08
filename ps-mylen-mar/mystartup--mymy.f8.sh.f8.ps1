0384e5332f6f03201e3d7a42b80db43c5069f1e4
Martin Milan <martin12333@users.noreply.github.com>
2023-03-08 18:32:33 +0100

    rename

---
+++ ps-mylen-mar/mystartup--mymy.f8.sh.f8.ps1
@@ -0,0 +1,38 @@
+
+# f8_text_file
+# (an f8_text_file is-not meant to be run as a whole)
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+
+echo '-------- section --------'
+
+
+
+
+
+
+
+
+ls -l ~/log/mymyrsync.*
+
+
+
+
+
+mkdir ~/log
+
+echo >~/log/mymyrsync.sh.log
+
+# file:///C:/Users/marti/log/mymyrsync.sh.log
+
+ls -l ~/log/mymyrsync.sh.log
+echo >~/log/mymyrsync.sh.err.log
+
+echo  'file:///C:/Users/marti/log/mymyrsync.sh.log'
+echo  'file:///C:/Users/marti/log/mymyrsync.sh.err.log'
+
+
