c300ac02d54c98356a41317ea32ba181f0fc57d5
Martin Milan <martin12333@users.noreply.github.com>
2023-03-08 23:12:30 +0100

    ...

--- redhat.vscode-didact/didact.f8.sh
+++ redhat.vscode-didact/didact.f8.sh
@@ -10,3 +10,33 @@ exit
 
 echo '-------- section --------'
 
+C:\pf\Git\bin\bash.exe
+
+cd
+
+ls  -d   *didact*
+
+#?($|[ `])`
+
+#grep -R --no-filename  -E -o "didact://[^ `\']{1,150}"    *didact*   |sort| uniq -c|  sort -n
+#'
+#`
+#\"
+grep -R --no-filename  -E -o ".?didact:[^ \`\')]{1,1500}"    *didact* | grep '^:'
+
+grep -R --no-filename  -E -o "[a-z-]{1,55}://[^ \`\')]{1,1500}"    *didact*  | cut -d ':' -f 1  |sort| uniq -c|  sort -n
+
+      1 hhttps
+      3 file
+     62 http
+     68 vscode
+    318 didact
+    785 https
+
+
+grep -R --no-filename  -E -o "(didact|vscode):[^ \`\')]{1,1500}"    *didact*  | cut -d ':' -f 1  |sort| uniq -c|  sort -n
+
+| grep '^:'
+
+ wc
+cut -c 1
