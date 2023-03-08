fb000997750937e26cd3b3cf9f4376e4b0f3784a
Martin Milan <martin12333@users.noreply.github.com>
2023-03-08 16:36:24 +0100

    add

---
+++ backup-f8/experim--mystartup,mymy,cp,cpio,tar.f8.sh
@@ -0,0 +1,38 @@
+
+# f8_text_file
+# (an f8_text_file is-not meant to be run as a whole)
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+
+echo '-------- section --------'
+
+podman machine start
+
+/c/Users/marti/OneDrive/git-bash-dotfiles/mymyrsync.sh
+#pwsh.exe -ExecutionPolicy Bypass -File  C:\Users\marti\OneDrive\ps-mylen-mar\mymyrsync.ps1
+
+echo '-------- section --------'
+
+
+
+
+
+# mymy*.* are  pseudo-pseudo-*.*
+# mymycron.*
+# mymyrsync.*
+
+
+
+search also
+mymyrsync.sh
+
+
+todo edit
+ls "C:\Users\marti\*robocopy*"
+ls "C:\Users\marti\OneDrive\*robocopy*"
+du -- *robocopy*
+du -- ../*robocopy*
+
