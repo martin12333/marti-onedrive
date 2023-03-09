c31b51a40142fa154966269ca250393abfb2aac0
Martin Milan <martin12333@users.noreply.github.com>
2023-03-09 18:59:37 +0100

    ...

--- powershell-f8/alias,function.f8.ps1
+++ powershell-f8/alias,function.f8.ps1
@@ -9,9 +9,36 @@ exit
 
 echo '-------- section --------'
 
+C:\pf\Git\bin\bash.exe
+x="c.:."
+x="c:\\.*[.]exe"
+x="c.:.\\..*[.].e.x.e"
+x="c.:.[\\/].*e.x"
+x="c:[\\/].*[.]exe"
+grep  --no-filename  -i -E -o "$x"  ` find . -name '*.f8.*' `   |sort   |uniq -c|  sort -n
+exit
 
+1 C:\Program Files\RedHat\Podman\win-sshproxy.exe
+1 C:\Windows\System32\OpenSSH\ssh.exe
+2 C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe
+3 C:\pf\totalcmd\TOTALCMD64.EXE
+3 C:\pf86\Minecraft Launcher\runtime\jre-legacy\windows-x64\jre-legacy\bin\java.exe
+4 C:\PF\Git\cmd\git.exe
+4 C:\Program Files\Git\cmd\git.exe
+4 C:\Windows\System32\tar.exe
 
+5 C:\Program Files\WindowsApps\Microsoft.SysinternalsSuite_2022.11.1.0_x64__8wekyb3d8bbwe\Tools\junction.exe
+junction.exe
+where.exe junction.exe
 
+7 C:\Program Files (x86)\Acronis\Agent\aakore.exe
+9 C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
+9 C:\Users\marti\AppData\Local\GitHubDesktop\app-2.6.5\resources\app\git\cmd\git.exe
+12 C:\gib\file.exe
+30 C:\pf\Git\cmd\git.exe
+126 C:\pf\Git\bin\bash.exe
+where.exe bash.exe
+where.exe  git.exe
 
 
 
@@ -20,8 +47,13 @@ Set-Alias -Name np -Value C:\Windows\notepad.exe
 
 nodeos\10-podman-import.f8.ps1.sh:
   44  ###c:\gib\bash -c git
-  45: dir c:\pf\git\cmd
-  46: dir c:\pf\git\bin
+   dir c:\pf\git\cmd
+
+   -a---          02/09/2021    12:57           3022 start-ssh-agent.cmd
+   -a---          02/09/2021    12:57           2723 start-ssh-pageant.cm
+													 d
+
+  dir c:\pf\git\bin
   47:
   c:\pf\git\bin\git
   clone https://github.com/nodeos/nodeos
