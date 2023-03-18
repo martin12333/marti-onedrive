644df1ea22be4cf6591cee743e23d917a9a677d2
Martin Milan <martin12333@users.noreply.github.com>
2023-03-18 07:04:38 +0100

    add

--- windows-f8/Power-Options.f8.ps1
+++ windows-f8/Power-Options.f8.ps1
@@ -3,10 +3,12 @@ Power-Options.f8.ps1
 exit
 
 Control Panel\Hardware and Sound\Power Options
+Control Panel\Hardware and Sound\Power Options\Edit Plan Settings
 
 
-
-
+respnt
+pre-disable-wake-timers
+35,disable
 
 
 
