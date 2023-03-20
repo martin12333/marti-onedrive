11e3f425b912785527c994a475d308c0378c44b9
Martin Milan <martin12333@users.noreply.github.com>
2023-03-20 05:28:44 +0100

    refactor

--- backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
+++ backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
@@ -179,52 +179,9 @@ cd ~/OneDrive
 	git commit -v -a -m maybe-enhance
 	git commit -v -a -m important-test
 
-🌞^ 😋☺🙂 ☀ ☺☼⚙⛭⛮ 🌞 🌣🛪😲😊😋😌😁😃😄😆🙂😇🙏🙇😌
+code vscode-f8\Emojis-in-Integrated-Terminal.f8.ps1
 
-todo
-: 1651088089:0;while read -k 1 x ;do echo -n "$x"|od -x ; done
-
-chcp /?
-chcp
-where.exe chcp
-
-	git commit -v -a -m "low emojis[☀] become empty?string if f8-sent into a  PS7 terminal "
-
-surrogate uD83C
-🌞
-	git commit -v -a -m " emojis[🌞] become � + sometimes no errmsg if f8-sent into a  PS7 terminal ?? "
-	git commit -v -a -m " emojis[🌞] become � + errmsg if f8-sent into a  PS7 terminal ? "
-
-Oops, something went wrong.
-Please report this bug with ALL the details below, including both the 'Environment' and 'Exception' sections.
-Please report on GitHub: https://github.com/PowerShell/PSReadLine/issues/new?template=Bug_Report.md
-System.Text.EncoderFallbackException: Unable to translate Unicode character \\uD83C at index 0 to specified code page.
-
-echo "☀" |c:\pf\git\usr\bin\od -x
- "☀" |c:\pf\git\usr\bin\od -x
-echo "☀" | od -x
-##c:\pf\git\usr\bin\od -x <<eof
-od -x <<eof
-☀
-
-🌞
-
-eof
- "🌞".length
- "🌞" |c:\pf\git\usr\bin\od -x
-echo "🌞" |c:\pf\git\usr\bin\od -x
-echo "🌞" | od -x
-# ☀ is OK in git bash git
-# 🌞 becomes �
-	git log
-q
-Date:   Sun Mar 12 01:57:14 2023 +0100
-    ☀ fix-comments
-Date:   Sun Mar 12 01:55:40 2023 +0100
-     fix-comments
-
-todo see the commit log at github
-
+errmsg(s) stopped because PSReadLine crashed
 
 cd ~/OneDrive
 	git add -vv  '*.f8.*'
