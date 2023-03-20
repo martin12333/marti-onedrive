cf4b16aeea3d1ad8d9f4dba6e0697e2bdfa576a4
Martin Milan <martin12333@users.noreply.github.com>
2023-03-20 09:18:46 +0100

    add

---
+++ git-f8/git-from-the-inside-out.f8.sh
@@ -0,0 +1,20 @@
+exit
+
+
+https://maryrosecook.com/blog/post/git-from-the-inside-out
+git-from-the-inside-out
+
+mkdir git-from-the-inside-out
+cd git-from-the-inside-out
+#mkdir alpha
+#cd alpha
+#cd ..
+code .
+
+
+git init
+
+echo 'a' > letter.txt
+git add -v .
+
+git log
