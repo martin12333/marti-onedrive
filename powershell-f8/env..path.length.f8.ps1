53b2844a7adf8f3792a36d4d5f7543d2adb155fb
Martin Milan <martin12333@users.noreply.github.com>
2023-03-29 20:08:43 +0200

    � it-works

--- powershell-f8/env..path.length.f8.ps1
+++ powershell-f8/env..path.length.f8.ps1
@@ -6,7 +6,12 @@ exit
 
 (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
 
+
+ (eepitch-shell)
+
+
 echo '-------- section --------'
+
 
 
 
@@ -21,6 +26,8 @@ characters
 
 setx command: WARNING: The data being saved is truncated to 1024 characters
 
+
+
 echo '-------- section --------'
 
 resto.point
@@ -31,13 +38,17 @@ resto.point
 
 echo '-------- section --------'
 
-see also
+a mitigation 
+to reduce
+path length
 C:\Users\marti\OneDrive\powershell-f8\alias,function.f8.ps1
+
+echo '-------- section --------'
+
 
 
 $env:Path.Length
 $env:Path
-
 
 $env:Path.Length
 1018
@@ -46,6 +57,63 @@ C:\Program Files\PowerShell\7;D:\conda;D:\conda\Library\mingw-w64\bin;D:\conda\L
 PS C:\Users\marti\OneDrive>
 
 
+
+
+$oldPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
+$oldPath
+$oldPath2 = [Environment]::GetEnvironmentVariable('PATH', 'user');
+$oldPath2
+$oldPath.Length + $oldPath2.Length
+
+$env:Path.Length
+
+
+
+cmd
+
+echo %path%
+echo %path%%path%
+echo %path%%path%%path%%path%
+echo %path%%path%%path%%path%%path%%path%%path%%path%
+echo %path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
+
+(base) C:\Users\marti\OneDrive>echo %path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
+The input line is too long.
+
+
+set /?
+set x=%path%%path%%path%%path%
+set x=%path%%path%%path%%path%%path%%path%%path%%path%
+set x=%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
+
+(base) C:\Users\marti\OneDrive>set x=%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
+The input line is too long.
+
+
+SETX /?
+
+
+https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/setx
+
+Be aware there's a limit of 1024 characters when assigning contents to a variable using setx.
+
+This means that the content is cropped if you go over 1024 characters, and that the cropped text is what's applied to the target variable. If this cropped text is applied to an existing variable, it can result in loss of data previously held by the target variable.
+
+
+
+setx  MYPATH_test    %PATH%
+ERROR: Invalid syntax. Default option is not allowed more than '2' time(s).
+spaces?
+
+##echo  ~PATH~~PATH~
+
+
+
+echo "%PATH%"
+echo "%PATH%%PATH%"
+setx MYPATH_test  "%PATH%%PATH%"
+
+WARNING: The data being saved is truncated to 1024 characters.
 
 
 
@@ -73,42 +141,3 @@ User Variables
 HKEY_CURRENT_USER\Environment
 
 
-
-
-
-$oldPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
-$oldPath
-$oldPath2 = [Environment]::GetEnvironmentVariable('PATH', 'user');
-$oldPath2
-$oldPath.Length + $oldPath2.Length
-
-$env:Path.Length
-
-
-cmd
-
-echo %path%
-echo %path%%path%
-echo %path%%path%%path%%path%
-echo %path%%path%%path%%path%%path%%path%%path%%path%
-echo %path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
-
-(base) C:\Users\marti\OneDrive>echo %path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
-The input line is too long.
-
-
-set /?
-set x=%path%%path%%path%%path%
-set x=%path%%path%%path%%path%%path%%path%%path%%path%
-set x=%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
-
-(base) C:\Users\marti\OneDrive>set x=%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
-The input line is too long.
-
-
-
-Try SET /? and SETX /? - that should give you some ideas. –
-Iszi
- May 19, 2015 at 12:33
-6
-Also, see SS64 references for SET and SETX. SS64 is a great command reference for several languages and command environments - I find it particularly handy for CMD & PowerShell.
