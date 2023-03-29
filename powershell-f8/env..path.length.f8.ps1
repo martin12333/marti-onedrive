4ae3a1cfe2acc1d45eb2a8c50aa4115c3e5e1bf7
Martin Milan <martin12333@users.noreply.github.com>
2023-03-29 21:52:22 +0200

    done

--- powershell-f8/env..path.length.f8.ps1
+++ powershell-f8/env..path.length.f8.ps1
@@ -69,26 +69,6 @@ echo '-------- section --------'
 
 
 
-$env:Path.Length
-$env:Path
-
-$env:Path.Length
-1018
-PS C:\Users\marti\OneDrive> $env:Path
-C:\Program Files\PowerShell\7;D:\conda;D:\conda\Library\mingw-w64\bin;D:\conda\Library\usr\bin;D:\conda\Library\bin;D:\conda\Scripts;D:\conda\bin;D:\conda\condabin;C:\pf\PowerShell\7;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\WINDOWS\System32\OpenSSH;C:\Program Files (x86)\Common Files\Acronis\VirtualFile;C:\Program Files (x86)\Common Files\Acronis\VirtualFile64;C:\Program Files (x86)\Common Files\Acronis\FileProtector;C:\Program Files (x86)\Common Files\Acronis\FileProtector64;C:\Program Files (x86)\Common Files\Acronis\SnapAPI;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\nodejs;C:\Program Files\dotnet;C:\Program Files (x86)\Windows Kits\10\Microsoft Application Virtualization\Sequencer;C:\Program Files\PowerShell\7;C:\Program Files\RedHat\Podman;C:\Program Files\Tailscale;C:\Users\marti\AppData\Local\Microsoft\WindowsApps;C:\Users\marti\AppData\Local\GitHubDesktop\bin;C:\Users\marti\AppData\Roaming\npm;C:\Users\marti\.dotnet\tools;.
-PS C:\Users\marti\OneDrive>
-
-
-
-
-$oldPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
-$oldPath
-$oldPath2 = [Environment]::GetEnvironmentVariable('PATH', 'user');
-$oldPath2
-$oldPath.Length + $oldPath2.Length
-
-$env:Path.Length
-
 
 
 cmd
@@ -100,7 +80,11 @@ echo %path%%path%%path%%path%%path%%path%%path%%path%
 echo %path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
 
 (base) C:\Users\marti\OneDrive>echo %path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
+
 The input line is too long.
+
+
+echo '-------- section --------'
 
 
 set /?
@@ -109,7 +93,11 @@ set x=%path%%path%%path%%path%%path%%path%%path%%path%
 set x=%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
 
 (base) C:\Users\marti\OneDrive>set x=%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%%path%
+
 The input line is too long.
+
+
+echo '-------- section --------'
 
 
 SETX /?
@@ -127,7 +115,7 @@ setx  MYPATH_test    %PATH%
 ERROR: Invalid syntax. Default option is not allowed more than '2' time(s).
 spaces?
 
-##echo  ~PATH~~PATH~
+###echo  ~PATH~~PATH~
 
 
 
@@ -139,24 +127,49 @@ WARNING: The data being saved is truncated to 1024 characters.
 
 
 
+echo '-------- section --------'
+
+powershell
+
+$env:Path.Length
+$env:Path
+
+
+$env:Path.Length
+1018
+PS C:\Users\marti\OneDrive> $env:Path
+C:\Program Files\PowerShell\7;D:\conda;D:\conda\Library\mingw-w64\bin;D:\conda\Library\usr\bin;D:\conda\Library\bin;D:\conda\Scripts;D:\conda\bin;D:\conda\condabin;C:\pf\PowerShell\7;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\WINDOWS\System32\OpenSSH;C:\Program Files (x86)\Common Files\Acronis\VirtualFile;C:\Program Files (x86)\Common Files\Acronis\VirtualFile64;C:\Program Files (x86)\Common Files\Acronis\FileProtector;C:\Program Files (x86)\Common Files\Acronis\FileProtector64;C:\Program Files (x86)\Common Files\Acronis\SnapAPI;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\nodejs;C:\Program Files\dotnet;C:\Program Files (x86)\Windows Kits\10\Microsoft Application Virtualization\Sequencer;C:\Program Files\PowerShell\7;C:\Program Files\RedHat\Podman;C:\Program Files\Tailscale;C:\Users\marti\AppData\Local\Microsoft\WindowsApps;C:\Users\marti\AppData\Local\GitHubDesktop\bin;C:\Users\marti\AppData\Roaming\npm;C:\Users\marti\.dotnet\tools;.
+PS C:\Users\marti\OneDrive>
+
+
+
+
+$oldPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
+$oldPath
+$oldPath2 = [Environment]::GetEnvironmentVariable('PATH', 'user');
+$oldPath2
+$oldPath.Length + $oldPath2.Length
 
 
 
 
 
 
-registry
 
- registry location that contains the system paths
- HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment Must read the rest of it there.
+echo '-------- section --------'
 
- set and setx commands truncate values to 1023 characters.
-Setting directly in the registry at
-HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment fails since regedit truncates entered strings after 2047 characters.
+
+
+quotes from the webpages
+
+HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment
 
 "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 
-HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
+
+HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment fails since regedit truncates entered strings after 2047 characters.
+# ??
+# a *loader* truncates?
 
 
 User Variables
