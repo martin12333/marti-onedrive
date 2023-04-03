ca343dc4090943c0a4cbe8082b2d3950d630e78b
Martin Milan <martin12333@users.noreply.github.com>
2023-04-03 18:55:28 +0200

    add

---
+++ windows--legacy/wine.f8.sh
@@ -0,0 +1,60 @@
+
+exit
+
+wsl -d Ubuntu-22.04
+
+sudo apt update
+
+
+
+
+
+sudo apt install  libwine
+y
+  gstreamer1.0-plugins-base
+y
+
+libz-mingw-w64
+
+
+sudo apt install wine
+sudo apt install wine64
+The following NEW packages will be installed:
+  wine wine64
+n
+
+
+
+
+sudo apt install wine-stable
+
+Description: Windows API implementation (transitional package)
+ This is a transitional dummy package to complete the migration to the Debian
+ wine packages. It can be safely removed.
+
+➜  OneDrive
+
+
+
+apt show  winetricks
+apt download winetricks
+
+
+ Winetricks lets you install missing DLLs or tweak various Wine settings
+ individually.  It also has a menu of supported games/apps for which it
+ can do all the workarounds automatically.
+ .
+ It can be used via GUI or commandline, whichever you prefer; the
+
+
+
+i am not a c++ developer
+➜  OneDrive sudo apt install wine
+wine                          wine64-tools
+wine64                        wine-binfmt
+wine64-development            wine-development
+wine64-development-preloader  wine-stable
+wine64-development-tools      winetricks
+wine64-preloader
+
+
