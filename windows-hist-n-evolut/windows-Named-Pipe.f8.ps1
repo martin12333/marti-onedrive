baa4dd9a2c53892e3d0ccb3e56323b84358e68cc
Martin Milan <martin12333@users.noreply.github.com>
2023-04-05 00:59:42 +0200

    pipelist

--- windows-hist-n-evolut/windows-Named-Pipe.f8.ps1
+++ windows-hist-n-evolut/windows-Named-Pipe.f8.ps1
@@ -6,6 +6,27 @@ pipelist.exe  /?
 
 
 
+Winsock2\CatalogChangeListener-494-0              1                1
+ProtectedPrefix\Administrators\Tailscale\tailscaled          5               -1
+AcronisFCDPService                               13               -1
+PSHost.133246648871844728.17368.DefaultAppDomain.pwsh          1                1
+msys-1888ae32e00d56aa-1784-sigwait
+conpty-9267912.518337209-in                       1                1
+conpty-9267912.518337209-out                      1                1
+conpty-9267912.518337209-out-worker               5               -1
+vscode-git-2641a1020e-sock                        4               -1
+docker_engine                                     2               -1
+podman-machine-default                            2               -1
+1                1
+powertoys_runner_6c93f926-744a-4ed2-8c70-e065f0ce85a8          1               -1
+dotnet-diagnostic-15248                           1               -1
+powertoys_settings_6c93f926-744a-4ed2-8c70-e065f0ce85a8
+wsl_debugshell_S-1-5-21-2941743733-2172176398-4109959614-1011          1                1
+
+
+
+
+
 
 job-object id 868
 
@@ -197,11 +218,15 @@ PSHost.133251195140674937.16428.DefaultAppDomain.pwsh          1                
 
 pipelist.exe  >8
 
+pipelist.exe  >.\windows-hist-n-evolut\windows-Named-Pipelist.not.f8.txt
+
+
+git add -vv  '*.f8.*'
+git commit -v -a -m pipelist
+git push
 
 
 
-bash
-exit
 
 
 
@@ -209,40 +234,6 @@ exit
 
 
 
-
-Winsock2\CatalogChangeListener-494-0              1                1
-
-ProtectedPrefix\Administrators\Tailscale\tailscaled          5               -1
-
-AcronisFCDPService                               13               -1
-
-
-PSHost.133246648871844728.17368.DefaultAppDomain.pwsh          1                1
-
-
-msys-1888ae32e00d56aa-1784-sigwait
-
-
-
-conpty-9267912.518337209-in                       1                1
-conpty-9267912.518337209-out                      1                1
-conpty-9267912.518337209-out-worker               5               -1
-
-
-vscode-git-2641a1020e-sock                        4               -1
-
-
-
-docker_engine                                     2               -1
-podman-machine-default                            2               -1
-
-
-1                1
-powertoys_runner_6c93f926-744a-4ed2-8c70-e065f0ce85a8          1               -1
-dotnet-diagnostic-15248                           1               -1
-powertoys_settings_6c93f926-744a-4ed2-8c70-e065f0ce85a8
-
-wsl_debugshell_S-1-5-21-2941743733-2172176398-4109959614-1011          1                1
 
 
 
