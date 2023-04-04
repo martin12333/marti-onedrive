1153f01f512decdacdd59955a16313b4792d5417
Martin Milan <martin12333@users.noreply.github.com>
2023-04-05 00:22:26 +0200

    add

--- windows-hist-n-evolut/windows-Named-Pipe.f8.ps1
+++ windows-hist-n-evolut/windows-Named-Pipe.f8.ps1
@@ -7,6 +7,31 @@ pipelist.exe  /?
 
 
 
+job-object id 868
+
+
+
+
+ps -ef
+
+
+pslist.exe -t
+
+Code                         23436   8  33  933 2173448612   66036   53040
+Code                       12372   8  24  400 2181648060   64692   55504
+  Code                      7016   8  14  224 2173165572   22776   24696
+  Code                     20868   8  17  269 2173190528   51432   49832
+	conhost                10624   8   6  112 2151766536    7488    1308
+	pwsh                   11676   8  40  898 2152600112  123576   90516
+	conhost                18828   8   5  104 2151766024    7212    1240
+	conhost                27704   8   7  108 2151766848    7396    1300
+	pwsh                   31316   8  17  725 2152505580   85912   46392
+	  bash                 26412   8   2   82 4285108    5364    1372
+		bash               26288   8   4  178 4815236   11428    7096
+
+
+		bash              3844   8   2  150 4811320    9256    6860
+		tclsh          29052   8   3   91 4281200    7212    1920
 
 
 
@@ -15,6 +40,8 @@ pipelist.exe  /?
 
 
 
+	pwsh                   32588   8  27  705 2152515448   94528   53472
+	  pslist               15620  13   4  210 4316084    9168    3692
 
 
 echo '-------- section: --------'
@@ -52,8 +79,135 @@ done
 
 
 
-
 pipelist.exe  /?
+pipelist.exe  >1
+
+tclsh
+
+
+
+Code                       30104   8  19  392 2173213864  1
+
+
+
+msys-1888ae32e00d56aa-3844-sigwait                1                1
+
+
+16f
+uv\FFFFFFFFFFFFFFFF-30104                         1                1
+uv\0000000000000001-30104                         1                1
+uv\0000000000000011-30104                         1                1
+uv\0000000000000012-30104                         1                1
+
+
+
+
+
+pipelist.exe  >2
+
+
+
+conpty-8717804.203554897-in                       1                1
+conpty-8717804.203554897-out                      1                1
+conpty-8717804.203554897-out-worker               5               -1
+msys-1888ae32e00d56aa-14528-sigwait               1                1
+msys-1888ae32e00d56aa-2812-sigwait                1                1
+conpty-4365372.5761100715-in                      1                1
+conpty-4365372.5761100715-out                     1                1
+conpty-4365372.5761100715-out-worker              5               -1
+dotnet-diagnostic-7600                            1               -1
+PSHost.133251185533653174.7600.DefaultAppDomain.pwsh          1                1
+
+
+pipelist.exe  >3
+
+node
+
+
+
+LOCAL\mojo.26652.9908.4804296046935205742          1                1
+msys-1888ae32e00d56aa-12544-sigwait               1                1
+pipe_return405c9c6d-86a9-4d12-9ba6-f3afdd7f8fb8          1              128
+
+
+
+pipelist.exe  >4
+
+
+
+
+
+
+& 'D:\conda\shell\condabin\conda-hook.ps1'
+conda activate 'D:\conda'
+
+spyder
+
+print(os.fstat(0)  )
+os.stat_result(st_mode=4096, st_ino=0, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=0, st_atime=0, st_mtime=0, st_ctime=0)
+
+print(os.fstat(1)  )
+os.stat_result(st_mode=8192, st_ino=0, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=0, st_atime=0, st_mtime=0, st_ctime=0)
+
+print(os.fstat(2)  )
+os.stat_result(st_mode=33206, st_ino=173388585653782314, st_dev=612422173, st_nlink=1, st_uid=0, st_gid=0, st_size=0, st_atime=1680646728, st_mtime=1680646728, st_ctime=1680646728)
+
+
+pipelist.exe  >5
+
+python
+
+uv\FFFFFFFFFFFFFFFF-30104                         1                1
+uv\0000000000000001-30104                         1                1
+
+pipelist.exe  >6
+
+pipelist.exe  >7
+
+python
+
+
+import os, sys
+print(os.fstat(0)  )
+print(os.fstat(1)  )
+print(os.fstat(2)  )
+
+
+>>> print(os.fstat(0)  )
+os.stat_result(st_mode=8192, st_ino=0, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=0, st_atime=0, st_mtime=0, st_ctime=0)
+>>> print(os.fstat(1)  )
+os.stat_result(st_mode=8192, st_ino=0, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=0, st_atime=0, st_mtime=0, st_ctime=0)
+>>> print(os.fstat(2)  )
+os.stat_result(st_mode=8192, st_ino=0, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=0, st_atime=0, st_mtime=0, st_ctime=0)
+
+node
+
+msys-1888ae32e00d56aa-29820-sigwait               1                1
+conpty-9142278.764932355-in                       1                1
+conpty-9142278.764932355-out                      1                1
+conpty-9142278.764932355-out-worker               5               -1
+dotnet-diagnostic-2052                            1               -1
+PSHost.133251195067149647.2052.DefaultAppDomain.pwsh          1                1
+conpty-7357467.815715959-in                       1                1
+conpty-7357467.815715959-out                      1                1
+conpty-7357467.815715959-out-worker               5               -1
+dotnet-diagnostic-16428                           1               -1
+PSHost.133251195140674937.16428.DefaultAppDomain.pwsh          1                1
+
+
+pipelist.exe  >8
+
+
+
+
+bash
+exit
+
+
+
+
+
+
 
 
 Winsock2\CatalogChangeListener-494-0              1                1
