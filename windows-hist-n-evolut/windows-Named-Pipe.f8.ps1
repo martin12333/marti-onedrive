99ee3ac21ee1bd298be1fa47f0acfcee2eeae6a5
Martin Milan <martin12333@users.noreply.github.com>
2023-04-04 23:04:35 +0200

    add

---
+++ windows-hist-n-evolut/windows-Named-Pipe.f8.ps1
@@ -0,0 +1,99 @@
+
+
+exit
+
+pipelist.exe  /?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo '-------- section: --------'
+
+
+u22 copy asmaz
+cd ~/asmaz
+cd /d/umarti/dowNLOADS--SYMLINKED
+#cd -
+
+
+zgrep  shell   **/*.gz
+file   **/*.gz
+
+for x in **/*.gz
+do
+zgrep  eshell   $x
+done
+
+13000 byte/s
+
+#zipinfo $x
+| grep -i '[.]wasm'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pipelist.exe  /?
+
+
+Winsock2\CatalogChangeListener-494-0              1                1
+
+ProtectedPrefix\Administrators\Tailscale\tailscaled          5               -1
+
+AcronisFCDPService                               13               -1
+
+
+PSHost.133246648871844728.17368.DefaultAppDomain.pwsh          1                1
+
+
+msys-1888ae32e00d56aa-1784-sigwait
+
+
+
+conpty-9267912.518337209-in                       1                1
+conpty-9267912.518337209-out                      1                1
+conpty-9267912.518337209-out-worker               5               -1
+
+
+vscode-git-2641a1020e-sock                        4               -1
+
+
+
+docker_engine                                     2               -1
+podman-machine-default                            2               -1
+
+
+1                1
+powertoys_runner_6c93f926-744a-4ed2-8c70-e065f0ce85a8          1               -1
+dotnet-diagnostic-15248                           1               -1
+powertoys_settings_6c93f926-744a-4ed2-8c70-e065f0ce85a8
+
+wsl_debugshell_S-1-5-21-2941743733-2172176398-4109959614-1011          1                1
+
+
+
+
+
+
+
+
