78be9f7169ed6af2f019665b8bc16a8d53cdc506
Martin Milan <martin12333@users.noreply.github.com>
2023-04-08 12:26:27 +0200

    fix

--- eev-f8/eev.f8.el
+++ eev-f8/eev.f8.el
@@ -1,5 +1,9 @@
 ; F10 menu; down
 
+diigo
+https://www.diigo.com/user/martin12333?query=%23todo234
+https://github.com/edrx/eev/blob/UTF-8/eepitch.el
+
 
  (eepitch-shell)
  (eepitch-kill)
@@ -16,9 +20,11 @@ M-T
 
 
 
+todo to init.el
+(code-c-d "marti" "C:/Users/marti/OneDrive/" :anchor)
+
 execute each one with M-e in sequence
 
-(code-c-d "marti" "C:/Users/marti/OneDrive/" :anchor)
 (find-martifile ".emacs.d/xah.el")
 (find-fline   "~/.emacs.d/xah.el")
 
@@ -30,7 +36,7 @@ dir c:\Users\marti\.em*
 
 
 
-Today i have attached a piece of paper to the wall near my monitor, with big
+Today i have attached a page of paper to the wall near my monitor, with a big
 M-j
 M-k
 M-e
@@ -55,10 +61,11 @@ is also correct, because
 eev-f8\test--eepitch-eshell-cmd-powershell-bash-node.f8.ps1
 
 
-eepitch.el" "ee-eval-last-sexp")
-;;  -i  ?
 
 
+ (find-eev "eepitch.el" "ee-eval-last-sexp")
+;; Version:    20230127
+;;  -i  ?  no
 
 
 
@@ -67,22 +74,47 @@ can you now navigate to another file inside C:/Users/marti/OneDrive/
 and then type M-x klf?
 this should "kill link to file
 
-... not found klf
+edit:
+eeklf
+eeklfs
 
-maybe eev version??
-
+fixed:
+ eev version
 installed
 "C:\Users\marti\OneDrive\.emacs.d\elpa\eev-20220814"
-
         available  nongnu     EditorConfig Emacs Plugin
   eev                20230127
  gih  20230127
-
  Status: Available from gnu -- Install
     Version: 20230127
      Commit: 16613c18c5ec17755f083f4839bca7e4e05dd4c7
    Requires: emacs-24.4
 Other versions: 20220814 (installed).
+
+list-packages
+
+
+
+
+
+
+
+
+
+
+
+;(defun ee-expand (fname)
+;  "Expand \"~\"s and \"$ENVVAR\"s in file names, but only at the beginning."
+
+  cmd
+  echo ~
+no  
+
+where.exe nodejs
+pwsh
+
+
+
 
 
 
