3e9f59f446017c82a4a8da49611424c09c187694
Martin Milan <martin12333@users.noreply.github.com>
2023-04-10 03:53:46 +0200

    add

--- emacs/move,copy-.emacs.d-out-of-onedri.f8.ps1
+++ emacs/move,copy-.emacs.d-out-of-onedri.f8.ps1
@@ -6,9 +6,21 @@ exit
 acronis
 git push
 
+.emacs.d
+C:\Users\marti\OneDrive\.emacs.d
+
+totalcmd
+rename
+copy
 
 
 cmd /c dir  C:\Users\marti\.emacs.*
 explorer.exe C:\Users\marti\.emacs.d
 
+Recycle Bin
+
+echo aaa > C:\Users\marti\OneDrive\.emacs.d\WARNING--THIS-IS-DISCONNECTED-FROM-HOME-EMACSD.f8.txt
+
+cd .\.emacs.d
+git ls-files
 
