edc4720a8fdad19ea89dabc38ddec9bea87da826
Martin Milan <martin12333@users.noreply.github.com>
2023-04-11 12:39:42 +0200

    it-works

--- eev-f8/mpv.f8.ps1
+++ eev-f8/mpv.f8.ps1
@@ -1,19 +1,23 @@
-
-
 exit
-
-
 
  (eepitch-shell)
  (eepitch-kill)
  (eepitch-shell)
 
-
 # msys2\msys2.f8.ps1
-Solved this issue by giving my user the Full Control permission over the directory.
-# 
+# Solved this issue by giving my user the Full Control permission over the directory.
 
 powershell.exe
+
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start  mpv  "C:\Users\marti\eev-wconfig\2022dragABC.mp4"
+
+
+git commit -v -a -m "😇 it-works"
+git commit -v -a -m "😌 it-works"
+git commit -v -a -m "🙂 it-works"
+
+
 
 D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start
 
@@ -21,36 +25,38 @@ pacman -Syy
 
 pacman -S mingw-w64-x86_64-mpv
 
+
 mingw-w64-x86_64-lua51-5.1.5-5
+
 Total Download Size:    414.88 MiB
 Total Installed Size:  2591.23 MiB
 
 
-
-
-
 mpv --help
 mpv  "C:\Users\marti\eev-wconfig\2022dragABC.mp4"
-# works!
 
 
 
 Optional dependencies for mingw-w64-x86_64-mpv
     mingw-w64-x86_64-youtube-dl: for video-sharing websites playback
 
+exit
 
 
 
 
+
+
+
+Packages (174) mingw-w64-x86_64-SDL2-2.26.5-1
+
+mingw-w64-x86_64-ncurses-6.4.20230211-1
 
 warning: dependency cycle detected:
 warning: mingw-w64-x86_64-harfbuzz will be installed before its mingw-w64-x86_64-freetype dependency
 warning: mingw-w64-x86_64-opencv will be installed before its mingw-w64-x86_64-ffmpeg dependency
 warning: mingw-w64-x86_64-libwebp will be installed before its mingw-w64-x86_64-libtiff dependency
 
-Packages (174) mingw-w64-x86_64-SDL2-2.26.5-1
-
-mingw-w64-x86_64-ncurses-6.4.20230211-1
 
 
 
