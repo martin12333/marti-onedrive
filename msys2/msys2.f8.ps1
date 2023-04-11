7d9cacc4a13a90add2970143c6e9cc4d15dccd90
Martin Milan <martin12333@users.noreply.github.com>
2023-04-11 10:19:39 +0200

    fix

--- msys2/msys2.f8.ps1
+++ msys2/msys2.f8.ps1
@@ -21,6 +21,9 @@ https://objects.githubusercontent.com/github-production-release-asset-2e65be/809
 cd D:\instu
 bash
 sha256sum ms*
+
+d05c0618027299e9a0834de968596c41bd2012ed8b2534b114a3b9d3fca56809
+
 pwd
 exit
 dir ms*
@@ -31,18 +34,6 @@ restore point
 post .\msys2
 
 
-settings.json:
-{
-    "terminal.integrated.profiles.windows": {
-        "MSYS2 UCRT": {
-            "path": "cmd.exe",
-            "args": [
-                "/c",
-                "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64"
-            ]
-        }
-    }
-}
 
 
 icacls.exe D:\msys64
@@ -68,11 +59,13 @@ ls /etc/p*
 
 
 
-D:\msys64
-D:\msys64\msys2_shell.cmd -clang64
-D:\msys64\msys2_shell.cmd -ucrt64
-D:\msys64\msys2_shell.cmd -msys
 
+
+dir D:\msys64\home
+
+D:\msys64
+D:\msys64\msys2_shell.cmd
+D:\msys64\msys2_shell.cmd -msys
 
 D:\msys64\msys2_shell.cmd -mingw64 -defterm
 D:\msys64\msys2_shell.cmd -mingw64  -no-start
@@ -85,19 +78,16 @@ D:\msys64\msys2_shell.cmd -mingw64    -conemu
 D:\msys64\msys2_shell.cmd --help
 
 -defterm | -mintty | -conemu                            Set terminal type
+
 -here                            Use current directory as working
 								 directory
 -where DIRECTORY                 Use specified DIRECTORY as working
 								 directory
--[use-]full-path                 Use full current PATH variable
-								 instead of trimming to minimal
 
  -no-start                        Do not use "start" command and
 								 return login shell resulting
 								 errorcode as this batch file
 								 resulting errorcode
--shell SHELL                     Set login shell
--help | --help | -? | /?         Display this help and exit
 
 Any parameter that cannot be treated as valid option and all
 following parameters are passed as login shell command parameters.
@@ -110,36 +100,72 @@ env
 
 PATH=/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/c/Windows/System32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0/:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 
+D:\msys64\msys2_shell.cmd
 D:\msys64\msys2_shell.cmd -mingw64
 
 D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start
 
+settings.json:
+{
+    "terminal.integrated.profiles.windows": {
+        "MSYS2 UCRT": {
+            "path": "cmd.exe",
+            "args": [
+                "/c",
+                "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64"
+            ]
+        }
+    }
+}
+
+
+
 pacman -Syy
+error
+error failed to synchronize all databases (unable to lock database)
+
+https://github.com/msys2/MSYS2-packages/issues/2933#issuecomment-1461479234
+Solved this issue by giving my user the Full Control permission over the directory.
+
+# THIS HELPED !
+powershell.exe
+icacls.exe D:\msys64   /grant 'marti:(oi)(ci)(f)'
+exit
+WIN-X ADM
+
+unnecessary worry ... nothing adm
+to check
+Taskmgr.exe
+image path
+D:\msys64
+
+
 
 ls -la /var/lib/pacman/*
-
 exit
 pslist.exe -t|findstr mintt
 pslist.exe -t
 |findstr mintt
 
-icacls.exe E:\marti\ /grant 'marti:(oi)(ci)(f)'
 
 
-pacman -Syu
+pacman -Syy
+#pacman -Syu
 
 
-
+goog
 pacman Syy
 error: mingw64: missing required signature
 error: msys: missing required signature
 error: failed to synchronize all databases (unable to lock database)
 
-pacman-key --init
+#pacman-key --init
 
-which -a gpg
+#which -a gpg
 
-update-core
+#update-core
+
+
 
 pacman -S mingw-w64-x86_64-mpv
 exit
