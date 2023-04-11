38fa7dc81ee2ae7b782ea07af2423b09c9122542
Martin Milan <martin12333@users.noreply.github.com>
2023-04-11 12:23:38 +0200

    ...

--- eev-f8/mpv.f8.ps1
+++ eev-f8/mpv.f8.ps1
@@ -9,7 +9,7 @@ exit
  (eepitch-shell)
 
 
-msys2\msys2.f8.ps1
+# msys2\msys2.f8.ps1
 
 
 powershell.exe
@@ -20,6 +20,23 @@ pacman -Syy
 
 pacman -S mingw-w64-x86_64-mpv
 
+mingw-w64-x86_64-lua51-5.1.5-5
+Total Download Size:    414.88 MiB
+Total Installed Size:  2591.23 MiB
+
+
+
+
+
+mpv --help
+mpv  "C:\Users\marti\eev-wconfig\2022dragABC.mp4"
+# works!
+
+
+
+
+
+
 
 
 
@@ -28,35 +45,18 @@ warning: mingw-w64-x86_64-harfbuzz will be installed before its mingw-w64-x86_64
 warning: mingw-w64-x86_64-opencv will be installed before its mingw-w64-x86_64-ffmpeg dependency
 warning: mingw-w64-x86_64-libwebp will be installed before its mingw-w64-x86_64-libtiff dependency
 
-Packages (174) mingw-w64-x86_64-SDL2-2.26.5-1  mingw-w64-x86_64-aom-3.6.0-1
-
-mingw-w64-x86_64-curl-8.0.1-1  mingw-w64-x86_64-cython-0.29.34-1
-
-mingw-w64-x86_64-glsl-optimizer-git-1~r66913.d78c3d2f24-1
-mingw-w64-x86_64-glslang-12.1.0-1  mingw-w64-x86_64-gmp-6.2.1-5
-mingw-w64-x86_64-gnutls-3.8.0-1
-mingw-w64-x86_64-graphene-1.10.8-2
-mingw-w64-x86_64-graphite2-1.3.14-2  mingw-w64-x86_64-gsl-2.7.1-1
-
-
-
-mingw-w64-x86_64-libxslt-1.1.37-2  mingw-w64-x86_64-lua51-5.1.5-5
-
+Packages (174) mingw-w64-x86_64-SDL2-2.26.5-1
 
 mingw-w64-x86_64-ncurses-6.4.20230211-1
 
 
-Total Download Size:    414.88 MiB
-Total Installed Size:  2591.23 MiB
-
 
 df -m
+
+# F8_text_file\win-disk-dee-full.f8.ps1
 
 cd /d
 du -m | sort -n
 
 
-
-mpv --help
-mpv  "C:\Users\marti\eev-wconfig\2022dragABC.mp4"
 
