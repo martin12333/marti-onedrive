a983b79cd3bd9478fff377bb336478c9906c9e65
Martin Milan <martin12333@users.noreply.github.com>
2023-04-12 05:52:25 +0200

    add

---
+++ F8_text_file/SCRATCH.f8.ps1
@@ -0,0 +1,34 @@
+
+exit
+
+
+
+
+or TODO file ?
+
+
+(find-wconfig-links)
+(find-wconfig-browser-links)
+
+
+
+
+
+
+
+
+
+
+win-q controlpan
+maximum processor state
+#85
+92
+
+
+relogin after experim
+MicrosoftEdge.exe  --help
+###MicrosoftEdgeBCHost.exe  --help
+###MicrosoftEdgeCP.exe  --help
+MicrosoftEdgeDevTools.exe  --help
+#MicrosoftEdgeSH.exe
+
