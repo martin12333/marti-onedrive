9e2a53cb548e642a1374712965a276d799f8fcbd
Martin Milan <martin12333@users.noreply.github.com>
2023-04-13 09:28:04 +0200

    ...

--- windows-f8/cmd.f8.cmd
+++ windows-f8/cmd.f8.cmd
@@ -1,5 +1,14 @@
 exit
 
+@echo off
+
+
+
+
+
+
+
+
 
 
 :: Notes about batch files for Python people:
@@ -11,5 +20,37 @@ exit
 ::
 
 
+SET COMMAND_TO_RUN=%*
 
 
+SETLOCAL
+
+setlocal EnableDelayedExpansion
+setlocal EnableExtensions
+
+
+SET "SCRIPT_DIR=%~dp0"
+
+
+
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
+ml64 /nologo /c /Fo switch_x64_masm.obj switch_x64_masm.asm
+
+
+set "LOGINSHELL=bash"
+set /a msys2_shiftCounter=0
+
+
+
+
+
+for /F "tokens=1,2,3 delims= " %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RecoveryEnvironment" /v TargetOS') DO SET TARGETOS=%%C
+
+
+@echo off
+setlocal
+set VSCODE_DEV=
+set ELECTRON_RUN_AS_NODE=1
+"%~dp0..\Code.exe" "%~dp0..\resources\app\out\cli.js" --ms-enable-electron-run-as-node %*
+endlocal
