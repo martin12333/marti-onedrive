f9ee46d973f4f383d91b1fd60c000b1dca8c8722
Martin Milan <martin12333@users.noreply.github.com>
2023-04-14 08:52:44 +0200

    it-works

--- F8_text_file/TODO.f8.ps1
+++ F8_text_file/TODO.f8.ps1
@@ -10,6 +10,25 @@ SCRATCH IS NOT PERSISTENT
 
 
 
+ (eepitch-eshell)
+ (eepitch-kill)
+ (eepitch-eshell)
+
+ (eepitch-shell)
+ (eepitch-kill)
+ (eepitch-shell)
+
+
+
+echo $PATH
+env
+set
+
+(getenv "HOME")
+"C:\\Users\\marti"
+
+
+
 
 
 # Â«2021aug16Â»  (to ".2021aug16")
@@ -97,4 +116,137 @@ MicrosoftEdge.exe  --help
 ###MicrosoftEdgeCP.exe  --help
 MicrosoftEdgeDevTools.exe  --help
 #MicrosoftEdgeSH.exe
+
+
+
+
+
+
+
+
+(find-wconfig-links)
+(find-wconfig-browser-links)
+
+(find-angg-es-links)
+
+
+
+
+(find-wconfig-links)
+
+(find-wconfig-browser-links)
+and typing `M-x browse-url':
+"https://www.lua.org/"
+(find-wconfig-wget-links)
+(find-wconfig-shell-links)
+(find-wconfig-lua-links)
+(find-wconfig-mpv-links)
+
+
+
+
+# Â«2021aug16Â»  (to ".2021aug16")
+
+
+i will fix that later
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+it-works
+
+todo -> extract to file
+
+a TODO file
+SCRATCH IS NOT PERSISTENT
+
+
+ (eepitch-eshell)
+ (eepitch-kill)
+ (eepitch-eshell)
+
+
+echo $PATH
+
+env
+set
+
+Welcome to the Emacs shell
+
+~ $ echo $PATH
+D:\msys64\mingw64\bin;D:\msys64\usr\local\bin;D:\msys64\usr\bin;D:\msys64\usr\bin;C:\Windows\System32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;D:\msys64\usr\bin\site_perl;D:\msys64\usr\bin\vendor_perl;D:\msys64\usr\bin\core_perl
+~ $
+
+
+ (eepitch-shell)
+ (eepitch-kill)
+ (eepitch-shell)
+
+bash: cannot set terminal process group (-1): Inappropriate ioctl for device
+bash: no job control in this shell
+]0;~
+marti@len20 MINGW64 ~
+$ echo $PATH
+/mingw64/bin:/usr/local/bin:/usr/bin:/usr/bin:/c/Windows/System32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+]0;~
+marti@len20 MINGW64 ~
+$
+
+
+
+
+(getenv "HOME")
+"C:\\Users\\marti"
+
+
+
+
+
+
+
+(find-wconfig-links)
+(find-wconfig-browser-links)
+
+(find-angg-es-links)
+
+
+
+# Â«2021aug16Â»  (to ".2021aug16")
+
+
+i will fix that later
+
+
+
+(find-wconfig-links)
+
+(find-wconfig-browser-links)
+and typing `M-x browse-url':
+"https://www.lua.org/"
+(find-wconfig-wget-links)
+(find-wconfig-shell-links)
+(find-wconfig-lua-links)
+(find-wconfig-mpv-links)
+
+
+
 
