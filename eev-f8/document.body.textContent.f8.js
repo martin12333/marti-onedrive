10ab250ae42528b1f6ed535033043e08c89978fe
Martin Milan <martin12333@users.noreply.github.com>
2023-04-17 19:29:55 +0200

    add

--- eev-f8/document.body.textContent.f8.js
+++ eev-f8/document.body.textContent.f8.js
@@ -1,6 +1,8 @@
 
 
 
+console.log( document.body.children[0].children[0] )
+
 console.log( document.body.children[0].children[0].children[1] )
 
 document.body.textContent.replaceAll(/[\n\t\r ]+/g,' ')
@@ -12,6 +14,12 @@ document.body.textContent.replaceAll(/[\n\t\r]/g,' ').replaceAll(/ +/g,' ')
 
 
 getSelection()
+
+
+
+
+
+
 
 var xpath = function(xpathToExecute){
 	var result = [];
@@ -37,8 +45,14 @@ var xpath = function(xpathToExecute){
   Follow
   edited Mar 15, 2022 at 2:53
 
-  
+
   {for(var x of nodesSnapshot){console.log(x)}}
+
+
+
+  <a href={`command:vscode.open?${encodeURIComponent(JSON.stringify(fileUri))}`}>Open file</a>
+
+
 
 <div class="relative node pen node-hover" data-node-id="85" style="margin-top: 17px; margin-bottom: 17px; padding-top: 29px;"><div></div><div class="relative"><div class="relative pea"><div></div><div class="node-hover node-focus relative"><div class="cm-editor ͼ1 ͼ2 ͼ4 ͼ5"><div aria-live="polite" style="position: absolute; top: -10000px;"></div><div tabindex="-1" class="cm-scroller"><div spellcheck="false" autocorrect="off" autocapitalize="off" translate="no" contenteditable="true" class="cm-content cm-lineWrapping" style="tab-size: 2;" role="textbox" aria-multiline="true" aria-autocomplete="list" data-cy="editor"><div class="cm-activeLine cm-line"><span class="tok-variableName">document</span><span class="tok-operator">.</span><span class="tok-propertyName">getElementById</span><span class="tok-punctuation">(</span><span class="tok-string">''</span><span class="tok-punctuation">)</span></div></div><div class="cm-selectionLayer" aria-hidden="true"></div><div class="cm-cursorLayer" aria-hidden="true" style="animation-duration: 1200ms; animation-name: cm-blink2;"><div class="cm-cursor cm-cursor-primary" style="left: 163.683px; top: 6.13333px; height: 16.5333px;"></div></div></div><div class="cm-contextual-info cm-tooltip cm-tooltip-autocomplete cm-tooltip-above" style="position: fixed; top: 388.533px; left: 185.683px;"><div></div></div></div><button title="Run cell Shift-Enter" class="flex items-center absolute top-0 bg-transparent bn br2 pointer squishy blue hover-dark-blue dn-print" style="right: -14px; height: 29px;"><svg width="16" height="16" fill="currentColor" stroke="currentColor" stroke-linejoin="round" stroke-width="2" viewBox="0 0 16 16"><path d="M4 12.4788V3.52116C4 2.23802 5.50646 1.54723 6.47878 2.38451L11.68 6.86335C12.375 7.46178 12.375 8.53822 11.68 9.13665L6.47878 13.6155C5.50646 14.4528 4 13.762 4 12.4788Z"></path></svg></button></div><div class="flex absolute moon-gray" style="top: 4px; left: -55px;"><svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16" class="pointer squishy blue" role="button" aria-label="Unpin" style="padding: 3px 0px 2px;"><path d="M8 1h3v1l-1 1v4l2 .875V9H9v5.125L8 15l-1-.875V9H4V7.875L6 7V3L5 2V1z"></path><title>Unpin cell Ctrl-.</title></svg></div></div></div><div></div></div>
 
---
+++ hardware-f8/keyboard.f8.ps1
@@ -0,0 +1,40 @@
+keyboard.f8.ps1
+
+
+
+test
+
+
+vscode
+f1
+{
+	"key": "ctrl+k ctrl+s",
+	"command": "workbench.action.openGlobalKeybindings"
+  }Preferences: Open Keyboard Shortcuts
+
+  esc
+ row1
+ ins
+ row -1
+
+no diff alt altgr
+.\powertoys
+alt(right)
+ok
+
+row2
+`12#$%678(_)-=
+
+	qwertyuiop[{}]
+
+asdfghjkl:''"""
+|\zxcvbnm,./?
+
+
+/*-
+789+
+456+
+1233
+00;
+
+
