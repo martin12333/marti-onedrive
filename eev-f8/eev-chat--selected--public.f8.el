72fbf5e704825a619dae13285db7a956b281305f
Martin Milan <martin12333@users.noreply.github.com>
2023-04-21 19:51:24 +0200

    ...

--- eev-f8/eev-chat--selected--public.f8.el
+++ eev-f8/eev-chat--selected--public.f8.el
@@ -28,6 +28,60 @@ The main advantage of IRC is that we can send sexps throught it and they will be
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; Tests:
+;; (ee-find-eww-preprocess-url "/")
+;; (ee-find-eww-preprocess-url "/foo")
+;; (ee-find-eww-preprocess-url "C:/Users/")
+;; (ee-find-eww-preprocess-url "C:\\Users\\")
+(defun ee-find-eww-preprocess-url (url)
+  (setq url (ee-expand url))
+  (setq url (replace-regexp-in-string "\\\\" "/" url))
+  (if (string-match "^/" url)
+      (concat "file://" url)
+    (if (string-match "^[A-Za-z]:/" url)
+	(concat "file:///" url)
+      url)))
+
+(defun find-eww (url &rest pos-spec-list)
+  "Hyperlink to a page in HTML. Use eww as the browser.
+URL can be either a real URL or a file name.
+This function searches for POS-SPEC-LIST in the page, but only
+after eww finishes rendering it. This is implemented by a hook:
+this function saves the POS-SPEC-LIST in the variable
+`ee-find-eww-search-for' and sets the variable
+ee-find-eww-search-yes' to t; eww' runs the function
+`ee-find-eww-search' after rendering the page, and
+`ee-find-eww-search' processes these two variables."
+  (setq ee-find-eww-search-for pos-spec-list)
+  (setq ee-find-eww-search-yes t)
+  (eww (ee-find-eww-preprocess-url url)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 email
 // I am not pasting a url, because my messages with a url ... often were considered as spam
 
