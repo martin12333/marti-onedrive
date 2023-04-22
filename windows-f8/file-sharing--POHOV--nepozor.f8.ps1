90274098caedc5361a6b619e874dd405767c167d
Martin Milan <martin12333@users.noreply.github.com>
2023-04-22 20:02:02 +0200

    add

--- windows-f8/file-sharing--POHOV--nepozor.f8.ps1
+++ windows-f8/file-sharing--POHOV--nepozor.f8.ps1
@@ -1,21 +1,39 @@
-POHOV-ne-pozor.f8.ps1
-
 
 exit
+
+
 
 dir //desktop-psj0tro/e--everyone-read/
 ###cmd /c dir \\DESKTOP-PSJ0TRO\D$
 cmd /c dir \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor
 
 
-ve win 10 home uz asi ~~nejde
-rozch
+nepozor
+FALSE-ALARM
+
+
+copy C:\Users\marti\OneDrive\windows-f8\file-sharing--POHOV--nepozor.f8.ps1  \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor
 
 
 
+ve win 10 home uz asi ~~nejde everyone anonymous
+
+https://www.diigo.com/user/martin12333?query=%23filesharing
+https://www.diigo.com/user/martin12333?query=%23filesharing
+diigo 04/22/2023
+
+windows 10 creating local accounts just for file sharing site:superuser.com
+windows file sharing by creating local accounts just for sharing
 
 
+
+rozchodil jen pres heslo of
 Eda
+
+
+
+
+
 
 
 
@@ -26,9 +44,8 @@ Get-LocalUser
 
 Eda
 
-dir //desktop-psj0tro/e--everyone-read/
-###cmd /c dir \\DESKTOP-PSJ0TRO\D$
-cmd /c dir \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor
+
+
 cat  \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor\share.f8.ps1
 
 # file://desktop-psj0tro/e--everyone-read/
