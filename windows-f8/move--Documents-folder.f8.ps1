011f72fd7836e1a7e4cf9e560ae11bd14182c4a4
Martin Milan <martin12333@users.noreply.github.com>
2023-05-02 00:44:25 +0200

    add

---
+++ windows-f8/move--Documents-folder.f8.ps1
@@ -0,0 +1,17 @@
+exit
+
+
+https://superuser.com/questions/1725456/wha-cant-i-move-my-documents-folder
+
+https://superuser.com/questions/1728621/onedrive-wont-let-me-stop-syncing-desktop-and-documents
+
+https://superuser.com/questions/1734525/windows-11-keeps-changing-documents-pictures-folders-to-onedrive-folder
+
+
+
+
+
+completion
+
+ls ~\
+ls ~/
