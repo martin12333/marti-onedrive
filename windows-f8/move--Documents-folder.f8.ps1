43a61dcb4e0c6bd851e2dd60aa8f4a6034d69082
Martin Milan <martin12333@users.noreply.github.com>
2023-05-05 16:51:05 +0200

    partial-refactor

--- windows-f8/move--Documents-folder.f8.ps1
+++ windows-f8/move--Documents-folder.f8.ps1
@@ -1,3 +1,4 @@
+
 exit
 
 
@@ -6,6 +7,15 @@ https://superuser.com/questions/1725456/wha-cant-i-move-my-documents-folder
 https://superuser.com/questions/1728621/onedrive-wont-let-me-stop-syncing-desktop-and-documents
 
 https://superuser.com/questions/1734525/windows-11-keeps-changing-documents-pictures-folders-to-onedrive-folder
+
+
+
+
+
+acronis
+
+git push
+
 
 
 
