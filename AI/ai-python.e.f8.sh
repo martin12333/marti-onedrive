78fafb9447a899c91e83b7c4604df967cd1a594c
Martin Milan <martin12333@users.noreply.github.com>
2023-05-06 17:59:37 +0200

    add

---
+++ AI/ai-python.e.f8.sh
@@ -0,0 +1,61 @@
+ai-python.e.f8.sh
+
+exit
+
+pwsh
+
+& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate 'D:\conda'
+
+
+
+
+
+#C:\Users\marti\OneDrive\ps-mylen-mar\Visual Studio Code Host_history.txt
+
+
+
+C:\Users\marti\OneDrive\conn\mynotes-mart,j.txt
+conda create --name myenv2 --clone myenv
+
+
+
+bash -i
+  python --help | grep -C 3 intera
+
+# -i     : inspect interactively after running script; forces a prompt even
+#         if stdin does not appear to be a terminal; also PYTHONINSPECT=x
+
+-u     : force the stdout and stderr streams to be unbuffered;
+
+
+
+
+
+
+
+
+C:\Users\marti\OneDrive\conn\mynotes-mart,j.txt
+
+tf_bert_for_sequence_classification/bert/embeddings/word_embeddings/weight:0
+(105879, 768)
+tf_bert_for_sequence_classification/bert/embeddings/position_embeddings/embeddings:0
+(512, 768)
+tf_bert_for_sequence_classification/bert/embeddings/token_type_embeddings/embeddings:0
+(2, 768)
+tf_bert_for_sequence_classification/bert/embeddings/LayerNorm/gamma:0
+(768,)
+tf_bert_for_sequence_classification/bert/embeddings/LayerNorm/beta:0
+(768,)
+
+
+tf_bert_for_sequence_classification/bert/encoder/layer_._0/attention/self/query/kernel:0
+(768, 12, 64)
+tf_bert_for_sequence_classification/bert/encoder/layer_._0/attention/self/query/bias:0
+(12, 64)
+tf_bert_for_sequence_classification/bert/encoder/layer_._0/attention/self/key/kernel:0
+(768, 12, 64)
+tf_bert_for_sequence_classification/bert/encoder/layer_._0/attention/self/key/bias:0
+(12, 64)
+
+
+
