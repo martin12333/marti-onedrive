712e789074a15500c025fcd08ac632e5a1c2af9c
Martin Milan <martin12333@users.noreply.github.com>
2023-05-08 00:44:59 +0200

    ...

--- AI/kaggle.com--intro.e.f8.py
+++ AI/kaggle.com--intro.e.f8.py
@@ -1,4 +1,12 @@
 
+https://www.kaggle.com/general/108481
+Weekly Maximum GPU Usage
+As many of you know, Kaggle gives users free access to GPU's in our notebooks.
+We wish we could give free compute without any bounds, because they help a lot of people do deep learning who otherwise lack access to GPUs.
+
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 
 
@@ -7,6 +15,12 @@ https://www.kaggle.com/page/GPU-tips-and-tricks
 
 
 
+
+
+
+ won't get new packages, but your code is less likely to break.
+
+Internet off
 
 
 !conda --version
@@ -28,6 +42,15 @@ import torch
 x = torch.rand(5, 3)
 print(x)
 
+
+maybe dont
+!env
+KAGGLE_USER_SECRETS_TOKEN=
+
+
+HOME=/root
+LC_ALL=C.UTF-8
+LANG=C.UTF-8
 
 
 
