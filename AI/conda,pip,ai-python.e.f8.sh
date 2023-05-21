2565e62dc7400877fe40a8eede43bea71732e1f7
Martin Milan <martin12333@users.noreply.github.com>
2023-05-21 12:59:59 +0200

    refactor

--- AI/conda,pip,ai-python.e.f8.sh
+++ AI/conda,pip,ai-python.e.f8.sh
@@ -385,139 +385,27 @@ C:\Users\marti\OneDrive\AI\conda----20torch_nonpip310.e.f8.sh
 
 
 
-conda env list
-#mamba
 
-conda create --name pip310  --clone torch_nonpip310
 
-conda activate pip310
 
 
 
 
 
-https://pip.pypa.io/en/stable/topics/caching/#avoiding-caching
 
-pip tries to use its cache whenever possible, and it is designed do the right thing by default.
 
 
 
 
-bash -c 'df -m'
-D:                       78056    74743      3314   96% /d
-D:                       78056  75283      2774  97% /d
 
 
 
 
 
-exit
-bash
-pip install   --dry-run   git+https://github.com/neelnanda-io/TransformerLens
-pip install      git+https://github.com/neelnanda-io/TransformerLens
 
-#Collecting torch>=1.10
- # Downloading torch-2.0.1-cp310-cp310-win_amd64.whl (172.3 MB
-Requirement already satisfied: torch>=1.10 in d:\conda\envs\pip310\lib\site-packages (from transformer-lens==0.0.0) (1.13.1)
 
 
-pip install   --dry-run   gradio
-pip install     gradio
 
-
-
-
-cd d:\
-jupyter lab
-
-cd d:\AI
-
-git clone https://github.com/neelnanda-io/TransformerLens
-
-exit
-
-
-& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate pip310
-
-bash -c 'df -m'
-C:/Program Files/Git    151328 116045     35284  77% /
-D:                       78056  75451      2606  97% /d
-
-
-
-
-C:\Users\marti\OneDrive\AI\ai-212.e.f8.py
-default_text = "The following is a list of powers of 10: 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000"
-
-
-
-
-
-pip install   --dry-run    circuitsvis
-pip install    circuitsvis
-
-  # PySvelte is an unmaintained visualization library, use it as a backup if circuitsvis isn't working\n",
-
- Attempting uninstall: importlib-metadata
-    Found existing installation: importlib-metadata 6.0.0
-    Uninstalling importlib-metadata-6.0.0:
-      Successfully uninstalled importlib-metadata-6.0.0
-Successfully installed circuitsvis-1.39.1 importlib-metadata-5.2.0
-
-pip install    --dry-run   plotly
-pip install    plotly
-
-
-pip install     --dry-run    tokenizers
-#pip install tokenizers
-
-cd AI
-wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt
-
-
-
-
-& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate pip310
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#conda search  -c conda-forge    poetry
-###conda install poetry='1.*' -c conda-forge
-
-
-
-
-
-
-
-
-
-regex==2017.4.5 # used by the bpe tokenizer
-
-pip list |findstr rege
-conda list |findstr rege
-conda search regex| findstr py310
-
-conda install --dry-run  regex==2017.4.5 # used by the bpe tokenizer
-conda install     regex==2021.8.3
-
-exit()
-quit()
 
 
 
@@ -552,7 +440,7 @@ AI\gpt-from-scratch.e.f8.py
 
 
 C:\Users\marti\OneDrive\conn\mynotes-mart,j.txt
-conda create --name myenv2 --clone myenv
+#conda create --name myenv2 --clone myenv
 
 
 
---
+++ AI/pip----30pip310.e.f8.sh
@@ -0,0 +1,167 @@
+#pip----30pip310.e.f8.sh
+
+exit
+
+
+pwsh
+& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate pip310
+#& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate 'D:\conda'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+conda env list
+#mamba
+
+conda create --name pip310  --clone torch_nonpip310
+
+conda activate pip310
+
+
+
+
+
+https://pip.pypa.io/en/stable/topics/caching/#avoiding-caching
+
+pip tries to use its cache whenever possible, and it is designed do the right thing by default.
+
+
+
+
+bash -c 'df -m'
+D:                       78056    74743      3314   96% /d
+D:                       78056  75283      2774  97% /d
+
+
+
+
+
+exit
+bash
+pip install   --dry-run   git+https://github.com/neelnanda-io/TransformerLens
+pip install      git+https://github.com/neelnanda-io/TransformerLens
+
+#Collecting torch>=1.10
+ # Downloading torch-2.0.1-cp310-cp310-win_amd64.whl (172.3 MB
+Requirement already satisfied: torch>=1.10 in d:\conda\envs\pip310\lib\site-packages (from transformer-lens==0.0.0) (1.13.1)
+
+
+pip install   --dry-run   gradio
+pip install     gradio
+
+
+
+
+cd d:\
+jupyter lab
+
+cd d:\AI
+
+git clone https://github.com/neelnanda-io/TransformerLens
+
+exit
+
+
+& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate pip310
+
+bash -c 'df -m'
+C:/Program Files/Git    151328 116045     35284  77% /
+D:                       78056  75451      2606  97% /d
+
+
+
+
+C:\Users\marti\OneDrive\AI\ai-212.e.f8.py
+default_text = "The following is a list of powers of 10: 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000"
+
+
+
+
+
+pip install   --dry-run    circuitsvis
+pip install    circuitsvis
+
+  # PySvelte is an unmaintained visualization library, use it as a backup if circuitsvis isn't working\n",
+
+ Attempting uninstall: importlib-metadata
+    Found existing installation: importlib-metadata 6.0.0
+    Uninstalling importlib-metadata-6.0.0:
+      Successfully uninstalled importlib-metadata-6.0.0
+Successfully installed circuitsvis-1.39.1 importlib-metadata-5.2.0
+
+pip install    --dry-run   plotly
+pip install    plotly
+
+
+pip install     --dry-run    tokenizers
+#pip install tokenizers
+
+cd AI
+wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt
+
+
+
+
+& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate pip310
+
+
+
+
+
+
+
+
+
+
+
+
+#conda search  -c conda-forge    poetry
+###conda install poetry='1.*' -c conda-forge
+
+
+
+
+
+
+
+unnecessary
+
+something else brought-in the regex
+
+regex==2017.4.5 # used by the bpe tokenizer
+
+pip list |findstr rege
+conda list |findstr rege
+conda search regex| findstr py310
+
+conda install --dry-run  regex==2017.4.5 # used by the bpe tokenizer
+conda install     regex==2021.8.3
+
+exit()
+quit()
+
+
+
+
+
+
