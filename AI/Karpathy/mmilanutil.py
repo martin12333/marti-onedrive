fb1f772824613dd598695e68ae203441927456d8
Martin Milan <martin12333@users.noreply.github.com>
2023-05-31 16:49:05 +0200

    ...

--- AI/Karpathy/mmilanutil.py
+++ AI/Karpathy/mmilanutil.py
@@ -1,3 +1,11 @@
 
-print('dsfdsfdsf')
+
+import torch
+##NameError: name 'torch' is not defined
+
+
+# pytorch Compute the maximum absolute value of x
+def mmptmaxabs(x):
+    return torch.max(torch.abs(x.detach().cpu().flatten()  )) .item()
+    #np.abs(x.detach().numpy().flatten()).max()
 
