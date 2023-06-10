1ba39e80f1638a98e6f3acf61fb770e891e8a55a
Martin Milan <martin12333@users.noreply.github.com>
2023-06-10 04:09:51 +0200

    add

--- AI/conda,pip,mamba--docker,podman--236.e.f8.sh
+++ AI/conda,pip,mamba--docker,podman--236.e.f8.sh
@@ -14,33 +14,26 @@ jupyter
 
 python -m http.server 8888
 
+conda
 
 
 
-bash
+file:///C:\Users\marti\OneDrive\tar-exe\w\home\martin\eternalCopy.zsh_history.f8.sh
 
-find . -name  "*.f8.*"| tr ' ' '@' >1 #1.f8.names.txt
-
-#cat 1.f8.names.txt
-cat 1| tr ' ' '@'  | xargs grep -lE 'docker|podman'
-#cat 1.f8.names.txt
-cat 1| tr ' ' '@' | xargs grep -lE 'conda |pip '
-
-#cat 1.f8.names.txt
-cat 1 |  tr ' ' '@'|xargs grep -lE 'conda |pip ' >3
-
-# file 3 is prob needed xargs xatrgs failed
-
-cat 3 | grep AI | tr ' ' '@'   | xargs grep -E 'docker|podman' 2>2
-cat 3 | grep . | tr ' ' '@'   | xargs grep -El 'docker |podman ' 2>2
- | grep pip
-cat 3 | grep . | tr ' ' '@'   | xargs grep -El 'docker|podman' 2>2 | grep con
-
- | xargs grep -lE 'conda|pip'
-
-grep -rl conda
-
- | xargs grep -l docker
+https://raw.githubusercontent.com/martin12333/marti-onedrive/main2/tar-exe/w/home/martin/eternalCopy.zsh_history.f8.sh
 
 
-grep
+: 1644084942:0;bash Miniconda3-latest-Linux-x86_64.sh
+
+bash Miniconda3-latest-Linux-x86_64.sh
+
+
+
+
+
+
+
+
+
+
+
