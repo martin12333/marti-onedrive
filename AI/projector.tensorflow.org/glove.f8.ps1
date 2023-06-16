7fdc0202b4160ae5aed81d07ca63033e965de266
Martin Milan <martin12333@users.noreply.github.com>
2023-06-16 15:55:35 +0200

    ...

--- AI/projector.tensorflow.org/glove.f8.ps1
+++ AI/projector.tensorflow.org/glove.f8.ps1
@@ -9,12 +9,25 @@ cmd /c mklink /j d:\ai\cee  C:\Users\marti\.cache\my
 cmd /c dir d:\ai\c*
 
 cd  C:\Users\marti\.cache\my
+dir
+
 bash
 wc -l *
+wc  *
+
+cut -f 1 -d ' '   glove.6B.300d.txt  >metadata.tsv
+
+cut -f 2- -d ' '   glove.6B.300d.txt  |  tr ' '   '\t'  >tensors.tsv
+
+>1
+
 
 
 cd D:\ai\gpt2-embedding-wte-10k\00000\default
 ls
 
+
+10000 metadata.tsv
+10000 tensors.tsv
 
 
