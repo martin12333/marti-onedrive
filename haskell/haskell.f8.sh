ebf2a2716fab8b0aa5cbc8042d002f0b1d81c1ad
Martin Milan <martin12333@users.noreply.github.com>
2023-08-15 05:04:57 +0200

    add

---
+++ haskell/haskell.f8.sh
@@ -0,0 +1,42 @@
+
+exit
+
+bash -c 'df -m'
+
+podman machine start
+#podman machine stop
+
+reboot helped
+API forwarding for Docker API clients is not available due to the following startup failures.
+        CreateFile \\.\pipe\docker_engine: All pipe instances are busy.
+Error: failed to connect: dial tcp [::1]:61617: connectex: No connection could be made because the target machine actively refused it.
+
+
+podman start   -ai    cmy22b
+n
+
+#podman stop cmy22b
+
+---echo '-------- section --------'
+
+sudo apt update
+##sudo apt install ghc
+
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org >haskell.org.sh  # | sh
+cat haskell.org.sh
+head -n 20 haskell.org.sh
+
+git ls-files
+ls -ld *git*
+ls -ld .*git*
+git init
+ls -latr
+mv -iv .ghc OLD.ghc
+
+git add .bashrc
+git add .zshrc
+git commit -a -m bashrc,zshrc
+
+
+
+
