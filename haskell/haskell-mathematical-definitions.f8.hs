d40101d0b513caf6d64b5c7d8644a107b4110214
Martin Milan <martin12333@users.noreply.github.com>
2023-08-23 03:08:18 +0200

    backup

--- haskell/haskell-mathematical-definitions.f8.hs
+++ haskell/haskell-mathematical-definitions.f8.hs
@@ -13,12 +13,49 @@ ghci
 
 
 
-:showi language
 
-base language is: GHC2021
-with the following modifiers:
-  -XExtendedDefaultRules
-  -XNoMonomorphismRestriction
+
+https://en.wikibooks.org/wiki/Haskell/Denotational_semantics
+
+Adding ⊥ to the set of values is also called lifting. This is often depicted by a subscript like in
+�
+⊥
+{\mathbb  {Z}}_{\bot }. While this is the correct notation for the mathematical set "lifted integers", we prefer to talk about "values of type Integer". We do this because
+�
+⊥
+{\mathbb  {Z}}_{\bot } suggests that there are "real" integers
+�
+\mathbb {Z} , but inside Haskell, the "integers" are Integer.
+
+
+
+In Haskell, the expression undefined denotes ⊥. With its help, one can indeed verify some semantic properties of actual Haskell programs. undefined has the polymorphic type forall a . a which of course can be specialized to undefined :: Integer, undefined :: (), undefined :: Integer -> Integer and so on. In the Haskell Prelude, it is defined as
+
+undefined = error "Prelude.undefined"
+
+https://wiki.haskell.org/Bottom
+
+ghci> :i undefined
+undefined :: GHC.Stack.Types.HasCallStack => a
+        -- Defined in `GHC.Err'
+
+undefined
+*** Exception: Prelude.undefined
+
+print (sizeOf (undefined :: Int))
+
+print (head (1 : undefined))
+----print (head (1..78))
+head [1..7]
+
+
+https://google.com
+
+Primitive types are always unlifted; that is, a value of a primitive type cannot be bottom. ... a “lifted” type means that terms of that type may be bottom.
+
+https://en.wikipedia.org/wiki/Bottom_type
+
+In Python, the bottom type is typing.NoReturn (typing.Never since version 3.11).[10][11]
 
 
 ghci> :i True
@@ -100,9 +137,16 @@ data Bool = False | True
 
 
 
+:showi language
+
+base language is: GHC2021
+with the following modifiers:
+  -XExtendedDefaultRules
+  -XNoMonomorphismRestriction
 
 
 ghci -W f5.hs
 
 ghci
 :q
+
