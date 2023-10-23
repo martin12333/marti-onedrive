b3b1f51096fd82f5eb0845026523143975b31fa8
Martin Milan <martin12333@users.noreply.github.com>
2023-10-23 11:46:02 +0200

    add

---
+++ GUI-automation/pyautogui.f8.py
@@ -0,0 +1,55 @@
+
+
+# f8_text_file
+# (an f8_text_file is-not meant to be run as a whole)
+from platform import python_branch
+
+
+)
+exit
+
+(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+
+pwsh
+& 'D:\conda\shell\condabin\conda-hook.ps1'  ;   conda activate pip310ecco
+
+# file:///C:\Users\marti\OneDrive\AI\pip----31pip310ecco.e.f8.sh
+
+
+
+Successfully installed EasyProcess-1.1 entrypoint2-1.1 mouseinfo-0.1.3 mss-9.0.1 pyautogui-0.9.54 pygetwindow-0.0.9 pymsgbox-1.0.9 pyperclip-1.8.2 pyrect-0.2.0 pyscreenshot-3.1 pyscreeze-0.1.29 pytweening-1.0.7
+
+
+python
+###ipython
+import pyautogui
+
+#quit()
+
+ wh = pyautogui.size() # Obtain the screen resolution.
+ wh
+Size(width=1920, height=1080)
+ wh[0]
+1920
+ wh.width
+1920
+
+for i in range(10): # Move mouse in a square.
+       pyautogui.moveTo(100, 100, duration=0.25)
+       pyautogui.moveTo(200, 100, duration=0.25)
+       pyautogui.moveTo(200, 200, duration=0.25)
+       pyautogui.moveTo(100, 200, duration=0.25)
+
+
+
+
+pyautogui.getAllTitles()
+
+###cscript
+
+###wscript
+
+
