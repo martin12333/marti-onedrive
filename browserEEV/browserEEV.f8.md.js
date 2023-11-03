02e7026d0d939ae9e1354d34e740e4ede6700a6f
Martin Milan <martin12333@users.noreply.github.com>
2023-11-03 15:08:53 +0100

    backup

--- browserEEV/browserEEV.f8.md.js
+++ browserEEV/browserEEV.f8.md.js
@@ -1,41 +1,72 @@
 
-https://www.bing.com/search?q=Bing+AI&showconv=1
-a simple web page with a Codemirror editor, and javascript code to `eval()` the selected text, when the user presses the F9 key
-"C:\Users\marti\OneDrive\browserEEV\Conversation (16).txt"
+browserEEV ideas
 
-ace
-monaco seems baroque
-CodeMirror 6 seems too young for gpt
+early drafts
+
+browserEEV will be something like `eev`, but in a web browser ... analogies of Emacs `eev`
+
+...
+
+JS functions and hotkeys
+to send
+a line of text (or, perhaps, a block of text)
+from an embedded editor widget,
+to the eval() of the browser javascript
+(or maybe to an embedded ~~jslinux terminal in a web page?)
 
 
+
+searches
+
+site:codepen.io ace-editor   "eval" selection
 CodeMirror
-https://codepen.io/martin_m_n_novy/pen/eYxdMYj
+Monaco-editor
 
-https://codepen.io/martin_m_n_novy/pen/RwvGQqb
+https://www.bing.com/search?q=Bing+AI&showconv=1
+a simple web page with a Codemirror editor, and javascript code to `eval()` the selected text, when the user presses the
+F8? in browsers?
+F9? key
 
-
-
-https://observablehq.com/d/3555200f7e68f405
-https://observablehq.com/@tophtucker/eval-in-place
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"C:\Users\marti\OneDrive\browserEEV\Conversation (16).txt"
 
 
 https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly#firefox:win11:fx118
+
+
+
+monaco seems baroque?
+CodeMirror 6 seems too new for gpt
+
+ace
+CodeMirror/5
+
+https://codepen.io/martin_m_n_novy/pen/eYxdMYj
+//  BingAI is helping me
+
+https://codepen.io/martin_m_n_novy/pen/RwvGQqb
+forked
+
+
+https://observablehq.com/@tophtucker/eval-in-place
+//// forked  https://observablehq.com/d/3555200f7e68f405
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -50,9 +81,6 @@ https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-qu
 
 
 https://jsfiddle.net/stpmu61y/
-
-
-
 
 // Initialize CodeMirror editor
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
