7a06678982cc8465be121517c9ca417b6450989b
Martin Milan <martin12333@users.noreply.github.com>
2023-12-27 12:07:16 +0100

    add

---
+++ F8_text_file/clipboard-history.f8.md
@@ -0,0 +1,12 @@
+clipboard-history.f8.md
+
+
+
+"Loading..." endlessly, going to reinstall
+or maybe windows
+clipboard history
+pro
+
+win+v
+
+
---
+++ Non-24-Hour--Sleep-Disorder/Kernel-Power.f8.ps1
@@ -0,0 +1,12 @@
+exit
+
+image.png
+
+
+%SystemRoot%\System32\Winevt\Logs\System.evtx
+40960
+
+
+Kernel-Power, Kernel-PowerTrigger, Kernel-Processor-Power (Microsoft-Windows-Kernel-Interrupt-Steering), Kernel-Processor-Power (Microsoft-Windows-Kernel-Processor-Power)
+
+
