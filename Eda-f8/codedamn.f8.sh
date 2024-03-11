c5bf432e120a068244949d86a06d85c2a0ef14b6
Martin Milan <martin12333@users.noreply.github.com>
2024-03-11 23:22:54 +0100

    backup

---
+++ Eda-f8/codedamn.f8.sh
@@ -0,0 +1,65 @@
+
+# f8_text_file
+# (an f8_text_file is-not meant to be run as a whole)
+)
+exit
+
+#  ^Sp
+#  ^Sp   F1
+
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+echo '-------- section --------'
+
+## file:///C:\Users\marti\OneDrive\alma-233c\alma.f8.sh
+
+
+
+
+ls -la
+##ls -la ../.test
+ls -la .. >ls.txt
+###find .. -ls >find.txt
+
+
+set
+set >set.txt
+
+
+free -m  >free.txt
+df -h >>free.txt
+#df -m >>free.txt
+
+vmstat >>free.txt
+
+history
+
+#cat /etc/lsb*release
+
+history -a; history
+#id
+
+
+
+
+C:\Users\marti\OneDrive\Eda-f8\set.txt
+
+DEBIAN_FRONTEND=noninteractive
+
+HISTFILE=/home/damner//.bash_history
+
+MACHTYPE=x86_64-pc-linux-gnu
+
+NODE_OPTIONS=--max_old_space_size=496
+
+OPTERR=1
+OPTIND=1
+OSTYPE=linux-gnu
+
+
+
+
+
+
+
+
