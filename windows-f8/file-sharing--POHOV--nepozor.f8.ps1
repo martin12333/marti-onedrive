3d16d9675070719b90f1ae9edd6c64f7c637aa23
Martin Milan <martin12333@users.noreply.github.com>
2024-03-27 18:00:57 +0100

    backup

---
+++ windows-f8/file-sharing--POHOV--nepozor.f8.ps1
@@ -0,0 +1,52 @@
+
+exit
+
+
+
+dir //desktop-psj0tro/e--everyone-read/
+###cmd /c dir \\DESKTOP-PSJ0TRO\D$
+cmd /c dir \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor
+
+nepozor
+FALSE-ALARM
+
+copy C:\Users\marti\OneDrive\windows-f8\file-sharing--POHOV--nepozor.f8.ps1  \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor
+
+
+
+ve win 10 home uz asi ~~nejde everyone anonymous
+
+https://www.diigo.com/user/martin12333?query=%23filesharing
+https://www.diigo.com/user/martin12333?query=%23filesharing
+diigo 04/22/2023
+
+windows 10 creating local accounts just for file sharing site:superuser.com
+windows file sharing by creating local accounts just for sharing
+
+
+rozchodil jen pres heslo of
+Eda
+
+
+
+
+net user
+Get-LocalUser
+
+Eda
+
+cat  \\DESKTOP-PSJ0TRO\e--evwrite-everyone-pozor\share.f8.ps1
+
+# file://desktop-psj0tro/e--everyone-read/
+
+rfdfgdgfdfgfd
+
+e--everyone-read (
+file://DESKTOP-PSJ0TRO/e--everyone-read
+)
+
+
+pspad.com
+
+icacls d:\
+
---
+++ windows-f8/share.f8.ps1
@@ -0,0 +1,21 @@
+
+exit
+
+
+net user
+Get-LocalUser
+
+Eda
+
+
+rfdfgdgfdfgfd
+
+e--everyone-read (
+file://DESKTOP-PSJ0TRO/e--everyone-read
+)
+
+
+pspad.com
+
+icacls d:\
+
