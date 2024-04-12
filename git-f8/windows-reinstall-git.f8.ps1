45c804940125a8d45b945bf7ee13f0a01bec347b
Martin Milan <martin12333@users.noreply.github.com>
2024-04-12 21:50:00 +0200

    backup

--- git-f8/windows-reinstall-git.f8.ps1
+++ git-f8/windows-reinstall-git.f8.ps1
@@ -3,10 +3,15 @@
 exit
 
 
+# file:///C:\Users\marti\OneDrive\git-f8\windows-reinstall-git.f8.ps1
+
 
 I recommend to create a windows restore point before and after an installation
 
-https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx
+
+
+https://www.bing.com/search?q=Bing+AI
+&
 
 That's a great recommendation! Creating a **Windows Restore Point** before and after an installation can help protect your system. If the installation causes any issues, you can use the restore point to return your system to the state it was in before the installation. This can be a lifesaver when dealing with problematic software or updates. Here's a brief guide on how to
 ...
@@ -22,6 +27,28 @@ Get-Alias -Verbose bash
 dir 'C:\Program Files\Git*'
 
 & 'C:\Program Files\totalcmd\TOTALCMD64.EXE'  'C:\Program Files\Git'
+
+
+
+file:///C:\Users\marti\OneDrive\git-f8\Git\etc\install-options.txt
+
+
+
+
+file:///C:\Users\marti\OneDrive\git-f8\Git\etc\gitconfig
+
+autocrlf = true
+bash
+cd 'C:\Program Files\Git'
+diff etc/gitconfig  etc/gitconfig.0
+
+marti@len20 MINGW64 / not__git_ps1
+$ diff etc/gitconfig  etc/gitconfig.0
+12c12
+<       autocrlf = true
+---
+>       autocrlf = false
+
 
 Milan 2021
 secu-rename-NOTUNINST-244 due to security bug from 2021
