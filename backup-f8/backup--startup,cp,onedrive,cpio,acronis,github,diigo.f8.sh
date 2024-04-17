df581ca156bb716c5fddb0ebcd1233e6af907f3d
Martin Milan <martin12333@users.noreply.github.com>
2024-04-17 16:06:09 +0200

    backup

--- backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
+++ backup-f8/backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh
@@ -1,9 +1,10 @@
 
 # f8_text_file
-# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
-(do-not run this file as a whole)
+# (an f8_text_file is-not meant to be run as a whole)
 )
 exit
+
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
 
 echo '-------- section --------'
 
