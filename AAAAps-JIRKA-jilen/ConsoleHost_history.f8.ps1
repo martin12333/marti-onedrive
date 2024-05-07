4e07b9d1de817af791427e90b6f09d97e210c9ee
Martin Milan <martin12333@users.noreply.github.com>
2024-05-07 23:45:51 +0200

    backup

---
+++ AAAAps-JIRKA-jilen/ConsoleHost_history.f8.ps1
@@ -0,0 +1,149 @@
+
+exit
+
+cd \
+dir
+dir /a
+d:
+cp C:\Users\milan\Desktop\du64.exe .
+ls
+.\du64.exe -v -ct .\adokumnt\
+.\du64.exe -v -ct .\adokumnt\ >d:\elevaa-okno-x-du64-ct-v-c
+.\du64.exe -v -ct c:\ >d:\elevaa-okno-x-du64-ct-v-c
+history
+ls
+dir
+dir /a
+cmd
+e:
+history
+history /?
+history -?
+chkdsk
+c:
+cd /users
+C:\Users\milan\Desktop\du64.exe .
+echo .\du64.exe -v -ct dgdgc:\ dgdfdgd:\elevaa-okno-x-du64-ct-v-c
+C:\Users\milan\Desktop\du64.exe -v -ct . >E:\milan\elev-okno-x-du64-ct-v-users
+ls
+cd .\milan_loc_a\
+dir
+dir /a
+cmd
+cd ..\Jirka\
+cmd
+explorer.exe
+net user
+net user /help
+net user
+net user Ljuba /add /active:yes
+net user Ljuba *
+net localgroup
+net localgroup users
+net localgroup users Ljuba
+net localgroup users Ljuba /delete
+net localgroup users
+net localgroup users Ljuba /add
+net localgroup users
+net localgroup users Ljuba /delete
+net localgroup guests Ljuba /add
+net localgroup guests
+e:
+icacls e:\
+icacls /?
+icacls e:\ /save aclfile
+ls
+dir
+cat .\aclfile
+icacls e:\ >icacls.txt
+cat .\icacls.txt
+icacls C:\Windows\
+icacls e:\
+icacls E:\milan\
+cmd
+runas /noprofile
+icacls e:
+echo DESKTOP-PSJ0TRO\Ljuba
+runas /noprofile /user:DESKTOP-PSJ0TRO\Ljuba cmd
+icacls E:\milan\
+icacls e:\ >icacls2.txt
+net localgroup guests
+net localgroup
+net localgroup administrators
+dir C:\Users\Jirka\
+dir C:\Users\Jirka\Dropbox\
+e:
+dir C:\Users\marti\AppData\Local\Google\Chrome\
+dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\'
+dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 1\'
+dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\'
+dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /a /s
+cmd
+cmd dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /a /s
+cmd dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /a
+cmd /c dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /a
+cmd /c dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /?
+cmd /c "dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /?"
+cmd /c "dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' "
+cmd /c "dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\'"
+cmd /c "dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /s"
+echo "dir 'C:\Users\marti\AppData\Local\Google\Chrome\User Data\Profile 2\' /s"
+cmd
+exit
+cd C:\Users\
+cmd /c dir /a
+whoami.exe
+whoami.exe /?
+whoami.exe /upn
+whoami.exe /user
+whoami.exe /groups
+whoami.exe /claims
+whoami.exe /priv
+whoami.exe /all
+cmd /?
+cmd /c dir /a
+cmd /c dir /a /s
+cmd /c dir /a
+net localgroup administrators
+net localgroup
+net localgroup users
+cmd /c dir /a
+cmd /c dir C:\ProgramData\  /a
+cmd /c dir /a
+cmd /c dir .\BokLocal\  /a /s
+cmd /c dir /a
+cmd /c dir .\ivala\  /a /s
+cmd /c dir /a
+cmd /c dir .\Ljuba\  /a /s
+cmd /c dir /a
+cmd /c dir .\marti\  /a /s
+cmd /c dir /a
+cmd /c dir .\Public\  /a /s
+cmd /c dir .\milan_loc_a\  /a /s
+cmd /c dir .\milan  /a /s
+net user Eda /add /active:yes
+e:
+net user Eda /add /active:yes
+net user Eda /add /active:yes
+net user Eda *
+net user Eda *
+net user Ljuba *
+net localgroup users Ljuba /add
+net localgroup guests Ljuba /delete
+net localgroup guests
+net localgroup users
+systeminfo.exe
+cd .\Dokumenty\
+dir
+ls
+cd ..
+cd .\Documents\
+ls
+cd ..
+cd .\OneDrive\
+cd .\ps-jilen\
+systeminfo.exe >systeminfo.txt
+Get-Content .\systeminfo.txt
+systeminfo.exe /?
+winver.exe
+winver.exe /?
