387561238bb4ec9a1e205a993b454119995ffdc6
Martin Milan <martin12333@users.noreply.github.com>
2024-05-22 04:56:12 +0200

    backup

--- eev-f8/anggtwu.net--e.f8.sh
+++ eev-f8/anggtwu.net--e.f8.sh
@@ -3,12 +3,14 @@ exit
 
 http://anggtwu.net/e/
 
+^a
 ^c
 
 file:///C:\Users\marti\OneDrive\eev-f8\anggtwu.net--e.txt
 
 ///
 site:angg.twu.net mathematics philosophy
+site:anggtwu.net mathematics philosophy
 edrx
 
 
@@ -21,20 +23,37 @@ cd ~/OneDrive
 bash -i
 
 
-cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i
-
-cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i lean
-cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i agda
-cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i coq
 
 
 
-cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep "Nov  1"| grep '[:]'
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep "Dec"| grep '[:]'
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep "Nov "| grep '[:]'
 cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Oct| grep '[:]'
 cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Sep| grep '[:]'
 cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Jul| grep '[:]'
 cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep May| grep '[:]'
 cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Apr| grep '[:]'
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Mar| grep '[:]'
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Feb| grep '[:]'
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep Jan| grep '[:]'
+
+
+##-rw-r--r--   1 edrx edrx   13025 Feb  5 20:27 howm.e
+-rw-r--r--   1 edrx edrx   22965 Apr 14 20:50 hd.e
+-rw-r--r--   1 edrx edrx    2922 Apr  4 13:21 wine.e
+
+-rw-r--r--   1 edrx edrx  741550 May 14 01:24 emacs.e
+-rw-r--r--   1 edrx edrx  114806 May 15 21:55 hardware.e
+
+-rw-r--r--   1 edrx edrx   54187 May  6 00:03 w32.e
+
+
+
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i sage  #
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i lean
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i agda
+cat eev-f8/anggtwu.net--e.txt|  grep 'e$' |grep -i coq
+
 
 
 cat eev-f8/anggtwu.net--e.txt|  grep 'e$'| grep '[:]'  |grep Mar\ 3
@@ -48,7 +67,6 @@ cat eev-f8/anggtwu.net--e.txt|  grep 'e$'| grep '[:]'  |grep Mar\ \ ''
 
 
 
--rw-r--r--   1 edrx edrx   44657 Apr 22 00:00 w32.e
 
 
 
