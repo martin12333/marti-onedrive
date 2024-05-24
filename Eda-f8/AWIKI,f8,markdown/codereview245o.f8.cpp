8a3132f73e86747b261e03753811d71e87a0d602
Martin Milan <martin12333@users.noreply.github.com>
2024-05-25 00:52:49 +0200

    backup

--- Eda-f8/AWIKI,f8,markdown/codereview245o.f8.cpp
+++ Eda-f8/AWIKI,f8,markdown/codereview245o.f8.cpp
@@ -1,21 +1,47 @@
-codereview245o.f8.md
+
+f8_text_file
+
+https://onedrive.live.com/
+programming stuff - OneDrive
 
 
 tempCodeRunnerFile.cpp
 
+                    getline(cin, text);
 
 
 
+// "C:\Users\marti\OneDrive\Eda-f8\backups\etc.zip"
+gitconfig
+//[credential "https://dev.azure.com"]
 
-"C:\Users\marti\OneDrive\Eda-f8\backups\assignments.zip"
+
+
+//  "C:\Users\marti\OneDrive\Eda-f8\backups\assignments.zip"
 
 	Print_this_character_when_other_character.cpp
+
+    string toby = "To be, or not to be, that is the question:";
+    for(char c : toby);
+
 
 	Language_file_-asssignment-.cpp
 	Languages2_file-assignment-.cpp
 
+	vector<language> languages;
+
+		languages.push_back(langg);
+
+				for(const language& lang : languages)
+
+
 "C:\Users\marti\OneDrive\Eda-f8\backups\Other_code(examples, etc.).zip"
 append.cpp
+
+
+    hello.append(" world"s);
+string literal
+
 namespaces_function_example.cpp
 substring_example.cpp
 insert.cpp
@@ -30,6 +56,14 @@ while_loop_example.cpp
 "C:\Users\marti\OneDrive\Eda-f8\backups\personal projects.zip"
 introduce_Alice_Bob_and_Charlie.cpp
 login_to_the_internet.cpp
+
+
+    string& getName(){
+        return name;
+    }
+
+
+
 ShayNote.cpp
 
 
