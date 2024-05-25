7d4d8a50781e578f5a3ccf5f243c4f3013e2b2d9
Martin Milan <martin12333@users.noreply.github.com>
2024-05-25 05:49:48 +0200

    backup

--- Eda-f8/AWIKI,f8,markdown/codereview245o.f8.cpp
+++ Eda-f8/AWIKI,f8,markdown/codereview245o.f8.cpp
@@ -23,35 +23,119 @@ trick: emojis in chapter titles
 
 
 
+
+
+
+
+//[credential "https://dev.azure.com"]
+
 # vím, že bych neměl dělat poplachy příliš brzo, a příliš často   🦉   🧙‍♂️
 
+ZMENA 05:00
+EDIT
+byl to PLANY POPLACH
+JA TO MAM take
+//[credential "https://dev.azure.com"]
+prestoze POUZIVAM GITHUB nikoli Azure
+
+// [credential
+// *gitconfig
+/*
+muj c pf gitconfig
+[credential]
+	helper = manager
+[credential "https://dev.azure.com"]
+	useHttpPath = true
+*/
+
+MYLNE JSEM SE DOMNIVAL, ZE EDUV GIT CRED. MANAGER JE NEJAK SPOJENY S Azure NIKOLI S GITHUB ...  ALE O 2 HOD POZDEJI JSEM ZJISTIL, ZE
+JA TO MAM take
+//[credential "https://dev.azure.com"]
+prestoze POUZIVAM GITHUB nikoli Azure
+
+
 // "C:\Users\marti\OneDrive\Eda-f8\backups\etc.zip"
-⚠️gitconfig
-⚠️//[credential "https://dev.azure.com"]
+gitconfig
+//[credential]
+//	helper = manager
+//[credential "https://dev.azure.com"]
+//	useHttpPath = true
 
-proc ne nejdriv github?   github pages ?  github codespaces ?
+btw, nez azure, radeji
+ nejdriv github   github pages   github codespaces
 
-proc ne codedamn playgrounds?
+ codedamn playgrounds
 
-mozna bych si to mel pozdeji take poridit?
+oBSOLETE mozna bych si to mel pozdeji take poridit??
+OBSOLETE kdo to doporucil? proc?
 
 https://azure.microsoft.com/en-us/free/free-account-faq
 
 https://learn.microsoft.com/en-us/answers/questions/1582156/unpaid-bill
 
-I am just a hobbyist. Is there a way to set a hard limit on spending at Microsoft Azure?
+
+
+
 
 https://gemini.google.com/app/6ec8432ee5fcff1e
 
+I am just a hobbyist. Is there a way to set a hard limit on spending at Microsoft Azure?
+
 		Unfortunately, Azure doesn't offer a hard spending limit in the way you might expect for most accounts. This means you can't set a specific amount and have Azure automatically shut down spending once you reach it.
 
-		This means you can't set a specific amount and have Azure automatically shut down spending once you reach it.
 
 		pen_spark
 
 		Budgets: Azure Cost Management lets you set budgets for your account. This isn't a hard limit, but it will send you alerts when you reach a certain percentage of your budget. You can then take manual steps to control spending, like disabling unused resources.  '   https://learn.microsoft.com/en-us/azure/cost-management-billing/manage/spending-limit
 
 		pen_spark
+
+
+https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx
+
+Is this true? ...		Unfortunately, Azure doesn't offer a hard spending limit. This means you can't set a specific amount and have Azure automatically shut down spending once you reach it.
+
+
+You’re absolutely right! I appreciate your attention to detail. Unfortunately, Azure doesn’t provide a hard spending limit that automatically shuts down spending once a specific amount is reached. Instead, users have to rely on other methods to manage their costs effectively.
+...
+Remember that the spending limit isn’t available for subscriptions with commitment plans or pay-as-you-go pricing.
+
+https://azure.microsoft.com/en-us/support/legal/offer-details/
+
+
+Azure for Students is available only to students who meet the following requirements. You must affirm that you are age 18 or older and attend an accredited, degree-granting two-year or four-year educational institution where you’re a full-time student. You must verify your academic status through your organization’s email address. This offer is not available for use in a massive open online course (MOOC) or in other professional trainings from for-profit organizations.
+
+Microsoft Azure for Students Starter is available only to students. You must affirm that you are age 13 or older if you reside in the United States and age 16 or older if you reside in a country other than the United States and verify your academic status through your organization’s email address (or Shibboleth, if supported by your organization). This offer is not available for use in a massive open online course (MOOC) or in other professional trainings from for-profit organizations.
+
+
+
+
+https://azure.microsoft.com/en-us/pricing/offers/ms-azr-0111p/
+Get Started with Azure in Open Licensing
+
+Azure service credits are redeemable in increments of the approximate equivalent of $100 per unit purchased. See the equivalent amount of Azure service credit per unit purchased by currency using the currency selector. The below table is the list of currencies that can be used to purchase Azure.
+
+You can add credits to your Azure in Open subscription(s) at any time by purchasing through your partner using the same process you followed originally. Credits are used on a ‘first in first out’ basis and each purchase must be used within 12 months of activation.
+
+
+
+In the event you are at risk of exhausting your credits prior to reordering additional credits through your partner, there is an option to add credits to your subscription(s) through the use of a credit card. This is done by selecting the subscription to which you would like add credits in the usage and billing portal. From this page, select the ‘Add Credits’ option and then select the credit card option under ‘Method.’
+
+
+
+No. The Azure in Open program does not offer any rollover benefits to future periods. Customers have a “use or lose” deadline that expires 12 months after the date of activation. If a customer has activated multiple OSA keys at different times, each key will have a separate expiration date based on the each key’s individual activation date.
+
+
+
+
+If an Azure in Open subscription reaches a zero balance the services will be suspended. The data will be retained for 90 days and once funds are added to the subscription the services can be reinstated, however there could be some services that would need to be redeployed.
+
+
+
+
+
+
+
 
 
 
---
+++ Eda-f8/AWIKI,f8,markdown/mmb...falsealarm245_p05,azure245_p03.f8.md
@@ -0,0 +1,3 @@
+mmb...falsealarm245_p05,azure245_p03.f8.md
+
+
