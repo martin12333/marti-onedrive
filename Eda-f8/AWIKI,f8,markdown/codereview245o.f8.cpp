81e3d7c8d47b5a130880484275ceb7a60c038763
Martin Milan <martin12333@users.noreply.github.com>
2024-06-01 18:37:10 +0200

    backup

--- Eda-f8/AWIKI,f8,markdown/codereview245o.f8.cpp
+++ Eda-f8/AWIKI,f8,markdown/codereview245o.f8.cpp
@@ -78,12 +78,15 @@ tempCodeRunnerFile.cpp
 
 
 
+reviewed👍 2461
+"C:\Users\marti\OneDrive\Eda-f8\kopie--programming stuff\something_else"
+tempCodeRunnerFile.cpp
 
 
 
-🚧
 245p 03:08
-TODO LATER
+ LATER  2461
+
 
 "C:\Users\marti\OneDrive\Eda-f8\backups\Other_code(examples, etc.).zip"
 append.cpp
@@ -92,13 +95,120 @@ append.cpp
 ""s // string literal?
 
 namespaces_function_example.cpp
+
 substring_example.cpp
+
 insert.cpp
+    string str = "fo    r";
+    cout << "str = " << str << endl;
+    str.insert(2, "lde"s);
+
+
+
 tempCodeRunnerFile.cpp
+
 messing_around_with_find.cpp
 messing_around_with_rfind.cpp
+
 insert_and_npos.cpp
+    auto opos = hello.find('o');
+
+    if(opos != string::npos){
+        hello.insert(opos, 2, 'o');     // I do not understand
+    }
+
+
+
 string_and_size.cpp
+
+    string howdy = {'H', 'e', 'l', 'l', 'o'};
+    cout << "howdy has " << howdy.size() << " characters: " << howdy << endl;
+
+
+
+
 while_loop_example.cpp
 
+
+
+
+
+
+
+
+
+
+
+dollar_sign_in_console.log_example.js
+let name = "Alice";
+console.log(`I'm ${name}!`);  '
+
+todo google search:
+ templating string literal
+
+
+Filestream.cpp
+if_and_elseif.js
+
+if(js === 'amazing'){
+    console.log("javascript is amazing!");
+}
+
+making_text_backwards.cpp
+my1.js
+
+
+function main() { // note, this code was created by chatGPT
+
+    const readline = require('readline');
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+
+// mb:2461
+// oh, a  nodejs  callback
+//  deno, v8, qjs has a function for `cin >> x`
+
+    rl.question("Is JavaScript amazing or terrible? ", function(answer) {
+        if (answer.toLowerCase() === "amazing") {
+            console.log("JavaScript is amazing!");
+        } else if (answer.toLowerCase() === "terrible") {
+            console.log("JavaScript is terrible!!!");
+        } else {
+            console.log("Invalid input. Please enter 'amazing' or 'terrible'.");
+        }
+        rl.close();
+    });
+}
+
+
+
+
+minulou neděli jsem opět zazálohoval všechny Tvoje soubory z Onedrive, kromě toho git.exe 400MB
+
+dneska jsem je dočetl
+
+jsou docela dobré
+
+sám si musím  připomenout '==='
+
+a prace se stringy je dulezita v kazdem jazyce
+
+mozna si najdu
+	c++ string literal ... the  '"'s   suffix
+
+
+MIMOCHODEM:
+chtěl bych se zeptat, zda už jde
+ spustit ten `node` z toho VSCode
+... zda pomohlo to
+
+> **Restarting my computer worked.**
+
+
+
+🚧2461
 
