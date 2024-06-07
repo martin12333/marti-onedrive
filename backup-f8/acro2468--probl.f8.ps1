e15159a24041dd7d8a3fffb2c664b4999b0b5eb6
Martin Milan <martin12333@users.noreply.github.com>
2024-06-08 01:58:03 +0200

    add

--- backup-f8/acro2468--probl.f8.ps1
+++ backup-f8/acro2468--probl.f8.ps1
@@ -5,9 +5,38 @@ gui
 maybe deleted backups of MILAN 1dr
 while thinking they were of mar 1 dr  ?
 
+MAYBE BECAUSE I **CLONED** THE SETTINGS  ?!
+
+MAYBE BECAUSE ONEDRIVE IS NOT A NORMAL FOLDER
+... STRANGE ~~~JUNCTI WHEN VIEWED FROM LINUX ....
+AND I SHOULD BE LOGGED-IN AS MILAN TO BACKUP MILAN ?!
+
 
 quickfix: set  different  output directories
+still strange in gui cleanup dele
+BOJIM NEPOMOHLO
+
+QUICKFIX
+
+export settings
+acro-sett--acro2468-prb--before-del-setti-of-milan
+
+ctrl-prtscr
+
+RE-CREATE THE SETTINGS OF MILAN
+
+REDO BKUP OF MARTIN
+
+
+
+restart acro
+nepomohlo
+
+
+
+
 export settings
 acro2468--probl.f8.ps1
 
+REDELTE exceptions for allowing tc.exe sslldlljhost to modify tib*
 
