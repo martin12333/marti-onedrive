4a88f79e8e5bff3c22e6789fc9b0bde5ed6dd8a1
Martin Milan <martin12333@users.noreply.github.com>
2024-06-07 21:38:33 +0200

    backup

---
+++ git-f8/probl2467.f8.ps1
@@ -0,0 +1,79 @@
+
+
+exit
+
+git commit -v -a -m backup
+
+byl jsem jinde
+
+
+oprava
+
+necham byt
+
+cd C:\Users\marti\OneDrive
+git commit -v -a -m backup
+ git add -vv  '*.f8.*'   "C:\Users\marti\OneDrive\0000"
+
+
+byl jsem jinde
+
+PS D:\umarti\OneDrive--robo> git commit -v -a -m backupwarning: in the working copy of 'tar-exe/c/Users/marti/AppData/Roaming/Code/User/settings.json', CRLF will be replaced by LF the next time Git touches it
+
+a dal git commit
+
+PS D:\umarti\OneDrive--robo> git commit -v -a -m backupwarning: in the working copy of 'tar-exe/c/Users/marti/AppData/Roaming/Code/User/settings.json', CRLF will be replaced by LF the next time Git touches it
+warning: in the working copy of 'tar-exe/d/umarti/powershell_eternal_history.txt', CRLF will be replaced by LF the next time Git touches it
+[main2 41a2223c] backup
+ 8 files changed, 71 insertions(+), 3919 deletions(-)
+ delete mode 100644 codewars,haskell,math/node_modules/@types/node/fs.d.ts
+
+ delete mode 100644 mylen-ila-git-mirmir/OneDrive/AAABKUP-sums20/old-releases.ubuntu.com-focal/SHA256SUMS.gpg
+ delete mode 100644 mylen-ila-git-mirmir/OneDrive/AAABKUP-sums20/old-releases.ubuntu.com-focal/SHA256SUMS.txt
+ delete mode 100644 mylen-ila-git-mirmir/OneDrive/AAABKUP-sums20/old-releases.ubuntu.com-focal/mila.txt
+
+PS D:\umarti\OneDrive--robo> dir  .git
+Get-ChildItem: Could not find item D:\umarti\OneDrive--robo\.git.
+PS D:\umarti\OneDrive--robo> dir  ../.git
+Get-ChildItem: Cannot find path 'D:\umarti\.git' because it does not exist.
+PS D:\umarti\OneDrive--robo> dir  ../../.git
+PS D:\umarti\OneDrive--robo> bash
+
+marti@len20 MINGW64 /d/umarti/OneDrive--robo (main2)
+$ ls  ../../.git
+
+marti@len20 MINGW64 /d/umarti/OneDrive--robo (main2)
+$ ls  -ld  ../../.git
+drwxr-xr-x 1 marti 197619 0 May  5  2022 ../../.git
+
+marti@len20 MINGW64 /d/umarti/OneDrive--robo (main2)
+$ exit
+exit
+PS D:\umarti\OneDrive--robo> dir  .git
+Get-ChildItem: Could not find item D:\umarti\OneDrive--robo\.git.
+PS D:\umarti\OneDrive--robo> bash
+
+marti@len20 MINGW64 /d/umarti/OneDrive--robo (main2)
+$ ls  -ld  .git
+-rw-r--r-- 1 marti 197619 39 Mar 10  2021 .git
+
+marti@len20 MINGW64 /d/umarti/OneDrive--robo (main2)
+$ ls  -ld  ../.git
+ls: cannot access '../.git': No such file or directory
+
+marti@len20 MINGW64 /d/umarti/OneDrive--robo (main2)
+$ cmd /c dir  .git
+Microsoft Windows [Version 10.0.22631.3593]
+(c) Microsoft Corporation. All rights reserved.
+
+D:\umarti\OneDrive--robo>cmd /c dir  .git  /a
+ Volume in drive D is dee
+ Volume Serial Number is A226-771A
+
+ Directory of D:\umarti\OneDrive--robo
+
+03/10/2021  18:17                39 .git
+               1 File(s)             39 bytes
+               0 Dir(s)   1,848,459,264 bytes free
+
+D:\umarti\OneDrive--robo>
