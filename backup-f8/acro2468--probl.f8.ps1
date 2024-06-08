e58fc162a1643135f5ff8a1471db205d790bbabd
Martin Milan <martin12333@users.noreply.github.com>
2024-06-08 03:53:03 +0200

    prb

--- backup-f8/acro2468--probl.f8.ps1
+++ backup-f8/acro2468--probl.f8.ps1
@@ -1,14 +1,15 @@
-acro2468--probl.f8.ps1
+exit
 
 acro
 gui
-maybe deleted backups of MILAN 1dr
+maybe i  deleted backups of MILAN 1dr
 while thinking they were of mar 1 dr  ?
 
 MAYBE BECAUSE I **CLONED** THE SETTINGS  ?!
 
-// probably not MAYBE BECAUSE ONEDRIVE IS NOT A NORMAL FOLDER ... there are ... STRANGE ~~~JUNCTI WHEN VIEWED FROM LINUX .... AND I SHOULD BE LOGGED-IN AS MILAN TO BACKUP MILAN ?!
+// probably not: MAYBE BECAUSE ONEDRIVE IS NOT A NORMAL FOLDER ... there are ... STRANGE ~~~JUNCTI WHEN VIEWED FROM LINUX .... AND I SHOULD BE LOGGED-IN AS MILAN TO BACKUP MILAN ?!
 // probably not
+
 
 quickfix: set  different  output directories
 still strange in gui cleanup dele
@@ -16,32 +17,50 @@ BOJIM NEPOMOHLO
 restart acro
 nepomohlo
 
-
-
 export settings
+C:\Users\marti\OneDrive\acro-sett
 acro-sett--acro2468-prb--before-del-setti-of-milan
 
 
 ctrl-prtscr
-rename mila to prb 2468
+rename the cofig "mila" to "prb 2468"
+
 
 RE-CREATE THE SETTINGS OF MILAN
-del old
+del old  tib
 trash recyc
 	///strange full 1.7G should be 2.2
 
-
-export settings
-acro-sett--acro2468-prb--after-del-setti-of-milan.f8.zip.rename
-
-
 REDELeTE exceptions for allowing tc.exe,  dlljhost to modify tib*
+
+shouldnt have deleted the  config, didnt help anyhow
+
 
 export settings
 C:\Users\marti\OneDrive\acro-sett\acro-sett--acro2468-prb--after-del-setti-of-milan.f8.zip.rename
 
-REDO BKUP OF MARTIN  after editing this file
-	
+REDO BKUP OF MARTIN  after editing this file   .ps1
+
+
 
 look inside the 1.7g ... too late
 filelist?
+"C:\Users\marti\OneDrive\backup-f8\divnyMILAN-222-onedri-to-D_inc_b17_s2_v1.tib.txt"
+
+
+"C:\Users\marti\OneDrive\backup-f8\acro2468--probl\2024-06-07_23h10_47.png"
+
+
+web search
+
+strings from gui
+
+	~~clean versions
+	~~clone setti
+
+
+
+
+
+
+
