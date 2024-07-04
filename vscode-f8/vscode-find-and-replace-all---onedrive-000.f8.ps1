b794dd741fb7c7788b81f4a910ab6253ef437793
Martin Milan <martin12333@users.noreply.github.com>
2024-07-04 21:49:26 +0200

    backup

--- vscode-f8/vscode-find-and-replace-all---onedrive-000.f8.ps1
+++ vscode-f8/vscode-find-and-replace-all---onedrive-000.f8.ps1
@@ -39,7 +39,8 @@ c.users.marti[\\/]onedrive[\\/]$
 onedrive[\\/]00
 
 c.{1,2}users.marti[\\/]onedrive[\\/][$]
-24
+c.{1,2}users.marti[\\/]onedrive[^"' ]?[$]
+'"24
 
 c.{1,2}users.marti[\\/]onedrive[\\/][^'" ]*
 2634
@@ -51,40 +52,88 @@ bash
 ###grep    --count  --no-filename  -i -R -E 'c.{1,2}users.marti[\\/]onedrive$'   .
 grep      -i -R -E 'c.{1,2}users.marti[\\/]onedrive$'   .  |  wc
 grep     --no-filename   -i -R -E -o  'c.{1,2}users.marti[\\/]onedrive(.)'   .  |  sort -u
-grep     --no-filename   -i -R -E -o  'c.{1,2}users.marti[\\/]onedrive(.)'   .  |  sort -u
+
+x='c.{1,2}users.marti[\\/]''onedrive[\\/]'"[^\"' ]*"
+'
+echo $x
+grep     --no-filename   -i -R -E -o  "$x"   .  |  sort -u   >../1
+grep     --no-filename   -i -R -E -o  "$x"   .    >../2
+mv -iv 1  ../1
+mv -iv 2  ../2
+y
+mv -iv 1  ../1
+mv -iv ../2  vscode-f8/grep-o--debug.txt
+sort -u  vscode-f8/grep-o--debug.txt  > vscode-f8/grep-o--debug--sorted-u.txt
+
+
 grep --help
 exit
 
-C:\Users\marti\OneDrive\conn\nefoam antidepresiva slovník 2021.html
-  41,1: onedrive/antid</p>
-
-C:\Users\marti\OneDrive\git-bash-dotfiles\2245.gitignore
-  27,1: OneDrive/Obr*/
-  29,1: OneDrive/alm*/
-
-C:\Users\marti\OneDrive\git-f8\git-add--mistake.f8.sh
-  45,1: OneDrive/                old-marti-hyperpolyglot/
-  49,1: OneDrive/                old-marti-hyperpolyglot/
-
-C:\Users\marti\OneDrive\mylen-ila-git-mirmir\rem.gitignore
-  23,1: OneDrive/Obr*/
-  25,1: OneDrive/alm*/
-
-C:\Users\marti\OneDrive\ntidep\nefoam-antidepresiva-slovnik-2021.f8.md
-  276,1: OneDrive/antid
-
-C:\Users\marti\OneDrive\OLDgit-bash-dotfiles - Copyl\2245.gitignore
-  27,1: OneDrive/Obr*/
-  29,1: OneDrive/alm*/
-
-C:\Users\marti\OneDrive\ROOT\nefoam antidepresiva slovník 2021.md
-  253,1: OneDrive/antid
 
 
+Binary file ./.vscode/my/state.vscdb matches
+Binary file ./AI/einops.rocks/utils/__pycache__/__init__.cpython-310.pyc matches
+Binary file ./AI/Karpathy/__pycache__/__init__.cpython-310.pyc matches
+Binary file ./AI/Karpathy/__pycache__/mmilanutil.cpython-310.pyc matches
+Binary file ./AI/Karpathy/Antimatter543/__pycache__/__init__.cpython-310.pyc matches
+Binary file ./AI/Karpathy/Antimatter543/__pycache__/mmilanutil.cpython-310.pyc matches
+Binary file ./conda-dotfiles/.vs/dotfiles/v16/.suo matches
+Binary file ./cpio/228o.cpio matches
+Binary file ./cpio/2292-2000k matches
+Binary file ./cpio/229f-2000k.cpio matches
+Binary file ./Desktop/(❁◡❁)ALINKS - Shortcut - Copy.lnk matches
+Binary file ./Desktop/ALINKS - Shortcut.lnk matches
+Binary file ./Desktop/ALINKS/Screenshots - Shortcut.lnk matches
+Binary file ./Desktop/Screenshots - Shortcut.lnk matches
+Binary file ./Desktop/Shortcut to Desktop (OneDrive - Personal).lnk matches
+Binary file ./docker-214/22a/ma214e2.tar matches
+Binary file ./Dokumenty234/Shortcut to Documents (OneDrive - Personal).lnk matches
+Binary file ./easy-extensibility--f8/easy-extensibilityOLDOLD - Shortcut.lnk matches
+Binary file ./Eda-f8/backups/EdL-pstuff-MMilan (1).zip matches
+Binary file ./Eda-f8/backups/EdL-pstuff-MMilan.zip matches
+Binary file ./Eda-f8/backups/OneDrive-2024-05-25.zip matches
+Binary file ./Eda-f8/backups/powershell (1).zip matches
+Binary file ./Eda-f8/backups/powershell (2).zip matches
+Binary file ./Eda-f8/backups/powershell (3).zip matches
+Binary file ./Eda-f8/backups/programming stuff (3).zip matches
+Binary file ./hello-debugger-onedr/state.vscdb matches
+Binary file ./mmilanutil/__pycache__/__init__.cpython-310.pyc matches
+Binary file ./mmilanutil/__pycache__/mmilanutil.cpython-310.pyc matches
+Binary file ./Non-24-Hour--Sleep-Disorder/History matches
+Binary file ./Obrázky/Shortcut to Pictures (OneDrive - Personal).lnk matches
+Binary file ./Obrázky/Snímky obrazovky/Shortcut to Screenshots (OneDrive - Personal).lnk matches
+Binary file ./OLDwsl-dotfiles - Copy/.bash_history matches
+Binary file ./ROOT/OckovaciCertifikat.pdf - Shortcut.lnk matches
+Binary file ./tar-exe/home/martin/.bash_history matches
+Binary file ./tar-exe/w/home/martin/.bash_history matches
+Binary file ./tar-TAR-MA-SOUKR-KLIC/ma21.3k.tar matches
+Binary file ./tar-TAR-MA-SOUKR-KLIC/ma214.72.tar matches
+Binary file ./tar-TAR-MA-SOUKR-KLIC/ma214e2.tar matches
+Binary file ./vscode----roaming--Code/User4/globalStorage/state.vscdb matches
+Binary file ./vscode----roaming--Code/User4/globalStorage/state.vscdb.backup matches
+Binary file ./vscode-zsh-bash-gitbash-pwsh/0state.vscdb matches
+Binary file ./vscode-zsh-bash-gitbash-pwsh/state.vscdb matches
+Binary file ./vscode-zsh--myjupoid/state.vscdb matches
+Binary file ./wsl-dotfiles/.bash_history matches
+Binary file ./wsl-dotfiles/Copy.bash_history matches
+Binary file ./wsl-dotfiles-gold-20-to-22/home/martin/.bash_history matches
+Binary file ./wsl-dotfiles-gold-20-to-22/home/martin/Copy.bash_history matches
 
 
 
+c/Users/marti/OneDrive/AI:/cai
+c/Users/marti/OneDrive/bcloudREADONLY
+c/Users/marti/OneDrive/docker-214
+c/Users/marti/OneDrive/docker-214:/acloudSUBDIR
+c/Users/marti/OneDrive/docker-214:/amyclouddrive
+c/Users/marti/OneDrive/docker-214:/amyclouddrive<span
+c/Users/marti/OneDrive/docker-214:/home/jovyan/acloudSUBDIR
+c/Users/marti/OneDrive/docker-214:/home/jovyan/myclouddrive
 
-##C:\Users\marti\OneDrive\f8-notebooks--older\how-do-i-find-and-replace-all-occurrences-in-all-files-in-visual-studio-code.txt
+
+
+c:/users/marti/onedrive/ai/file:1
+c:/users/marti/onedrive/ai/file:1)
+
 
 
