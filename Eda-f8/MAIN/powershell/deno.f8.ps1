2fddc725d8f0326adf3696a5c71c174a18fd2ff0
Martin Milan <martin12333@users.noreply.github.com>
2024-08-26 15:17:32 +0200

    backup

--- Eda-f8/MAIN/powershell/deno.f8.ps1
+++ Eda-f8/MAIN/powershell/deno.f8.ps1
@@ -21,6 +21,17 @@ console.log(x,x)
 
 
 
+# d.docs.live.net failed all attempts d.docs.live.net
+https://onedrive.live.com/?id=79E151E1DB9C98B0%21226&cid=79E151E1DB9C98B0
+empty  \\d.docs.live.net\79e151e1db9c98b0\
+faaail https://d.docs.live.net/79e151e1db9c98b0/
+??? \\d.docs.live.net@SSL\DavWWWRoot\79e151e1db9c98b0\
+d.docs.live.net
+winscp webdav fail 404 or empty  ... ???  must be enabled at ms ????
+
+
+
+
 
 Deno ... a few beginner commands to play with the Deno REPL
 
@@ -29,8 +40,89 @@ Deno ... play with the Deno REPL console.log env.get readtextfile globalThis
 
 
 
+# https://docs.deno.com/runtime/tutorials/hello_world/
+
+
+https://docs.deno.com/examples/prompts/
+
+
+alert("Please acknowledge the message.");
+console.log("The message has been acknowledged.");
+
+const shouldProceed = confirm("Do you want to proceed?");
+y
+console.log("Should proceed?", shouldProceed);
+
+const name = prompt("Please enter your name:");
+milan
+console.log("Name:", name);
+
+const age = prompt("Please enter your age:", "18");
+console.log("Age:", age);
+
+
+---------------------------
+node.exe
+function  prompt() {}
+const cage = prompt("Please enter your age:", "18");
+// Uncaught SyntaxError: Identifier 'age' has already been declared
+
+var vage = prompt("Please enter your age:", "18");
+
+let lage = prompt("Please enter your age:", "18");
+// Uncaught SyntaxError: Identifier 'age' has already been declared
+
+.quit
+-----------------------
+
+
+
+
+alert("Please acknowledge the message.");
+console.log("The message has been acknowledged.");
+
+const shouldProceed = confirm("Do you want to proceed?");
+console.log("Should proceed?", shouldProceed);
+
+const name = prompt("Please enter your name:");
+console.log("Name:", name);
+
+const age = prompt("Please enter your age:", "18");
+console.log("Age:", age);
+
+
+
+
+
+https://deno.com/learn/scripts-clis
+
+npm cowsay
+
+
+https://docs.deno.com/examples/color-logging/
+
+console.log("%cHello World", "color: red");
+console.log("%cHello World", "background-color: blue");
+console.log("%cHello World", "text-decoration: underline");
+console.log("%cHello World", "text-decoration: line-through");
+console.log("%cHello World", "font-weight: bold");
+console.log("%cHello World", "color: red; font-weight: bold");
+console.log("%cHello %cWorld", "color: red", "color: blue");
+console.log("%cHello World", "color: #FFC0CB");
+console.log("%cHello World", "color: rgb(255, 192, 203)");
+
+
+https://docs.deno.com/examples/hex-base64-encoding/
+
+
+
+
+
 https://docs.deno.com/runtime/manual/tools/repl/
 
+
+too heavy for me now?
+https://docs.deno.com/runtime/manual/references/vscode_deno/
 
 
 
@@ -63,3 +155,5 @@ $env:Path
 # Stuck? Join our Discord https://discord.gg/deno
 # PS C:\Eduard laifr\test file\graphics programs>
 
+
+https://docs.deno.com/runtime/manual/getting_started/installation/#testing-your-installation
