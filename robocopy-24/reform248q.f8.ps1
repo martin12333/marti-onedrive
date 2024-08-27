06d8a8cb3cb87949c9cd8f424043043e01fdfc2a
Martin Milan <martin12333@users.noreply.github.com>
2024-08-27 15:26:39 +0200

    backup

--- robocopy-24/reform248q.f8.ps1
+++ robocopy-24/reform248q.f8.ps1
@@ -1,54 +1,24 @@
 
-# f8_text_file
 # (an f8_text_file is-not meant to be run as a whole)
-)
 exit
 
-(selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
-
-
 echo '-------- section --------'
+
+***
 
 refactor of onedrive
 reconstruction
 
 
-
 echo '-------- section --------'
-
-# tech detail
-code C:\Users\marti\OneDrive
-funguje z druheho okna   vsc onedri\xyz ,  if C:\Users\marti\OneDrive  is opened
-code C:\Users\marti\OneDrive\tar-exe\xcopy-robocopy--mymy.f8.ps1
-
-
-
-
-***
-
-
-echo '-------- section --------'
-
-
-echo '-------- section --------'
-
-
-
-****.*
-
-
-
-
-
-
-echo '-------- section --------'
-
-
 
 248r
 15:05
 
-existing fold
+existing folders
+
+
+proposed changes
 
 
 
@@ -57,3 +27,11 @@ Sent by you:
 
 
 echo '-------- section --------'
+
+echo '-------- section --------'
+
+# a tech detail
+code C:\Users\marti\OneDrive
+funguje z druheho okna   vsc onedri\xyz ,  if C:\Users\marti\OneDrive  is opened
+code C:\Users\marti\OneDrive\tar-exe\xcopy-robocopy--mymy.f8.ps1
+
