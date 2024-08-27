8bbbac9bfc49d17c8456820e3cd502f9eb4b1461
Martin Milan <martin12333@users.noreply.github.com>
2024-08-27 19:58:34 +0200

    backup

--- robocopy-24/reform248q.f8.ps1
+++ robocopy-24/reform248q.f8.ps1
@@ -93,3 +93,49 @@ code C:\Users\marti\OneDrive
 funguje z druheho okna   vsc onedri\xyz ,  if C:\Users\marti\OneDrive  is opened
 code C:\Users\marti\OneDrive\tar-exe\xcopy-robocopy--mymy.f8.ps1
 
+
+
+
+echo '------------'
+
+add 'gh--martin--to--Eda/00BACKUP.f8.dib'
+[31;1mwarning: in the working copy of 'tar-exe/d/umarti/powershell_eternal_history.txt', CRLF will be replaced by LF the next time Git touches it[0m
+[main2 02fb2934] backup
+ 39 files changed, 395 insertions(+), 3 deletions(-)
+ rename {Eda-f8/MAIN => 0RENAME MOVE DIRS USING VSCODE}/icecream.f8.yaml (100%)
+ rename Eda-f8/MAIN/powershell/BACKUP.f8.dib => gh--martin--to--Eda/00BACKUP.f8.dib (95%)
+ create mode 100644 gh--martin--to--Eda/0RENAME MOVE DIRS USING VSCODE/icecream.f8.yaml
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/24-3-tech_La_Bo.f8.md (100%)
+ rename {Eda-f8/MAIN/powershell => gh--martin--to--Eda}/24-4-explain-f8_text_file.f8.ps1 (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/24-5-tech_La_Bo.f8.md (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/24-8-tech_La_Bo.f8.md (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/Ed...path245p--reboot,restart.f8.md (100%)
+ rename {Eda-f8/MAIN/powershell => gh--martin--to--Eda}/Intel_OpenCL-vulkaninfo.f8.ps1 (100%)
+ rename {Eda-f8/MAIN/powershell => gh--martin--to--Eda}/MB-GIT.f8.dib (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/MSEdge-Workspace-2----EL--MB.f8.md (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/ai---write-a-stand-up-comedy-script-about.f8.md (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/code-runner.f8.md (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/codereview245o.f8.cpp (100%)
+ rename {Eda-f8/MAIN => gh--martin--to--Eda}/convert-cpp-to-js/10--ai.prompt.f8.md (100%)
+ rename {Eda-f8/MAIN => gh--martin--to--Eda}/convert-cpp-to-js/20--ai.prompt.f8.md (100%)
+ rename {Eda-f8/MAIN => gh--martin--to--Eda}/convert-cpp-to-js/icecream.f8.cpp (100%)
+ rename {Eda-f8/MAIN => gh--martin--to--Eda}/convert-cpp-to-js/mini.f8.cpp (100%)
+ rename {Eda-f8/MAIN => gh--martin--to--Eda}/convert-cpp-to-js/readme.f8.md (100%)
+ rename {Eda-f8/MAIN/powershell => gh--martin--to--Eda}/deno.f8.ps1 (100%)
+ create mode 100644 gh--martin--to--Eda/icecream.f8.yaml
+ rename {Eda-f8/MAIN/powershell => gh--martin--to--Eda}/main.f8.ps1 (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/mmb...falsealarm245_p05,,,azure245_p03.f8.md (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/C++-get-the-name-of-the-compiler.f8.cpp (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/Eda-is-my-nephew--publ.f8.md (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/attempt--cpp-to-yaml.f8.yaml (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/codedamn-bug-244b--lost-the-files-in--codedamn-lab.f8.md (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/codedamn.f8.sh (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/cpp.f8.sh.cpp (100%)
+ rename {Eda-f8 => gh--martin--to--Eda/root244-folder}/ms-vscode.cpptools-extension-pack.f8.md (100%)
+ rename {Eda-f8/MAIN/powershell => gh--martin--to--Eda}/semi-prog-0.f8.cmd.ps1 (100%)
+ rename {Eda-f8/MAIN/AWIKI,f8,markdown => gh--martin--to--Eda}/visualstudio.f8.md.ps1 (100%)
+ rename {invite246--onedrive--mitigate => invite246--onedrive--mitigated}/invite246--cleanup243,244.f8.ps1 (100%)
+ rename {invite246--onedrive--mitigate => invite246--onedrive--mitigated}/invite246s.f8.ipynb (100%)
+ rename {invite246--onedrive--mitigate => invite246--onedrive--mitigated}/out246s.f8.txt (100%)
+ create mode 100644 robocopy-24/00BACKUP.f8.dib
+ create mode 100644 robocopy-24/00BACKUP.f8.ipynb
