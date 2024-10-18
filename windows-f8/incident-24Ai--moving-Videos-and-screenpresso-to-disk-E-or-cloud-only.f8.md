62997a33a9bc9a6477fc3b85417a26d4076e6f9b
Martin Milan <martin12333@users.noreply.github.com>
2024-10-19 01:46:33 +0200

    backup

---
+++ windows-f8/incident-24Ai--moving-Videos-and-screenpresso-to-disk-E-or-cloud-only.f8.md
@@ -0,0 +1,22 @@
+incident-24Ai--moving-Videos-and-screenpresso-to-disk-E-or-cloud-only.f8.md
+
+
+
+
+"D:\umarti\Videos\Screenpresso"
+506 m
+622 m
+C:\pf\Git\bin\bash.exe -c 'df -m'   |clip
+Filesystem           1M-blocks     Used Available  Use% Mounted on
+C:/Program Files/Git    151328   132471     18858   88% /
+D:                       78056    77208       849   99% /d
+E:                       71000     2743     68258    4% /e
+F:                       20992        1     20992    1% /f
+G:                        8084     2941      5144   37% /g
+H:                       11000     2981      8020   28% /h
+U:                           1 -1024335    971891 1954% /u
+
+todo
+https://support.microsoft.com/en-us/office/back-up-your-folders-with-onedrive-d61a7930-a6fb-4b95-b28a-6552e77c3057
+"D:\umarti\OneDrive--robo\Videos"
+
--- windows-f8/win-disk-c-full.f8.ps1
+++ windows-f8/win-disk-c-full.f8.ps1
@@ -821,26 +821,45 @@ Swollen-Battery--li-ion
 https://onedrive.live.com/?id=4B12298D932A2860%21382&cid=4B12298D932A2860
 436
 
-# 24Ai moving Videos to cloud-only
-https://support.microsoft.com/en-us/office/back-up-your-folders-with-onedrive-d61a7930-a6fb-4b95-b28a-6552e77c3057
-"D:\umarti\Videos\Screenpresso"
-506 m
-622 m
+
+
+# 24Ai incident
+# moving Videos to ~--cloud-only--~ Edit: no, to E:, only screenpresso to cloud-only
+
+incident-24Ai--moving-Videos-and-screenpresso-to-disk-E-or-cloud-only.f8.md
+
+# onedrive--Automatic-file-downloads--robocopy.f8.md
+onedrive--Automatic-file-downloads--robocopy.f8.md
+
+
+
+
+
+
+
+# get size of a folder on disk in windows
+
+# https://stackoverflow.com/questions/1298066/get-size-of-a-folder-on-disk-in-windows
+
+
+cmd /c dir "D:\umarti\Videos\Screenpresso"
+Get-ChildItem "D:\umarti\Videos\Screenpresso"
+ -Recurse | Measure-Object -Property Length -Sum
+
+
+# get size of a disk
+C:\pf\Git\bin\bash.exe -c 'df -m' |findstr -i c:  |clip
+C:/Program Files/Git    151328   133695     17634   89% /
+
 C:\pf\Git\bin\bash.exe -c 'df -m'   |clip
 Filesystem           1M-blocks     Used Available  Use% Mounted on
-C:/Program Files/Git    151328   132471     18858   88% /
-D:                       78056    77208       849   99% /d
+C:/Program Files/Git    151328   136081     15248   90% /
+D:                       78056    77215       842   99% /d
 E:                       71000     2743     68258    4% /e
 F:                       20992        1     20992    1% /f
 G:                        8084     2941      5144   37% /g
 H:                       11000     2981      8020   28% /h
 U:                           1 -1024335    971891 1954% /u
-
-
-
-#
-C:\pf\Git\bin\bash.exe -c 'df -m' |findstr -i c:  |clip
-C:/Program Files/Git    151328   133695     17634   89% /
 
 cmd /c dir c:\| findstr.exe -i free | clip
               16 Dir(s)  36,459,503,616 bytes free
