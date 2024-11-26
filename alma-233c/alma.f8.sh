5d7148410b8a0c29c7917581598a7c4e514753fc
Martin Milan <martin12333@users.noreply.github.com>
2024-11-26 16:12:57 +0100

    backup

--- alma-233c/alma.f8.sh
+++ alma-233c/alma.f8.sh
@@ -10,6 +10,15 @@ echo '-------- section --------'
 
 
 
+file:///C:\Users\marti\OneDrive\alma-233c\alma---24bL.f8.txt.md
+https://github.com/martin12333/marti-onedrive/blob/main2/alma-233c/alma---24bL.f8.txt.md
+
+
+
+echo '-------- section --------'
+
+
+
 https://keep.google.com/#NOTE/1lXoEIxE8WvRZLa57ajSK-h8gjH98PbgnB-EUVYHFb8msFNWNfAwsQRBc17QZEbE
 
 
@@ -343,45 +352,21 @@ echo '-------- section --------'
 
 # todo split to a separate file
 alma---24bL.f8.txt.md
+file:///C:\Users\marti\OneDrive\alma-233c\alma---24bL.f8.txt.md
+https://github.com/martin12333/marti-onedrive/blob/main2/alma-233c/alma---24bL.f8.txt.md
+
 
 
 /var/log/apache2/*.log
 
 jsem pojal male podezreni, ktere zda se ping potvrzuje:
-# na alme asi jsou weby pro nekolik ruznych slovnich adres
+# na alme jsou weby pro nekolik ruznych slovnich adres
 napr.
 ping alma.karlov.mff.cuni.cz
 ping biophysics.mff.cuni.cz
 ping fu.mff.cuni.cz
 ###ping utf.mff.cuni.cz
 ###ping quantum.karlov.mff.cuni.cz
-
-24bL
-24bn
-https://www.diigo.com/user/martin12333?query=mff.cuni.cz
-edge://favorites/?q=mff.cuni.cz
-https://mail.google.com/mail/u/0/#search/biophysics.mff.cuni.cz/FMfcgzQXKNDBZRWlzjSfFcTjphSxTJnM
-
-
-http://fu.mff.cuni.cz/theory/people/person/Bok/courses/
-
-http://alma.karlov.mff.cuni.cz/lmft/
-http://fu.mff.cuni.cz/
-http://biophysics.mff.cuni.cz/
-
-"C:\Users\marti\OneDrive\alma-233c\Fyzikální ústav UK_files"
-
-e:\alma246g\
-
-
-
-
-https://web.archive.org/web/*/http://biophysics.mff.cuni.cz/*
-https://web.archive.org/web/20241124031724/http://biophysics.mff.cuni.cz/
-https://web.archive.org/web/20241124031700/http://fu.mff.cuni.cz/
-https://web.archive.org/web/sitemap/http://fu.mff.cuni.cz/
-https://web.archive.org/web/*/http://fu.mff.cuni.cz/*
-https://web.archive.org/web/*/http://alma.karlov.mff.cuni.cz/*
 
 
 
@@ -420,6 +405,10 @@ Put Cloudflare in front of your WebServer and restrict access to your Web server
 Setup docs are aplenty for Synology. Enable the firewall in your SNAS, keep it behind your core hardware firewall and use port forwarding to allow access from the internet. Setup an SSL/TLS certificate and enable 2 factor authentication on all accounts. Backup your NAS data offsite to a OneDrive/Google Drive/Dropbox service. Multicloud will allow you to securely connect to just about any service out there these days. Keep the Synology firmware updated and you’ll be fine. Been running a web server on SNAS’s for 15+ years.
 
 https://www.reddit.com/r/synology/comments/1ck7ozl/how_safe_is_it_to_host_a_webserver_on_a_nas/
+
+
+
+
 
 echo '-------- section --------'
 
