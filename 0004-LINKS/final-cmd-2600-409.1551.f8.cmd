08a3229169a978347d82e3627aab3f8366db2e12
Martin Milan <martin12333@users.noreply.github.com>
2025-04-09 23:53:35 +0200

    2600-409.2349

--- 0004-LINKS/final-cmd-2600-409.1551.f8.cmd
+++ 0004-LINKS/final-cmd-2600-409.1551.f8.cmd
@@ -4,6 +4,8 @@ exit
 
 cmd
 
+should be .sh
+
 
 done
 file:///C:\Users\marti\OneDrive\0004-LINKS\cmd-2600-409.f8.cmd
@@ -14,6 +16,8 @@ file:///C:\Users\marti\OneDrive\git-f8\export-GIT_DIR.f8.md
 
 
 rem ------------------------------------
+
+cmd
 
 Robocopy.exe   C:\Users\marti\OneDrive  D:\umarti\OneDrive--deldest    /s   /XJ     /XD Obr*    /XD Vide*     /XD .git    /XD node_modules   /XD OLD*       /xf .git   /log+:C:\Users\marti\log\OneDrive--deldest-5.log     /tee    /ndl       /nfl      /MIR
 
@@ -29,10 +33,10 @@ rem  /L
 
 rem ------------------------------------
 
-todo
+# once
 
-type .git
-gitdir: D:/GIT-bk04/marti-onedrive.git
+#type .git
+#gitdir: D:/GIT-bk04/marti-onedrive.git
 
 C:\pf\Git\cmd\git.exe add C:\Users\marti\OneDrive\Dokumenty\PowerShell\profile.ps1
 
@@ -56,6 +60,22 @@ rem cd d:\
 
 c:
 
+
+
+
+
+
+
+
+
+
+
+rem ------------------------------------
+
+
+run Robocopy above
+
+
 set GIT_DIR=/d/GIT-bk04/marti-onedrive-2600-409.21.git
 set GIT_WORK_TREE=/d/umarti/onedrive--deldest
 
@@ -73,7 +93,7 @@ exit
 	echo .$GIT_DIR.
 
 
-export GIT_WORK_TREE=/d/umarti/old2-marti-onedrive
+export GIT_WORK_TREE=/d/umarti/old3-marti-onedrive
 
 	ls -la $GIT_WORK_TREE/.g*
 	echo .$GIT_WORK_TREE.
@@ -91,10 +111,20 @@ ls ~/OneDrive/Copy.git
 
 	exit
 
+
+
+
+
+
+
+
+
+
 	# once
 
 	##cd ~/OneDrive/2400-WIKI
 	cd onedrive--deldest
+	cd $GIT_WORK_TREE
 
 	#export GIT_DIR=/d/GIT-bk04/marti-onedrive.wiki33.git
 	ls -al $GIT_DIR
@@ -110,26 +140,77 @@ pwd|clip
 
 
 #	git init
-	git  status
+	git  status|clip
+	|more.exe
 	git  status  -vv
 q
 
 
-	git add -vv        .gitignore    #'install.sh*'
-	git add -vv --dry-run  '*.md'
+
+M       git-bash-dotfiles/.bash_eternal_history
+
+	#git add -vv        .gitignore    #'install.sh*'
+	#git add -vv --dry-run  '*.md'
 	git add -vv --dry-run  '*.f8.*'
+	git add -vv --dry-run  '*.f5.*'
 	git add -vv  '*.f8.*'
-	git add -vv  '*.md'
+	git add -vv  '*.f5.*'
+
+my typo *.f.*
+mynotes.f.py
+$ git add -vv  '*.f.*'
+add 'dr4/MYNOTES-MYNOTES/mynotes.f.py'
+
+git reset -- mynotes.f.py
+
+	##git add -vv  '*.md'
 	goes into subdirs
+
+[main2 96c203c7] 2600-409
+ 10 files changed, 3553 insertions(+), 9 deletions(-)
+ create mode 100644 bash_eternal_history.f8.sh
+
+ create mode 100644 dr4/MYNOTES-MYNOTES/mynotes.f.py
+
+ create mode 100644 unicode-in-windows-in-git-repo/unicode-in-windows-in-git-repo.f8.sh
+
+
+0004-LINKS/final-cmd-2600-409.1551.f8.cmd
+2400-WIKI/history.md
+bash_eternal_history.f8.sh
+
+dr4/MYNOTES-MYNOTES/mynotes.f.py
+
+gh--martin--to--Eda/00BACKUP.f8.dib
+git-bash-dotfiles/.bash_eternal_history
+git-f8/export-GIT_DIR.f8.md
+tar-exe/d/umarti/powershell_eternal_history.txt
+unicode-in-windows-in-git-repo/unicode-in-windows-in-git-repo.f8.sh
+wiki-f8/github-wiki.f8.md
 
 
 	git ls-files
 	ls
 
+
+
+[main2 99473ee7] 2600-409
+ 3 files changed, 36 insertions(+), 4 deletions(-)
+
+
+	git commit -vv   -a -m  2600-409.2349
+
 	git commit -a -m move-zzz-away
 	git commit -a -m 250129
-	git push -vv  origin master
 
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	#git push -vv  origin master
+	git push -vv
+
+	 origin master
+
+
+	git show --name-only
 
 
 	set | grep -i history
@@ -161,3 +242,53 @@ rem ------------------------------------
 
 exit
 
+
+
+
+
+marti@len20 MINGW64 /d/umarti/onedrive--deldest (main2)
+$ git reset -- mynotes.f.py
+Unstaged changes after reset:
+M       .emacs.d/custom.el
+M       .emacs.d/init.el
+M       2400-WIKI/docs/features/built-in-note-embedding-types.md
+M       2400-WIKI/history.md
+M       AI/myenv.ps1
+M       Dokumenty/AutoHotkey.ahk
+M       Dokumenty/PowerShell/profile.ps1
+M       LICENSE
+M       doku--PowerShell-bkup/profile.ps1
+M       doku--windowsPowerShell-bkup/profile.ps1
+M       eev-f8/find-kla-intro.e.el
+M       eev-f8/find-psne-intro.e.el
+M       eev-f8/find-wconfig-browser-links.e.el
+M       eev-f8/find-wconfig-mpv-links.e.el
+M       eev-f8/find-wconfig-wget-links.e.el
+M       git-bash-dotfiles/.bash_eternal_history
+M       git-bash-dotfiles/c-PF-git/etc/install-options.txt
+M       hyperpolyglot-f8/contrib. Math, Lean, Sage, Haskell - attempt1.csv
+M       hyperpolyglot-f8/contrib. Math, Lean, Sage, Haskell - meta.csv
+M       hyperpolyglot-f8/contrib. Math, Lean, Sage, Haskell - variables .csv
+M       hyperv-ubu-serv-20/wincmd.ini
+M       mylen-ila-git-mirmir/OneDrive/223c09/system_report/system_report.0.log
+M       mylen-ila-git-mirmir/OneDrive/223c09/ti_demon_3AC086B1-7D95-488F-AB6D-A8E98799D87A_F6950287-5AD6-4601-BD6B-4EF38D3EE820_2022-03-11-21-19-41.log
+M       mylen-ila-git-mirmir/OneDrive/223c09/ti_demon_7A25B121-2779-4DCA-A8B9-86BEA561DE51_E60A4C3B-2EA8-46FC-B2FA-9C2663C4264F_2022-03-11-21-37-24.log
+M       mylen-ila-git-mirmir/OneDrive/223c09/ti_demon_8DF92793-7FCE-491B-8A55-555B53EE6DF9_ED30DE72-91A9-4DF4-A8BD-A299CA1AB114_2022-03-11-21-27-54.log
+M       mylen-ila-git-mirmir/OneDrive/223c4/223q1620/disks.txt
+M       mylen-ila-git-mirmir/OneDrive/223c4/223q1620/uefi_vars.txt
+M       mylen-ila-git-mirmir/OneDrive/223c4/223q1620/version.txt
+M       ps-mylen-mar/myconda-code.ps1
+M       ps-mylen-mar/mymycron.ps1
+M       ps-mylen-mar/mymyrsync.ps1
+M       ps-mylen-mar/mystartup.ps1
+M       ps-mylen-mar/psISEUntitled1.ps1
+M       ps-mylen-mar/vscode-Host_history.txt
+M       screen-recorder,steps-recorder/gitpinned-presso-settings.xml
+M       tar-exe/c/Users/marti/AppData/Roaming/Code/User/settings.json
+M       tar-exe/c/Users/marti/AppData/Roaming/GHISLER/wincmd.ini
+M       tar-exe/c/Users/marti/AppData/Roaming/Microsoft/Windows/PowerShell/PSReadLine/ConsoleHost_history.txt
+M       tar-exe/d/umarti/powershell_eternal_history.txt
+M       vscode-f8/dot.vscode---add-all/code.cmd
+M       vscode-f8/dot.vscode---add-all/extensions.json
+
+marti@len20 MINGW64 /d/umarti/onedrive--deldest (main2)
