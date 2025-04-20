848c795d8d9d8f52a9818749d1394cf40a0ad4ce
Martin Milan <martin12333@users.noreply.github.com>
2025-04-20 02:40:55 +0200

    backup

--- 0004-LINKS/0004-LINKS/418-reform.f8.ps1
+++ 0004-LINKS/0004-LINKS/418-reform.f8.ps1
@@ -59,4 +59,11 @@ D:\000doc24 LEN20\marti:(OI)(CI)(F)
 
 
 
-			
+
+			2 files changed, 65 insertions(+)
+			create mode 100644 0004-LINKS/0004-LINKS/418-reform.f8.ps1
+		   fatal: unable to access 'https://github.com/martin12333/marti-onedrive.git/': Failed to connect to github.com port 443 after 21211 ms: Couldn't connect to 'server
+		   
+		   commit fd14b914fc83d3687d7053d34a8d8231b798f6b7 (HEAD -> main2)
+		   Author: Martin Milan <martin12333@users.noreply.github.com>
+		   Date:   Sun Apr 20 02:36:21 2025 +0200
