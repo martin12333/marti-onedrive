2a16d48da8b94bf65149d19bcc6f68e85d28e3fa
Martin Milan <martin12333@users.noreply.github.com>
2025-05-07 07:57:45 +0200

    backup

---
+++ edge-f12-devtools-override/snippet%3A/myScript-snippet1.f5.f8.js
@@ -0,0 +1,77 @@
+
+// ctrl-enter  in workspace : 0
+// ctrl-enter  in overrides :0
+// ctrl-enter  in snippets ... runs all
+// ctrl-s
+// ctrl-shift-p
+// ?
+// !run snipp
+
+//enable tab moves focus
+// evaluate selected text in console
+// edit from vsc  quick view
+// edit from vsc  show
+// edit from vsc   ctrl`  toggle
+
+// edit from edge
+// edit from edge
+
+1
+2
+3
+4
+
+
+  54*46
+  10 ** 50
+  10n ** 50n
+/*  a.b.c.d */
+
+
+888*8
+88*888*88
+8*888*8
+88*888*88
+5
+
+typeof WebAssembly // obj
+typeof WebAssembly.CompileError // func
+typeof WebAssembly.Exception
+typeof WebAssembly.Global
+
+typeof BigInt
+typeof Clipboard
+typeof Document
+typeof FileSystemHandle
+
+typeof SpeechSynthesisUtterance
+
+typeof JSON // obj
+
+
+
+
+  var searchEngine = "https://www.google.com/"
+  var searchEngine = "https://www.bing.com/"
+  var query = "emacs eev"
+//  window.open(searchEngine + "?q=" + query, '_blank');
+
+  console.log(
+        this
+        ,
+        globalThis
+        ,
+        window
+        ,
+        document
+        ,
+        document.getElementsByTagName('iframe')
+        ,
+        document.getElementsByTagName('textarea')
+
+      )
+
+//git ls-files
+typeof CodeMirror
+typeof CodeMirror.version
+ CodeMirror.version
