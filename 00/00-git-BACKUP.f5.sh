55d94bc2c00f8e10c98efa7e82627aabdf59c5b2
Martin Milan <martin12333@users.noreply.github.com>
2025-05-08 19:12:40 +0200

    backup

--- 00/00-git-BACKUP.f5.sh
+++ 00/00-git-BACKUP.f5.sh
@@ -9,6 +9,8 @@ C:/Users/marti/OneDrive/00/Robocopy---c-onedrive---d-mir.f5.cmd
 ##cmd '/'c echo dkjkkjkjkjkj
 ##cmd /c echo dkj
 ###exit
+git add -vv    --dry-run     '*.f[0-9].*'   '*.f7.*'       '*.f5.*'   #### '*-add-all'   ##   "C:\Users\marti\OneDrive\xxxxxxxxx"
+echo _____________ _______________
 git add -vv  '*.f8.*'   '*.f7.*'       '*.f5.*'   #### '*-add-all'   ##   "C:\Users\marti\OneDrive\xxxxxxxxx"
 
 # todo 00/2400-WIKI/*.md
