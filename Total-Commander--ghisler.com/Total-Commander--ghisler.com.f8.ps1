2e43cf3d9302637831e183dd096acb3a9912fb64
Martin Milan <martin12333@users.noreply.github.com>
2025-05-28 13:20:54 +0200

    backup

--- Total-Commander--ghisler.com/Total-Commander--ghisler.com.f8.ps1
+++ Total-Commander--ghisler.com/Total-Commander--ghisler.com.f8.ps1
@@ -1,5 +1,27 @@
 exit
 
-mkdir Total-Commander--ghisler.com
+
+# C:\Users\marti\OneDrive\1804-M\A5-NAMESYST---JiLen\dir-dr-ad-s-b.txt
+cmd /c dir /a /s /b >dir-a-s-b.txt
+
+findstr.exe /i ghisler dir-a-s-b.txt | measure-object -line -word -character
+findstr.exe -i -E '[.](md|f.*)$' dir-a-s-b.txt | measure-object -line -word -character
+
+exit
+bash
+quoting bug ,maybe
+bash -c ls -l
+###bash -c grep -i -E '[.](md|f.*)$' dir-a-s-b.txt | measure-object -line -word -character
+ grep -i -E '[.](md|f.*)$' dir-a-s-b.txt | wc
+
+| grep -i ghisler | wc -l
+
+
+
+[.](md|f.*)$
+
+
+*.md,*.f*
+
 
 
