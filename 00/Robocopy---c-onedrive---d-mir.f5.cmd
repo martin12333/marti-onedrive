3f685d44e9798f73caf100d9c29b78ba427d2554
Martin Milan <martin12333@users.noreply.github.com>
2025-10-29 14:52:05 +0100

    backup

--- 00/Robocopy---c-onedrive---d-mir.f5.cmd
+++ 00/Robocopy---c-onedrive---d-mir.f5.cmd
@@ -3,7 +3,11 @@ rem /min
 rem start  "OneDrive"   /low
 rem uqqqqmarti\OneDrive--deldest
 
-Robocopy.exe   C:\Users\marti\OneDrive  D:\0004-LINKS\OneDrive      /s   /XJ     /XD Obr*    /XD Vide*     /XD .git    /XD node_modules   /XD OLD*       /xf .git   /log+:C:\Users\marti\log\OneDrive--deldest-6.log     /tee    /ndl       /nfl      /MIR
+rem f6 cmd  f6 f6 f6
+rem  f8  test
+
+Robocopy.exe   C:\Users\marti\OneDrive  D:\0004-LINKS\OneDrive      /s   /XJ     /XD Obr*    /XD Vide*     /XD .git    /XD node_modules   /XD OLD*       /xf .git    /xf .849C9593-D756-4E56-8D6E-42412*     /log+:C:\Users\marti\log\OneDrive--deldest-6.log     /tee    /ndl       /nfl      /MIR
+
 
 mkdir D:\0004-LINKS\OneDrive\00000000--D--OneDrive-IN-DEE-IS-ROBOCOPY-MIR
 
