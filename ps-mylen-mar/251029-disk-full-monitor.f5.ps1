8a97ee86f4bed9180c5462dfcb7a1c77129ccac8
Martin Milan <martin12333@users.noreply.github.com>
2025-11-08 17:29:54 +0100

    backup

--- ps-mylen-mar/251029-disk-full-monitor.f5.ps1
+++ ps-mylen-mar/251029-disk-full-monitor.f5.ps1
@@ -1,9 +1,49 @@
 
 
-while ($true) {
-	 Start-Sleep -Seconds 4; cmd /c dir c:\p*.sys /as  |findstr bytes
+Add-Type -AssemblyName PresentationFramework
 
-#"`n{0:N1} GB free on C:`n" -f ((Get-PSDrive C).Free / 1GB)
-"{0:N1} GB free on C:" -f ((Get-PSDrive C).Free / 1GB)
+# Create a tiny invisible topmost window
+$owner = New-Object System.Windows.Window
+$owner.Topmost = $true
+
+#$owner.WindowStyle = 'None'
+#$owner.ShowInTaskbar = $false
+
+$owner.ShowActivated = $true
+
+#$owner.Width = 0
+#$owner.Height = 0
+#$owner.Left = 0
+#$owner.Top = 0
+
+# Show it and bring it to front (ensures MessageBox gets focus)
+$owner.Show()
+$owner.Activate()
+[System.Windows.Forms.Application]::DoEvents()  # process focus messages
+
+while ($true) {
+	 Start-Sleep -Seconds 4;
+
+	 cmd /c dir c:\p*.sys /as  |findstr bytes
+
+	# pwsh if($z is less than 1.00) { show a messagebox "Low disk space on C: drive" }
+	$z = (Get-PSDrive C).Free / 1GB
+
+	#"`n{0:N1} GB free on C:`n" -f ((Get-PSDrive C).Free / 1GB)
+	#"{0:N1} GB free on C:" -f ((Get-PSDrive C).Free / 1GB)
+	"{0:N1} GB free on C:" -f ($z)
+
+	if ($z -lt 2.00) {
+    	#[System.Windows.MessageBox]::Show("Low disk space on C: drive", "Disk Space Warning", 'OK', 'Warning')
+		
+    	[System.Windows.MessageBox]::Show( $owner, "Low disk space on C: drive", "Disk Space Warning", 'OK', 'Warning')
+
+		# Show the modal MessageBox owned by that window
+		#[System.Windows.MessageBox]::Show($owner, "This box should have focus!", "Focused Topmost Message")
 
 	}
+}
+
+# Close the invisible window after the message box is dismissed
+$owner.Close()
+
