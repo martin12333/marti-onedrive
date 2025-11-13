7d12817e8fb17f43232beb2107a10cf5a6dc15bf
Martin Milan <martin12333@users.noreply.github.com>
2025-11-13 04:57:39 +0100

    backup

--- Dokumenty/01 napoveda.f5.cmd
+++ Dokumenty/01 napoveda.f5.cmd
@@ -1,2 +1,6 @@
 
-explorer "C:\Users\marti\OneDrive\Dokumenty\01-napoveda"
+mkdir %OneDrive%\Dokumenty\01-napoveda
+explorer %OneDrive%\Dokumenty\01-napoveda
+rem explorer "C:\Users\marti\OneDrive\Dokumenty\01-napoveda"
+
+pause
